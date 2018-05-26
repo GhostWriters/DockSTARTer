9fe9f10b0b99bcd2d635b2920d2a0bacac04d202
Eric Nemchik <eric@nemchik.com>
2018-05-26 11:59:43 -0500

    generate-yml.sh creates docker-compose.*.yml

--- compose/generate-yml.sh
+++ compose/generate-yml.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "#!/bin/bash" > ./generate-yml.`hostname`.sh
-echo "docker run --rm -v ${PWD}:/workdir mikefarah/yq yq m \\" >> ./generate-yml.`hostname`.sh
-echo "./reqs/v1.yml \\" >> ./generate-yml.`hostname`.sh
-echo "./reqs/v2.yml \\" >> ./generate-yml.`hostname`.sh
+echo "#!/bin/bash" > ./docker-compose.`hostname`.sh
+echo "docker run --rm -v ${PWD}:/workdir mikefarah/yq yq m \\" >> ./docker-compose.`hostname`.sh
+echo "./reqs/v1.yml \\" >> ./docker-compose.`hostname`.sh
+echo "./reqs/v2.yml \\" >> ./docker-compose.`hostname`.sh
 while read l || [ -n "$l" ]; do
   for f in ./apps/*.yml
   do
     [[ -e $f ]] || break
     if [[ $f =~ \/$l\. ]]; then
-      echo "$f \\" >> ./generate-yml.`hostname`.sh
+      echo "$f \\" >> ./docker-compose.`hostname`.sh
     fi
   done
 done <./`hostname`.conf
-echo "> ./generate-yml.`hostname`.yml" >> ./generate-yml.`hostname`.sh
-echo "docker-compose -f ./generate-yml.`hostname`.yml up -d" >> ./generate-yml.`hostname`.sh
+echo "> ./docker-compose.`hostname`.yml" >> ./docker-compose.`hostname`.sh
+echo "docker-compose -f ./docker-compose.`hostname`.yml up -d" >> ./docker-compose.`hostname`.sh
 
-bash ./generate-yml.`hostname`.sh
-rm ./generate-yml.`hostname`.sh
+bash ./docker-compose.`hostname`.sh
+rm ./docker-compose.`hostname`.sh
