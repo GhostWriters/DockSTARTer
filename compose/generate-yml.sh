f0f6ef81cbfa99dd3a67f2c88bfd58257e3b8f75
Eric Nemchik <eric@nemchik.com>
2018-05-31 15:25:17 -0500

    Use brackets on variables and don't use ticks for hostname

--- compose/generate-yml.sh
+++ compose/generate-yml.sh
@@ -8,52 +8,52 @@ case $(uname -m) in
   arm)    dpkg --print-architecture | grep -q "arm64" && ARCH="arm64" || ARCH="arm" ;;
 esac
 
-RUNFILE="./docker-compose.`hostname`.sh"
-echo "#!/bin/bash" > $RUNFILE
-echo "rm -rf ./`hostname`/" >> $RUNFILE
-echo "mkdir -p ./`hostname`/" >> $RUNFILE
-echo "cp .env ./`hostname`/" >> $RUNFILE
-echo "docker run --rm -v ${PWD}:/workdir mikefarah/yq yq m \\" >> $RUNFILE
-echo "./.reqs/v1.yml \\" >> $RUNFILE
-echo "./.reqs/v2.yml \\" >> $RUNFILE
-while read l || [ -n "$l" ]; do
+RUNFILE="./docker-compose.${HOSTNAME}.sh"
+echo "#!/bin/bash" > ${RUNFILE}
+echo "rm -rf ./${HOSTNAME}/" >> ${RUNFILE}
+echo "mkdir -p ./${HOSTNAME}/" >> ${RUNFILE}
+echo "cp .env ./${HOSTNAME}/" >> ${RUNFILE}
+echo "docker run --rm -v ${PWD}:/workdir mikefarah/yq yq m \\" >> ${RUNFILE}
+echo "./.reqs/v1.yml \\" >> ${RUNFILE}
+echo "./.reqs/v2.yml \\" >> ${RUNFILE}
+while read l || [ -n "${l}" ]; do
   for f in ./.apps/*.override.yml
   do
-    [[ -e $f ]] || break
-    if [[ $f =~ \/$l\.override\. ]]; then
-      echo "$f \\" >> $RUNFILE
+    [[ -e ${f} ]] || break
+    if [[ ${f} =~ \/${l}\.override\. ]]; then
+      echo "${f} \\" >> ${RUNFILE}
     fi
   done
   for f in ./.apps/*.yml
   do
-    [[ -e $f ]] || break
-    if [[ $f =~ \/$l\. ]]; then
-      if [[ $ARCH == "arm64" ]]; then
+    [[ -e ${f} ]] || break
+    if [[ ${f} =~ \/${l}\. ]]; then
+      if [[ ${ARCH} == "arm64" ]]; then
         if [[ -f ${f/\.apps\//.apps\/aarch64\/} ]]; then
-          echo "${f/\.apps\//.apps\/aarch64\/} \\" >> $RUNFILE
+          echo "${f/\.apps\//.apps\/aarch64\/} \\" >> ${RUNFILE}
         fi
         if [[ -f ${f/\.apps\//.apps\/armhf\/} ]]; then
-          echo "${f/\.apps\//.apps\/armhf\/} \\" >> $RUNFILE
+          echo "${f/\.apps\//.apps\/armhf\/} \\" >> ${RUNFILE}
         fi
         if [[ -f ${f/\.apps\//.apps\/aarch64\/} ]] || [[ -f ${f/\.apps\//.apps\/armhf\/} ]]; then
-          echo "$f \\" >> $RUNFILE
+          echo "${f} \\" >> ${RUNFILE}
         fi
       fi
-      if [[ $ARCH == "arm" ]]; then
+      if [[ ${ARCH} == "arm" ]]; then
         if [[ -f ${f/\.apps\//.apps\/armhf\/} ]]; then
-          echo "${f/\.apps\//.apps\/armhf\/} \\" >> $RUNFILE
-          echo "$f \\" >> $RUNFILE
+          echo "${f/\.apps\//.apps\/armhf\/} \\" >> ${RUNFILE}
+          echo "${f} \\" >> ${RUNFILE}
         fi
       fi
-      if [[ $ARCH == "amd64" ]]; then
-        echo "$f \\" >> $RUNFILE
+      if [[ ${ARCH} == "amd64" ]]; then
+        echo "${f} \\" >> ${RUNFILE}
       fi
     fi
   done
-done <"./`hostname`.conf"
-echo "> ./`hostname`/docker-compose.yml" >> $RUNFILE
-echo "cd ./`hostname`/ || exit" >> $RUNFILE
-echo "docker-compose up -d" >> $RUNFILE
+done <"./${HOSTNAME}.conf"
+echo "> ./${HOSTNAME}/docker-compose.yml" >> ${RUNFILE}
+echo "cd ./${HOSTNAME}/ || exit" >> ${RUNFILE}
+echo "docker-compose up -d" >> ${RUNFILE}
 
-bash $RUNFILE
-rm $RUNFILE
+bash ${RUNFILE}
+rm ${RUNFILE}
--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # # Root check
-if [[ $EUID -ne 0 ]] ; then
+if [[ ${EUID} -ne 0 ]] ; then
     echo "Please run this script as root."
     exit 0
 fi
@@ -23,12 +23,12 @@ rm get-docker.sh
 
 # # https://docs.docker.com/compose/install/#install-compose
 AVAILABLEVERSION=$(curl -s "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-curl -L "https://github.com/docker/compose/releases/download/$AVAILABLEVERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/${AVAILABLEVERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 
 # # https://docs.docker.com/install/linux/linux-postinstall/
 groupadd docker
-usermod -aG docker $USER
+usermod -aG docker ${USER}
 systemctl enable docker
 echo "Don't forget to log out and log back in so that your group membership is re-evaluated"
