b76d66cbfe5621dc0b745288d0b20bfbd91ffcae
Eric Nemchik <eric@nemchik.com>
2018-06-04 15:02:55 -0500

    Relocate architecture files

--- compose/generate-yml.sh
+++ compose/generate-yml.sh
@@ -20,42 +20,37 @@ while read -r line || [ -n "${line}" ]; do
     if [[ ${line} ==  *"_ENABLED=true" ]]; then
         APPNAME=${line/_ENABLED=true/}
         FILENAME=${APPNAME,,}
-        for file in ./.apps/*.override.yml; do
-            [[ -e ${file} ]] || break
-            if [[ ${file} =~ /${FILENAME}\.override\. ]]; then
-                echo "${file} \\" >> "${RUNFILE}"
+        if [[ -f ./.apps/${FILENAME}.override.yml ]]; then
+            echo "./.apps/${FILENAME}.override.yml \\" >> "${RUNFILE}"
+            echo "./.apps/${FILENAME}.override.yml has been included."
+        fi
+        if [[ ${ARCH} == "arm64" ]]; then
+            if [[ -f ./.apps/architecture/${FILENAME}.aarch64.yml ]] && [[ -f ./.apps/${FILENAME}.yml ]]; then
+                echo "./.apps/architecture/${FILENAME}.aarch64.yml \\" >> "${RUNFILE}"
+                echo "./.apps/${FILENAME}.yml \\" >> "${RUNFILE}"
+            elif [[ -f ./.apps/architecture/${FILENAME}.armhf.yml ]] && [[ -f ./.apps/${FILENAME}.yml ]]; then
+                echo "./.apps/architecture/${FILENAME}.armhf.yml \\" >> "${RUNFILE}"
+                echo "./.apps/${FILENAME}.yml \\" >> "${RUNFILE}"
+                echo "Missing aarch64 option (may not be available) falling back on armhf."
+            else
+                echo "Could not find ./.apps/${FILENAME}.yml and either ./.apps/architecture/${FILENAME}.aarch64.yml or ./.apps/architecture/${FILENAME}.armhf.yml"
             fi
-        done
-        for file in ./.apps/*.yml; do
-            [[ -e ${file} ]] || break
-            if [[ ${file} =~ /${FILENAME}\. ]]; then
-                if [[ ${ARCH} == "arm64" ]]; then
-                    if [[ -f ${file/\.apps\//.apps\/aarch64\/} ]]; then
-                        {
-                            echo "${file/\.apps\//.apps\/aarch64\/} \\"
-                            echo "${file} \\"
-                        } >> "${RUNFILE}"
-                    fi
-                    if [[ -f ${file/\.apps\//.apps\/armhf\/} ]]; then
-                        {
-                            echo "${file/\.apps\//.apps\/armhf\/} \\"
-                            echo "${file} \\"
-                        } >> "${RUNFILE}"
-                    fi
-                fi
-                if [[ ${ARCH} == "arm" ]]; then
-                    if [[ -f ${file/\.apps\//.apps\/armhf\/} ]]; then
-                        {
-                            echo "${file/\.apps\//.apps\/armhf\/} \\"
-                            echo "${file} \\"
-                        } >> "${RUNFILE}"
-                    fi
-                fi
-                if [[ ${ARCH} == "amd64" ]]; then
-                    echo "${file} \\" >> "${RUNFILE}"
-                fi
+        fi
+        if [[ ${ARCH} == "arm" ]]; then
+            if [[ -f ./.apps/architecture/${FILENAME}.armhf.yml ]] && [[ -f ./.apps/${FILENAME}.yml ]]; then
+                echo "./.apps/architecture/${FILENAME}.armhf.yml \\" >> "${RUNFILE}"
+                echo "./.apps/${FILENAME}.yml \\" >> "${RUNFILE}"
+            else
+                echo "Could not find ./.apps/${FILENAME}.yml and ./.apps/architecture/${FILENAME}.armhf.yml"
             fi
-        done
+        fi
+        if [[ ${ARCH} == "amd64" ]]; then
+            if [[ -f ./.apps/${FILENAME}.yml ]]; then
+                echo "./.apps/${FILENAME}.yml \\" >> "${RUNFILE}"
+            else
+                echo "Could not find ./.apps/${FILENAME}.yml"
+            fi
+        fi
     fi
 done <<< "${SCRIPT_VARS}"
 {
