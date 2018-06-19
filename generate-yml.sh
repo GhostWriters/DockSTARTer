f2fbbdefefe1a229ed1d8e73e52a6942b1327683
Eric Nemchik <eric@nemchik.com>
2018-06-19 11:43:27 -0500

    SCRIPTPATH adjustments

--- generate-yml.sh
+++ generate-yml.sh
@@ -3,6 +3,7 @@
 readonly SCRIPTNAME="$(basename "$0")"
 readonly SCRIPTPATH="$(readlink -m "$(dirname "$0")")"
 readonly ARGS="$*"
+echo "${SCRIPTPATH}"
 source "${SCRIPTPATH}/scripts/common.sh"
 
 run_script 'generate_yml';
