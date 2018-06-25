c5688799afbcb5629b009e15b7a307bb385087e5
Eric Nemchik <eric@nemchik.com>
2018-06-25 10:44:45 -0500

    Add logging functions

--- scripts/root_check.sh
+++ scripts/root_check.sh
@@ -5,8 +5,6 @@ IFS=$'\n\t'
 root_check() {
     if [[ ${EUID} -ne 0 ]]; then
         echo
-        echo -e "${RED}Please run as root using the command: ${ENDCOLOR}sudo bash ${SCRIPTNAME} ${ARGS}"
-        echo
-        exit 0
+        fatal "Please run as root using the command: sudo bash ${SCRIPTNAME} ${ARGS}"
     fi
 }
