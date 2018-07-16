0feba64e22c3d2d136a6fa2ff9e51d5f2f0d287a
Eric Nemchik <eric@nemchik.com>
2018-07-16 16:51:46 -0500

    Fix env_set to handle escaping

--- scripts/env_set.sh
+++ scripts/env_set.sh
@@ -9,5 +9,9 @@ env_set() {
     NEW_VAL=${2:-}
     local VAR_VAL
     VAR_VAL=$(grep "${SET_VAR}" "${SCRIPTPATH}/compose/.env" | xargs || fatal "Could not find ${SET_VAR} in ${SCRIPTPATH}/compose/.env")
-    sed -i "s/${VAR_VAL}/${SET_VAR}=${NEW_VAL}/" "${SCRIPTPATH}/compose/.env"
+    local SED_FIND
+    SED_FIND=$(echo "${VAR_VAL}" | sed -e 's/[\/&]/\\&/g')
+    local SED_REPLACE
+    SED_REPLACE=$(echo "${SET_VAR}=${NEW_VAL}" | sed -e 's/[\/&]/\\&/g')
+    sed -i "s/${SED_FIND}/${SED_REPLACE}/" "${SCRIPTPATH}/compose/.env"
 }
