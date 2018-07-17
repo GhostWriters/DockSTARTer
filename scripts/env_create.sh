3eac9b86a819663ae06a914d7516eaf705f0dc8f
Eric Nemchik <eric@nemchik.com>
2018-07-17 09:24:32 -0500

    Update env_create for GUI

--- scripts/env_create.sh
+++ scripts/env_create.sh
@@ -3,16 +3,19 @@ set -euo pipefail
 IFS=$'\n\t'
 
 env_create() {
+    local PROMPT
+    PROMPT=${1:-}
     if [[ -f ${SCRIPTPATH}/compose/.env ]]; then
         info "${SCRIPTPATH}/compose/.env found."
     else
         warning "${SCRIPTPATH}/compose/.env not found. Copying example template."
-        cp "${SCRIPTPATH}/compose/.env.example" "${SCRIPTPATH}/compose/.env"
-        info "Example template has been copied. You should exit the script and edit ${SCRIPTPATH}/compose/.env before continuing."
-        info "Exit now?"
+        cp "${SCRIPTPATH}/compose/.env.example" "${SCRIPTPATH}/compose/.env" || fatal "${SCRIPTPATH}/compose/.env could not be copied."
+        [[ ${PROMPT} != "menu" ]] && info "You should exit the script and edit ${SCRIPTPATH}/compose/.env before continuing. Exit now?"
         local YN
         while true; do
             if [[ ${CI:-} == true ]] && [[ ${TRAVIS:-} == true ]]; then
+                YN=N
+            elif [[ ${PROMPT} == "menu" ]]; then
                 YN=N
             else
                 read -rp "[Yn]" YN
@@ -31,6 +34,5 @@ env_create() {
                     ;;
             esac
         done
-        echo
     fi
 }
