e51597f5b0c222382f4a5935f777077baeb7df20
Eric Nemchik <eric@nemchik.com>
2018-07-17 18:32:14 -0500

    Permissions for Travis.

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -9,6 +9,11 @@ set_permissions() {
     CH_PUID=${2:-$DETECTED_PUID}
     local CH_PGID
     CH_PGID=${3:-$DETECTED_PGID}
+    if [[ ${CI:-} == true ]] && [[ ${TRAVIS:-} == true ]]; then
+        info "Overriding PUID and PGID for Travis."
+        CH_PUID=${DETECTED_UNAME}
+        CH_PGID=${DETECTED_UGROUP}
+    fi
     info "Taking ownership of ${CH_PATH} for user ${CH_PUID} and group ${CH_PGID}"
     chown -R "${CH_PUID}":"${CH_PGID}" "${CH_PATH}"
     info "Setting folder permissions in ${CH_PATH} to 755"
