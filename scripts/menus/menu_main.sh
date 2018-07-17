6fc988566573cbfded5fe9784a1f54b7c61c1770
Eric Nemchik <eric@nemchik.com>
2018-07-17 09:25:05 -0500

    Move ui processes to separate files

--- scripts/menus/menu_main.sh
+++ scripts/menus/menu_main.sh
@@ -19,52 +19,22 @@ menu_main() {
             --menu "What would you like to do?" \
             --fb --cancel-button "Exit" \
             ${LINES} ${COLUMNS} ${NETLINES} \
-            "Install/Reconfigure" "Setup and start applications" \
-            "Install/Update Dependencies" "Latest version of Docker and Docker-Compose" \
-            "Update DockStarter" "Get the latest version of DockSTARTer" 3>&1 1>&2 2>&3)
+            "Configure Apps" "Setup and start applications" \
+            "Install Dependencies" "Latest version of Docker and Docker-Compose" \
+        "Update DockStarter" "Get the latest version of DockSTARTer" 3>&1 1>&2 2>&3)
 
-    local EXITSTATUS
-    EXITSTATUS=${?}
-    if [[ ${EXITSTATUS} == 0 ]]; then
-        case "${MAINCHOICE}" in
-            "Install/Reconfigure" )
-                run_menu 'application_selector'
-                run_menu 'timezone_set'
-                run_menu 'user_group_set'
-                run_menu 'config_folder_set'
-                run_menu 'download_folder_set'
-                run_menu 'media_folders_set'
-                run_script 'generate_yml'
-                run_script 'run_compose' menu
-                ;;
-            "Install/Update Dependencies" )
-                run_script 'run_apt'
-                run_script 'install_yq' force
-                run_script 'install_docker' force
-                run_script 'install_machine_completion'
-                run_script 'install_compose' force
-                run_script 'install_compose_completion'
-                run_script 'setup_docker_group'
-                run_script 'enable_docker_systemd'
-                run_script 'request_reboot' menu
-                ;;
-            "Update DockStarter" )
-                run_script 'update_self' menu
-                run_script 'run_apt'
-                run_script 'install_yq' force
-                run_script 'install_docker' force
-                run_script 'install_machine_completion'
-                run_script 'install_compose' force
-                run_script 'install_compose_completion'
-                ;;
-            *)
-                error "Invalid Option"
-                ;; #TODO Exit safely
-        esac
-    else
-        #TODO Thanks script
-        echo
-        sleep 1
-        exit 0
-    fi
+    case "${MAINCHOICE}" in
+        "Configure Apps")
+            run_script 'ui_config'
+            ;;
+        "Install Dependencies")
+            run_script 'ui_install'
+            ;;
+        "Update DockStarter")
+            run_script 'ui_update'
+            ;;
+        *)
+            error "Invalid Option"
+            ;;
+    esac
 }
