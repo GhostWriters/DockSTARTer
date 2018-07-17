534991d593444d7f73f99d2c6fd489d7507ab753
Eric Nemchik <eric@nemchik.com>
2018-07-17 10:28:03 -0500

    App on/off handling for menu cleanup

--- scripts/menu_app_helper.sh
+++ scripts/menu_app_helper.sh
@@ -15,72 +15,105 @@ menu_app_helper() {
             APPONOFF="off"
         fi
         case "${APPNAME}" in
-            "bazarr")
-                SupportedAppDescr+=("Bazarr" "Companion to Sonarr to manage and download subtitles" "${APPONOFF}") ;;
-            "couchpotato")
-                SupportedAppDescr+=("Couchpotato" "Movies PVR Client" "${APPONOFF}") ;;
-            "deluge")
-                SupportedAppDescr+=("Deluge" "Lightweight, Full-featured BitTorrent client" "${APPONOFF}") ;;
-            "duckdns")
-                SupportedAppDescr+=("DuckDNS" "free service which will point a DNS to an IP of your choice" "${APPONOFF}") ;;
-            "duplicati")
-                SupportedAppDescr+=("Duplicati" "Backup software to store backups online with strong encryption" "${APPONOFF}") ;;
-            "emby")
-                SupportedAppDescr+=("Emby" "Organize, play, and stream audio and video" "${APPONOFF}") ;;
-            "headphones")
-                SupportedAppDescr+=("Headphones" "Music PVR Client" "${APPONOFF}") ;;
-            "homeassistant")
-                SupportedAppDescr+=("Home Assistant" "Track and control all devices at home and automate control" "${APPONOFF}") ;;
-            "hydra2")
-                SupportedAppDescr+=("Hydra2" "Meta search for NZB indexers" "${APPONOFF}") ;;
-            "jackett")
-                SupportedAppDescr+=("Jackett" "API Support for your favorite private trackers" "${APPONOFF}") ;;
-            "lazylibrarian")
-                SupportedAppDescr+=("Lazylibrarian" "Book PVR Client" "${APPONOFF}") ;;
-            "letsencrypt")
-                SupportedAppDescr+=("Letsencrypt" "Certificate authority that provides free X.509 certificates" "${APPONOFF}") ;;
-            "lidarr")
-                SupportedAppDescr+=("Lidarr" "Music download manager for Usenet and BitTorrent users" "${APPONOFF}") ;;
-            "logarr")
-                SupportedAppDescr+=("Logarr" "Self-hosted, single-page, log consolidation tool" "${APPONOFF}") ;;
-            "monitorr")
-                SupportedAppDescr+=("Monitorr" "Webfront to live display the status of any webapp or service" "${APPONOFF}") ;;
-            "muximux")
-                SupportedAppDescr+=("Muximux" "A lightweight way to manage your HTPC" "${APPONOFF}") ;;
-            "nzbget")
-                SupportedAppDescr+=("NZBGet" "NZB Newsgrabber / Downloader" "${APPONOFF}") ;;
-            "ombi")
-                SupportedAppDescr+=("Ombi" "Allow your users to Request Movies, TV Shows and Albums" "${APPONOFF}") ;;
-            "organizr")
-                SupportedAppDescr+=("Organizr" "HTPC/Homelab Services Organizer" "${APPONOFF}") ;;
-            "plex")
-                SupportedAppDescr+=("Plex" "Organizes all of your video, music and photo collections" "${APPONOFF}") ;;
-            "plexrequests")
-                SupportedAppDescr+=("Plex Requests" "Automated way for users to request new content for Plex" "${APPONOFF}") ;;
-            "portainer")
-                SupportedAppDescr+=("Portainer" "Simple management UI for Docker" "${APPONOFF}") ;;
-            "radarr")
-                SupportedAppDescr+=("Radarr" "Automatically download movies via Usenet and BitTorrent" "${APPONOFF}") ;;
-            "rutorrent")
-                SupportedAppDescr+=("ruTorrent" "Web front-end for rTorrent" "${APPONOFF}") ;;
-            "sabnzbd")
-                SupportedAppDescr+=("SABnzbd" "NZB Newsgrabber / Downloader" "${APPONOFF}") ;;
-            "sickrage")
-                SupportedAppDescr+=("Sickrage" "Automatic Video Library Manager for TV Shows" "${APPONOFF}") ;;
-            "sonarr")
-                SupportedAppDescr+=("Sonarr" "Smart TV show PVR via Usenet and BitTorrent" "${APPONOFF}") ;;
-            "syncthing")
-                SupportedAppDescr+=("Syncthing" "Open-source peer-to-peer file synchronization" "${APPONOFF}") ;;
-            "tautulli")
-                SupportedAppDescr+=("Tautulli" "Monitoring and tracking tool for Plex Media Server" "${APPONOFF}") ;;
-            "transmission")
-                SupportedAppDescr+=("Transmission" "Fast, easy, and free BitTorrent client" "${APPONOFF}") ;;
-            "unifi")
-                SupportedAppDescr+=("Unifi" "Controller software for wireless networks" "${APPONOFF}") ;;
-            "watchtower")
-                SupportedAppDescr+=("Watchtower" "Automatically update running Docker containers" "${APPONOFF}") ;;
+            "BAZARR")
+                SupportedAppDescr+=("Bazarr" "Companion to Sonarr to manage and download subtitles" "${APPONOFF}")
+                ;;
+            "COUCHPOTATO")
+                SupportedAppDescr+=("Couchpotato" "Movies PVR Client" "${APPONOFF}")
+                ;;
+            "DELUGE")
+                SupportedAppDescr+=("Deluge" "Lightweight, Full-featured BitTorrent client" "${APPONOFF}")
+                ;;
+            "DUCKDNS")
+                SupportedAppDescr+=("DuckDNS" "free service which will point a DNS to an IP of your choice" "${APPONOFF}")
+                ;;
+            "DUPLICATI")
+                SupportedAppDescr+=("Duplicati" "Backup software to store backups online with strong encryption" "${APPONOFF}")
+                ;;
+            "EMBY")
+                SupportedAppDescr+=("Emby" "Organize, play, and stream audio and video" "${APPONOFF}")
+                ;;
+            "HEADPHONES")
+                SupportedAppDescr+=("Headphones" "Music PVR Client" "${APPONOFF}")
+                ;;
+            "HOMEASSISTANT")
+                SupportedAppDescr+=("Home Assistant" "Track and control all devices at home and automate control" "${APPONOFF}")
+                ;;
+            "HYDRA2")
+                SupportedAppDescr+=("Hydra2" "Meta search for NZB indexers" "${APPONOFF}")
+                ;;
+            "JACKETT")
+                SupportedAppDescr+=("Jackett" "API Support for your favorite private trackers" "${APPONOFF}")
+                ;;
+            "LAZYLIBRARIAN")
+                SupportedAppDescr+=("Lazylibrarian" "Book PVR Client" "${APPONOFF}")
+                ;;
+            "LETSENCRYPT")
+                SupportedAppDescr+=("Letsencrypt" "Certificate authority that provides free X.509 certificates" "${APPONOFF}")
+                ;;
+            "LIDARR")
+                SupportedAppDescr+=("Lidarr" "Music download manager for Usenet and BitTorrent users" "${APPONOFF}")
+                ;;
+            "LOGARR")
+                SupportedAppDescr+=("Logarr" "Self-hosted, single-page, log consolidation tool" "${APPONOFF}")
+                ;;
+            "MONITORR")
+                SupportedAppDescr+=("Monitorr" "Webfront to live display the status of any webapp or service" "${APPONOFF}")
+                ;;
+            "MUXIMUX")
+                SupportedAppDescr+=("Muximux" "A lightweight way to manage your HTPC" "${APPONOFF}")
+                ;;
+            "NZBGET")
+                SupportedAppDescr+=("NZBGet" "NZB Newsgrabber / Downloader" "${APPONOFF}")
+                ;;
+            "OMBI")
+                SupportedAppDescr+=("Ombi" "Allow your users to Request Movies, TV Shows and Albums" "${APPONOFF}")
+                ;;
+            "ORGANIZR")
+                SupportedAppDescr+=("Organizr" "HTPC/Homelab Services Organizer" "${APPONOFF}")
+                ;;
+            "PLEX")
+                SupportedAppDescr+=("Plex" "Organizes all of your video, music and photo collections" "${APPONOFF}")
+                ;;
+            "PLEXREQUESTS")
+                SupportedAppDescr+=("Plex Requests" "Automated way for users to request new content for Plex" "${APPONOFF}")
+                ;;
+            "PORTAINER")
+                SupportedAppDescr+=("Portainer" "Simple management UI for Docker" "${APPONOFF}")
+                ;;
+            "RADARR")
+                SupportedAppDescr+=("Radarr" "Automatically download movies via Usenet and BitTorrent" "${APPONOFF}")
+                ;;
+            "RUTORRENT")
+                SupportedAppDescr+=("ruTorrent" "Web front-end for rTorrent" "${APPONOFF}")
+                ;;
+            "SABNZBD")
+                SupportedAppDescr+=("SABnzbd" "NZB Newsgrabber / Downloader" "${APPONOFF}")
+                ;;
+            "SICKRAGE")
+                SupportedAppDescr+=("Sickrage" "Automatic Video Library Manager for TV Shows" "${APPONOFF}")
+                ;;
+            "SONARR")
+                SupportedAppDescr+=("Sonarr" "Smart TV show PVR via Usenet and BitTorrent" "${APPONOFF}")
+                ;;
+            "SYNCTHING")
+                SupportedAppDescr+=("Syncthing" "Open-source peer-to-peer file synchronization" "${APPONOFF}")
+                ;;
+            "TAUTULLI")
+                SupportedAppDescr+=("Tautulli" "Monitoring and tracking tool for Plex Media Server" "${APPONOFF}")
+                ;;
+            "TRANSMISSION")
+                SupportedAppDescr+=("Transmission" "Fast, easy, and free BitTorrent client" "${APPONOFF}")
+                ;;
+            "UNIFI")
+                SupportedAppDescr+=("Unifi" "Controller software for wireless networks" "${APPONOFF}")
+                ;;
+            "WATCHTOWER")
+                SupportedAppDescr+=("Watchtower" "Automatically update running Docker containers" "${APPONOFF}")
+                ;;
             *)
-                error "ERROR ${APPNAME} APP DESCRIPTION NOT FOUND"
+                warning "WARNING ${APPNAME} APP DESCRIPTION NOT FOUND"
+                SupportedAppDescr+=("${APPNAME}" "Description not found" "${APPONOFF}")
                 ;;
         esac
     }
@@ -89,20 +122,25 @@ menu_app_helper() {
     while IFS= read -r line; do
         local APPNAME
         APPNAME=${line/_ENABLED=*/}
-        APPNAME="${APPNAME,,}"
-        if [[ ${ARCH} == "arm64" ]]; then
-            if [[ -f ${SCRIPTPATH}/compose/.apps/${APPNAME}/${APPNAME}.arm64.yml ]]; then
-                GetMenuItem "${APPNAME}"
-            elif [[ -f ${SCRIPTPATH}/compose/.apps/${APPNAME}/${APPNAME}.armhf.yml ]]; then
-                GetMenuItem "${APPNAME}"
-            fi
-        elif [[ ${ARCH} == "armhf" ]]; then
-            if [[ -f ${SCRIPTPATH}/compose/.apps/${APPNAME}/${APPNAME}.armhf.yml ]]; then
-                GetMenuItem "${APPNAME}"
-            fi
-        else
-            if [[ -f ${SCRIPTPATH}/compose/.apps/${APPNAME}/${APPNAME}.yml ]]; then
-                GetMenuItem "${APPNAME}"
+        local FILENAME
+        FILENAME=${APPNAME,,}
+        if [[ -d ${SCRIPTPATH}/compose/.apps/${FILENAME}/ ]]; then
+            if [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.yml ]]; then
+                if [[ ${ARCH} == "arm64" ]]; then
+                    if [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.arm64.yml ]]; then
+                        GetMenuItem "${APPNAME}"
+                    elif [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.armhf.yml ]]; then
+                        GetMenuItem "${APPNAME}"
+                    fi
+                fi
+                if [[ ${ARCH} == "armhf" ]]; then
+                    if [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.armhf.yml ]]; then
+                        GetMenuItem "${APPNAME}"
+                    fi
+                fi
+                if [[ ${ARCH} == "amd64" ]]; then
+                    GetMenuItem "${APPNAME}"
+                fi
             fi
         fi
     done < <(grep '_ENABLED=' < "${SCRIPTPATH}/compose/.env")
