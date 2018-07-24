5239ca2532fb2c34e505581b3ed3070e9563477c
Tom <tom.eley@gmail.com>
2018-07-24 18:36:42 +0100

    GUI - Check .env file for set params

--- scripts/menus/download_folder_set.sh
+++ scripts/menus/download_folder_set.sh
@@ -3,13 +3,18 @@ set -euo pipefail
 IFS=$'\n\t'
 
 download_folder_set() {
+
+    local ENVDOWNLOADSDIR
+    ENVDOWNLOADSDIR=$(run_script 'env_get' 'DOWNLOADSDIR')
+    ENVDOWNLOADSDIR="${ENVDOWNLOADSDIR:-"${DETECTED_HOMEDIR}/Downloads"}"
+
     if [[ ${CI:-} != true ]] && [[ ${TRAVIS:-} != true ]] && (whiptail --title "Dowloads Location" --fb --yesno \
-            "The default place for download files is: ${DETECTED_HOMEDIR}/Downloads\\nThis will be passed into the applications.\\n\\nWould you like to accept this?" 12 78); then
+            "The default place for download files is:\\n${ENVDOWNLOADSDIR}\\n\\nThis will be passed into the applications.\\n\\nWould you like to accept this?" 14 78); then
         reset || true
         #TODO - Should we check if the folder exists?
         #TODO - Should we set permissions on the folder?
-        run_script 'env_set' 'DOWNLOADSDIR' "${DETECTED_HOMEDIR}/Downloads"
+        run_script 'env_set' 'DOWNLOADSDIR' "${ENVDOWNLOADSDIR}"
     else
-        run_menu 'input_prompt' 'DOWNLOADSDIR' "${DETECTED_HOMEDIR}/Downloads"
+        run_menu 'input_prompt' 'DOWNLOADSDIR' "${ENVDOWNLOADSDIR}"
     fi
 }
