06251f7ae7f99a6562ab5e53d08181ae63b652bf
Eric Nemchik <eric@nemchik.com>
2018-08-24 15:53:02 -0500

    Use uname -m instead of dpkg to detect arch

--- main.sh
+++ main.sh
@@ -13,19 +13,17 @@ readonly RED='\e[31m'
 readonly YLW='\e[33m'
 readonly ENDCOLOR='\e[0m'
 
+# # Log Functions
+readonly LOG_FILE="/tmp/dockstarter.log"
+info()    { echo -e "$(date +"%F %T") ${BLU}[INFO]${ENDCOLOR}       $*" | tee -a "${LOG_FILE}" >&2 ; }
+warning() { echo -e "$(date +"%F %T") ${YLW}[WARNING]${ENDCOLOR}    $*" | tee -a "${LOG_FILE}" >&2 ; }
+error()   { echo -e "$(date +"%F %T") ${RED}[ERROR]${ENDCOLOR}      $*" | tee -a "${LOG_FILE}" >&2 ; }
+fatal()   { echo -e "$(date +"%F %T") ${RED}[FATAL]${ENDCOLOR}      $*" | tee -a "${LOG_FILE}" >&2 ; exit 1 ; }
+
 # # Check Arch
-readonly ARCH=$(dpkg --print-architecture)
-
-# # Check Systemd
-if [[ -L "/sbin/init" ]]; then
-    readonly ISSYSTEMD=true
-else
-    readonly ISSYSTEMD=false
-fi
-
-# # Github Token for Travis CI
-if [[ ${CI:-} == true ]] && [[ ${TRAVIS:-} == true ]] && [[ ${TRAVIS_SECURE_ENV_VARS} == true ]]; then
-    readonly GH_HEADER="Authorization: token ${GH_TOKEN}"
+readonly ARCH=$(uname -m)
+if [[ ${ARCH} != "aarch64" ]] && [[ ${ARCH} != "armv7l" ]] && [[ ${ARCH} != "x86_64" ]]; then
+    fatal "Unsupported architecture."
 fi
 
 # # User/Group Information
@@ -35,12 +33,17 @@ readonly DETECTED_PGID=$(id -g "${DETECTED_PUID}" 2> /dev/null || true)
 readonly DETECTED_UGROUP=$(id -gn "${DETECTED_PUID}" 2> /dev/null || true)
 readonly DETECTED_HOMEDIR=$(eval echo "~${DETECTED_UNAME}" 2> /dev/null || true)
 
-# # Log Functions
-readonly LOG_FILE="/tmp/dockstarter.log"
-info()    { echo -e "$(date +"%F %T") ${BLU}[INFO]${ENDCOLOR}       $*" | tee -a "${LOG_FILE}" >&2 ; }
-warning() { echo -e "$(date +"%F %T") ${YLW}[WARNING]${ENDCOLOR}    $*" | tee -a "${LOG_FILE}" >&2 ; }
-error()   { echo -e "$(date +"%F %T") ${RED}[ERROR]${ENDCOLOR}      $*" | tee -a "${LOG_FILE}" >&2 ; }
-fatal()   { echo -e "$(date +"%F %T") ${RED}[FATAL]${ENDCOLOR}      $*" | tee -a "${LOG_FILE}" >&2 ; exit 1 ; }
+# # Github Token for Travis CI
+if [[ ${CI:-} == true ]] && [[ ${TRAVIS:-} == true ]] && [[ ${TRAVIS_SECURE_ENV_VARS} == true ]]; then
+    readonly GH_HEADER="Authorization: token ${GH_TOKEN}"
+fi
+
+# # Check Systemd
+if [[ -L "/sbin/init" ]]; then
+    readonly ISSYSTEMD=true
+else
+    readonly ISSYSTEMD=false
+fi
 
 # # Usage Information
 #/ usage: main.sh options
--- scripts/install_compose.sh
+++ scripts/install_compose.sh
@@ -12,7 +12,7 @@ install_compose() {
     FORCE=${1:-}
     if [[ "${AVAILABLE_COMPOSE}" != "${INSTALLED_COMPOSE}" ]] || [[ -n ${FORCE} ]]; then
         info "Installing latest compose."
-        if [[ ${ARCH} == "arm64" ]] || [[ ${ARCH} == "armhf" ]]; then
+        if [[ ${ARCH} == "aarch64" ]] || [[ ${ARCH} == "armv7l" ]]; then
             #TODO remove the next line...
             info "ARM architecture detected. Please let us know on Gitter chat if this works for you!"
             apt-get remove docker-compose > /dev/null 2>&1
@@ -20,7 +20,7 @@ install_compose() {
             pip uninstall docker-py > /dev/null 2>&1 || true
             pip install -U docker-compose > /dev/null 2>&1
         fi
-        if [[ ${ARCH} == "amd64" ]]; then
+        if [[ ${ARCH} == "x86_64" ]]; then
             curl -H "${GH_HEADER:-}" -L "https://github.com/docker/compose/releases/download/${AVAILABLE_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1
             chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1 || true
         fi
