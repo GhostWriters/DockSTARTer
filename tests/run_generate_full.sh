ae54fc5fce6a6b6abfa72ca907c5743bb4d9f5d4
Robert Baker <se7enalive@gmail.com>
2018-09-30 16:48:05 -0700

    fix: adding port redirects to full test

--- tests/run_generate_full.sh
+++ tests/run_generate_full.sh
@@ -23,6 +23,8 @@ run_generate_full() {
     run_script 'env_set' "SABNZBDVPN_PORT_8080" "38080"
     run_script 'env_set' "SICKRAGE_PORT_8081" "28081"
     run_script 'env_set' "THELOUNGE_PORT_9000" "9002"
+    run_script 'env_set' "TRAEFIK_PORT_80" "1080"
+    run_script 'env_set' "TRAEFIK_PORT_443" "1443"
     run_script 'env_set' "TRAEFIK_PORT_8080" "58080"
     run_script 'env_set' "TRANSMISSION_PORT_51413" "51414"
     run_script 'env_set' "TRANSMISSION_PORT_9091" "19091"
