e93067a61f90bda9c7e31393128905ad6ba40b1c
Eric Nemchik <eric@nemchik.com>
2018-11-16 11:43:53 -0600

    Rewrite shellcheck validation

--- .tests/validate_shellcheck.sh
+++ .tests/validate_shellcheck.sh
@@ -3,28 +3,30 @@ set -euo pipefail
 IFS=$'\n\t'
 
 validate_shellcheck() {
+    info "Installing shellcheck."
     apt-get -y install xz-utils > /dev/null 2>&1 || fatal "Failed to install shellcheck dependencies from apt."
     export scversion="stable" # or "v0.4.7", or "latest"
     wget "https://storage.googleapis.com/shellcheck/shellcheck-${scversion}.linux.x86_64.tar.xz" > /dev/null 2>&1 || fatal "Failed to download shellcheck."
-    tar --xz -xvf shellcheck-"${scversion}".linux.x86_64.tar.xz > /dev/null 2>&1 || fatal "Failed to extract shellcheck."
-    cp shellcheck-"${scversion}"/shellcheck /usr/bin/ || fatal "Failed to copy shellcheck to bin."
+    tar --xz -xvf "shellcheck-${scversion}.linux.x86_64.tar.xz" > /dev/null 2>&1 || fatal "Failed to extract shellcheck."
+    cp "shellcheck-${scversion}/shellcheck" /usr/bin/ || fatal "Failed to copy shellcheck to bin."
+    rm -rf "shellcheck-${scversion}.linux.x86_64.tar.xz" "shellcheck-${scversion}/shellcheck" || true
 
     shellcheck --version || fatal "Failed to check shellcheck version."
 
-    # Check for Shellcheck errors in the code.
-    local SCERRORS
-    SCERRORS=$(find . -name '*.sh' -print0 | xargs -0 shellcheck -e SC1090 -e SC1091 -e SC2034 || true)
-    if [[ -n ${SCERRORS} ]]; then
-        find . -name '*.sh' -print0 | xargs -0 shellcheck -e SC2034
-        fatal "Shellcheck validation failure."
-    fi
+    local VALIDATOR
+    VALIDATOR=shellcheck
+    local VALIDATIONFLAGS
+    VALIDATIONFLAGS="-x"
 
-    # Search for ShellCheck Warnings in all the scripts and fail if it finds any
-    #    local SCDISABLED
-    #    SCDISABLED=$(grep -r '^# shellcheck disable' "${SCRIPTPATH}" | grep -c 'shellcheck disable')
-    #    if [[ ${SCDISABLED} -gt 0 ]]; then
-    #        grep -rn "${SCRIPTPATH}" -e '^# shellcheck disable'
-    #        fatal "Shellcheck validation failure."
-    #    fi
-    info "Shellcheck validation complete."
+    # https://github.com/caarlos0/shell-ci-build
+    echo "Linting all executables and .*sh files with ${VALIDATOR}..."
+    while IFS= read -r line; do
+        if grep -q -E -w "sh|bash|dash|ksh" "${line}"; then
+            eval "${VALIDATOR} ${VALIDATIONFLAGS} ${SCRIPTPATH}/${line}" || fatal "Linting ${line}"
+            info "Linting ${line}"
+        else
+            info "Skipping ${line}..."
+        fi
+    done < <(git ls-tree -r HEAD | grep -E '^1007|.*\..*sh$' | awk '{print $4}')
+    info "${VALIDATOR} validation complete."
 }
