f9611a2a19dae70303eaba81a717499730ef1dd6
Eric Nemchik <eric@nemchik.com>
2018-11-21 17:06:24 -0600

    Use docker containers for all validations

--- .tests/validate_shfmt.sh
+++ .tests/validate_shfmt.sh
@@ -3,22 +3,14 @@ set -euo pipefail
 IFS=$'\n\t'
 
 validate_shfmt() {
-    info "Installing shfmt."
-    local AVAILABLE_SHFMT
-    AVAILABLE_SHFMT=$(curl -H "${GH_HEADER:-}" -s "https://api.github.com/repos/mvdan/sh/releases/latest" | grep -Po '"tag_name": "[Vv]?\K.*?(?=")')
-    # For some reason this isn't playing nice with Travis CI when the GH_HEADER is included.
-    #curl -H "${GH_HEADER:-}" -L "https://github.com/mvdan/sh/releases/download/v${AVAILABLE_SHFMT}/shfmt_v${AVAILABLE_SHFMT}_$(uname -s | sed -e 's/\(.*\)/\L\1/')_amd64" -o /usr/local/bin/shfmt > /dev/null 2>&1 || fatal "Failed to install shfmt."
-    curl -L "https://github.com/mvdan/sh/releases/download/v${AVAILABLE_SHFMT}/shfmt_v${AVAILABLE_SHFMT}_$(uname -s | sed -e 's/\(.*\)/\L\1/')_amd64" -o /usr/local/bin/shfmt > /dev/null 2>&1 || fatal "Failed to install shfmt."
-    chmod +x /usr/local/bin/shfmt > /dev/null 2>&1 || true
-
-    hash -r || true
-
-    shfmt --version || fatal "Failed to check shfmt version."
-
     local VALIDATOR
-    VALIDATOR=shfmt
+    VALIDATOR="docker run --rm -v ${SCRIPTPATH}:${SCRIPTPATH} jamesmstone/shfmt"
     local VALIDATIONFLAGS
     VALIDATIONFLAGS="-s -i 4 -ci -sr -d"
+    local VALIDATORVERFLAG
+    VALIDATORVERFLAG="--version"
+
+    eval "${VALIDATOR} ${VALIDATORVERFLAG}" || fatal "Failed to check ${VALIDATOR} version."
 
     # https://github.com/caarlos0/shell-ci-build
     info "Linting all executables and .*sh files with ${VALIDATOR}..."
