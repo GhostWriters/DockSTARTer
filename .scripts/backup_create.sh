a9212da5eead73942e479bbf594fd3711748481c
Eric Nemchik <eric@nemchik.com>
2019-01-07 14:59:32 -0600

    Backup script code cleanup

--- .scripts/backup_create.sh
+++ .scripts/backup_create.sh
@@ -36,7 +36,7 @@ backup_create() {
     DU=$(run_script 'env_get' BACKUP_DU)
 
     # ------------- initialization -----------------------------------------
-    shopt -s extglob #enable extended pattern matching operators
+    shopt -s extglob # enable extended pattern matching operators
 
     local OPTION
     OPTION="--stats \
@@ -82,7 +82,7 @@ backup_create() {
     local f
     while IFS= read -r b; do
         a=$((b / 2 + 1))
-        f=0 #this flag is set to 1 when we find the 1st snapshot in the range b..a
+        f=0 # this flag is set to 1 when we find the 1st snapshot in the range b..a
         for i in $(seq -f'%03g' "${b}" -1 "${a}"); do
             if [ -d "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${i}" ]; then
                 if [ "${f}" -eq 0 ]; then
@@ -111,13 +111,13 @@ backup_create() {
                 if [ ! -h "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last" ] && [ -d "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${i}" ]; then
                     ln -s "${SNAPSHOT_NAME}.${i}" "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last" || warning "Failed to create link for ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last"
                 fi
-                d=0 #disk space used by snapshots and free disk space are ok
+                d=0 # disk space used by snapshots and free disk space are ok
                 info "Checking free disk space..."
                 FREEDISK=$(df -m "${SNAPSHOT_DST}" | tail -1 | sed -e 's/  */ /g' | cut -d" " -f4 | sed -e 's/M*//g')
                 info "${FREEDISK} MiB free."
                 if [ "${FREEDISK}" -ge "${MIN_MIBSIZE2}" ]; then
                     info "Ok, bigger than ${MIN_MIBSIZE2} MiB."
-                    if [ "${DU}" -eq 0 ]; then #avoid slow 'du'
+                    if [ "${DU}" -eq 0 ]; then # avoid slow 'du'
                         break
                     else
                         info "Checking disk space used by ${SNAPSHOT_DST} ..."
@@ -127,13 +127,13 @@ backup_create() {
                             info "Ok, smaller than ${MAX_MIBSIZE2} MiB."
                             break
                         else
-                            d=2 #disk space used by snapshots is too big
+                            d=2 # disk space used by snapshots is too big
                         fi
                     fi
                 else
-                    d=1 #free disk space is too small
+                    d=1 # free disk space is too small
                 fi
-                if [ ${d} -ne 0 ]; then #we need to remove snapshots
+                if [ ${d} -ne 0 ]; then # we need to remove snapshots
                     if [ "${i}" -ne 1 ]; then
                         info "Removing ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${i} ..."
                         if [ "${CHATTR}" -eq 1 ]; then
@@ -143,9 +143,9 @@ backup_create() {
                         if [ -h "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last" ]; then
                             rm -f "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last" || error "Failed to remove ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last"
                         fi
-                    else #all snapshots except snapshot.001 are removed
-                        if [ ${d} -eq 1 ]; then #snapshot.001 causes that free space is too small
-                            if [ "${OVERWRITE_LAST}" -eq 1 ]; then #last chance: remove snapshot.001 and retry once
+                    else # all snapshots except snapshot.001 are removed
+                        if [ ${d} -eq 1 ]; then # snapshot.001 causes that free space is too small
+                            if [ "${OVERWRITE_LAST}" -eq 1 ]; then # last chance: remove snapshot.001 and retry once
                                 OVERWRITE_LAST=0
                                 warning "Warning, free disk space will be smaller than ${MIN_MIBSIZE} MiB."
                                 info "OVERWRITE_LAST enabled. Removing ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.001 ..."
@@ -156,7 +156,7 @@ backup_create() {
                             else
                                 for j in ${LNKDST//--link-dest=/}; do
                                     if [ -d "${j}" ] && [ "${CHATTR}" -eq 1 ] && [ "$(lsattr -d "${j}" | cut -b5)" != "i" ]; then
-                                        chattr -R +i "${j}" > /dev/null 2>&1 || warning "Failed to make ${j} backup immutable. Backup files may be overwritten by future backups." #undo unprotection that was needed to use hardlinks
+                                        chattr -R +i "${j}" > /dev/null 2>&1 || warning "Failed to make ${j} backup immutable. Backup files may be overwritten by future backups." # undo unprotection that was needed to use hardlinks
                                     fi
                                     if [ ! -h "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last" ]; then
                                         ln -s "${SNAPSHOT_NAME}.${j}" "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last" || warning "Failed to create link for ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.last"
@@ -164,7 +164,7 @@ backup_create() {
                                 done
                                 fatal "Sorry, free disk space will be smaller than ${MIN_MIBSIZE} MiB. Exiting..."
                             fi
-                        elif [ ${d} -eq 2 ]; then #snapshot.001 causes that disk space used by snapshots is too big
+                        elif [ ${d} -eq 2 ]; then # snapshot.001 causes that disk space used by snapshots is too big
                             warning "Warning, disk space used by ${SNAPSHOT_DST} will be bigger than ${MAX_MIBSIZE} MiB. Continuing anyway..."
                         fi
                     fi
@@ -177,7 +177,7 @@ backup_create() {
     local LNKDST
     local OOVERWRITE_LAST
     local LOG
-    while :; do #this loop is executed a 2nd time if OVERWRITE_LAST was ==1 and snapshot.001 got removed
+    while :; do # this loop is executed a 2nd time if OVERWRITE_LAST was ==1 and snapshot.001 got removed
         OOVERWRITE_LAST="${OVERWRITE_LAST}"
         info "Testing needed free disk space ..."
         mkdir -p "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.test-free-disk-space" || fatal "Failed to create ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.test-free-disk-space"
@@ -186,7 +186,7 @@ backup_create() {
         LNKDST=$(find "${SNAPSHOT_DST}/" -maxdepth 2 -type d -name "${SNAPSHOT_NAME}.001" -printf " --link-dest=%p")
         for i in ${LNKDST//--link-dest=/}; do
             if [ -d "${i}" ] && [ "${CHATTR}" -eq 1 ] && [ "$(lsattr -d "${i}" | cut -b5)" == "i" ]; then
-                chattr -R -i "${i}" > /dev/null 2>&1 || warning "Failed to remove immutable flag from ${i}" #unprotect last snapshots to use hardlinks
+                chattr -R -i "${i}" > /dev/null 2>&1 || warning "Failed to remove immutable flag from ${i}" # unprotect last snapshots to use hardlinks
             fi
         done
         eval rsync \
@@ -199,7 +199,7 @@ backup_create() {
         info "${i} MiB needed."
         rm -rf "${LOG}" "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.test-free-disk-space" || fatal "Failed to remove ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.test-free-disk-space"
         remove_snapshot $((MIN_MIBSIZE + i)) $((MAX_MIBSIZE - i))
-        if [ "${OOVERWRITE_LAST}" == "${OVERWRITE_LAST}" ]; then #no need to retry
+        if [ "${OOVERWRITE_LAST}" == "${OVERWRITE_LAST}" ]; then # no need to retry
             break
         fi
     done
@@ -228,7 +228,7 @@ backup_create() {
         "${SNAPSHOT_SRC}" "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.000" > /dev/null 2>&1 || fatal "Snapshot failed."
     for i in ${LNKDST//--link-dest=/}; do
         if [ -d "${i}" ] && [ "${CHATTR}" -eq 1 ] && [ "$(lsattr -d "${i}" | cut -b5)" != "i" ]; then
-            chattr -R +i "${i}" > /dev/null 2>&1 || warning "Failed to make ${i} backup immutable. Backup files may be overwritten by future backups." #undo unprotection that was needed to use hardlinks
+            chattr -R +i "${i}" > /dev/null 2>&1 || warning "Failed to make ${i} backup immutable. Backup files may be overwritten by future backups." # undo unprotection that was needed to use hardlinks
         fi
     done
 
@@ -240,7 +240,7 @@ backup_create() {
     fi
 
     # rotate the backups
-    if [ -d "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${BACKUP_RETENTION_MAX}" ]; then #remove snapshot.${BACKUP_RETENTION_MAX}
+    if [ -d "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${BACKUP_RETENTION_MAX}" ]; then # remove snapshot.${BACKUP_RETENTION_MAX}
         info "Removing ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${BACKUP_RETENTION_MAX} ..."
         if [ "${CHATTR}" -eq 1 ]; then
             chattr -R -i "${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${BACKUP_RETENTION_MAX}" > /dev/null 2>&1 || warning "Failed to remove immutable flag from ${SNAPSHOT_DST}/${SNAPSHOT_NAME}.${BACKUP_RETENTION_MAX}"
@@ -270,8 +270,8 @@ backup_create() {
     done
 
     # remove additional backups if free disk space is short
-    OVERWRITE_LAST=0 #next call of remove_snapshot() will not remove snapshot.001
-    remove_snapshot ${MIN_MIBSIZE} ${MAX_MIBSIZE}
+    OVERWRITE_LAST=0 # next call of remove_snapshot() will not remove snapshot.001
+    remove_snapshot "${MIN_MIBSIZE}" "${MAX_MIBSIZE}"
     run_script 'set_permissions' "${SNAPSHOT_DST}" "${PUID}" "${PGID}"
     info "Snapshot backup successfully done in $(($(date +%s) - STARTDATE)) sec."
 }
