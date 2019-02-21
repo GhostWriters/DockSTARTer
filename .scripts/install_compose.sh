43bef04ef3a27494905ec10a593e82b766094d87
Eric Nemchik <eric@nemchik.com>
2019-02-21 16:53:31 -0600

    Use better version checking

--- .scripts/install_compose.sh
+++ .scripts/install_compose.sh
@@ -7,10 +7,10 @@ install_compose() {
     local AVAILABLE_COMPOSE
     AVAILABLE_COMPOSE=$(curl -H "${GH_HEADER:-}" -s "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "[Vv]?\K.*?(?=")') || fatal "Failed to check latest available docker-compose version."
     local INSTALLED_COMPOSE
-    INSTALLED_COMPOSE=$( (docker-compose --version 2> /dev/null || true) | sed -E 's/.* version ([^,]*)(, build .*)?/\1/')
+    INSTALLED_COMPOSE=$( (docker-compose --version 2> /dev/null || echo "0") | sed -E 's/.* version ([^,]*)(, build .*)?/\1/')
     local FORCE
     FORCE=${1:-}
-    if [[ ${AVAILABLE_COMPOSE} != "${INSTALLED_COMPOSE}" ]] || [[ -n ${FORCE} ]]; then
+    if vergt "${AVAILABLE_COMPOSE}" "${INSTALLED_COMPOSE}" || [[ -n ${FORCE} ]]; then
         info "Removing old docker-compose."
         rm /usr/local/bin/docker-compose > /dev/null 2>&1 || true
         rm /usr/bin/docker-compose > /dev/null 2>&1 || true
@@ -22,8 +22,8 @@ install_compose() {
         pip install -IUq docker-compose > /dev/null 2>&1 || fatal "Failed to install docker-compose from pip."
 
         local UPDATED_COMPOSE
-        UPDATED_COMPOSE=$( (docker-compose --version 2> /dev/null || true) | sed -E 's/.* version ([^,]*)(, build .*)?/\1/')
-        if [[ ${AVAILABLE_COMPOSE} != "${UPDATED_COMPOSE}" ]]; then
+        UPDATED_COMPOSE=$( (docker-compose --version 2> /dev/null || echo "0") | sed -E 's/.* version ([^,]*)(, build .*)?/\1/')
+        if vergt "${AVAILABLE_COMPOSE}" "${UPDATED_COMPOSE}"; then
             fatal "Failed to install the latest docker-compose."
         fi
     fi
