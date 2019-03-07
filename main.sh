07b2e61b3335d674c3e6123380688ad582a0b9c4
Eric Nemchik <eric@nemchik.com>
2019-03-07 11:58:18 -0600

    Fix usage

--- main.sh
+++ main.sh
@@ -36,7 +36,7 @@ IFS=$'\n\t'
 #/      debug
 #/
 usage() {
-    grep -Po '^#/\K.*' "${SCRIPTNAME}" | xargs || echo "Failed to display usage information."
+    grep -Po '^#/\K.*' "${SCRIPTNAME}" | xargs -d \n || echo "Failed to display usage information."
     exit
 }
 
