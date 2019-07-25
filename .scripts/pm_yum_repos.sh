3f59973b73ce6fafe437bc69dee75785702b28cf
Eric Nemchik <eric@nemchik.com>
2019-07-25 14:33:08 -0500

    Add prompt to show output of yum IUS install

--- .scripts/pm_yum_repos.sh
+++ .scripts/pm_yum_repos.sh
@@ -5,12 +5,16 @@ IFS=$'\n\t'
 pm_yum_repos() {
     info "Installing EPEL and IUS repositories."
     local GET_IUS
-    GET_IUS=$(mktemp) || fatal "Failed to create temporary storage for IUS repo install."
+    GET_IUS=$(mktemp) || fatal "Failed to create temporary IUS repo install script."
     info "Downloading IUS install script."
     curl -fsSL setup.ius.io -o "${GET_IUS}" > /dev/null 2>&1 || fatal "Failed to get IUS install script."
     info "Running IUS install script."
-    bash "${GET_IUS}" > /dev/null 2>&1 || warn "Failed to install IUS."
-    rm -f "${GET_IUS}" || warn "Temporary setup.ius.io file could not be removed."
+    local REDIRECT="> /dev/null 2>&1"
+    if [[ -n ${VERBOSE:-} ]] || run_script 'question_prompt' "${PROMPT:-}" N "Would you like to display the command output?"; then
+        REDIRECT=""
+    fi
+    eval bash "${GET_IUS}" "${REDIRECT}" || warn "Failed to install IUS."
+    rm -f "${GET_IUS}" || warn "Failed to remove temporary IUS repo install script."
 }
 
 test_pm_yum_repos() {
