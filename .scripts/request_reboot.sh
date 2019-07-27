88de9eb60d2d165c7fe1544f55f8e8f87d5ed3ec
Eric Nemchik <eric@nemchik.com>
2019-07-27 08:08:11 -0500

    Do not use associative arrays for colors They caused issues with sudo crontab tasks such as backups

--- .scripts/request_reboot.sh
+++ .scripts/request_reboot.sh
@@ -6,7 +6,7 @@ request_reboot() {
     notice "Your system may need to reboot for changes to take effect."
     warn "If this is your first run reboot is required."
     warn "Failure to reboot on first run can cause errors with other operations."
-    notice "Please run ${F[Y]:-}sudo reboot${NC:-} manually."
+    notice "Please run ${F_Y:-}sudo reboot${NC:-} manually."
     notice "If this is not your first run you may disregard this message."
 }
 
