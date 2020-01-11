f35320b839953a4301d2017185bd0518772f6d58
Eric Nemchik <eric@nemchik.com>
2020-01-10 19:59:36 -0600

    Update to yq-go v3

--- .scripts/appvars_create.sh
+++ .scripts/appvars_create.sh
@@ -8,7 +8,7 @@ appvars_create() {
     local FILENAME=${APPNAME,,}
     while IFS= read -r line; do
         local VAR_LABEL
-        VAR_LABEL=$(echo "${line}" | grep --color=never -Po "^com\.dockstarter\.appvars\.\K[\w]+" || true)
+        VAR_LABEL=$(echo "${line}" | grep --color=never -Po "^[\w]+" || true)
         if [[ -z ${VAR_LABEL} ]]; then
             continue
         fi
@@ -18,12 +18,12 @@ appvars_create() {
             continue
         else
             local DEFAULT_VAL
-            DEFAULT_VAL=$(run_script 'yml_get' "${APPNAME}" "services.${FILENAME}.labels[com.dockstarter.appvars.${VAR_LABEL}]" || true)
+            DEFAULT_VAL=$(run_script 'yml_get' "${APPNAME}" "services.${FILENAME}.labels.com.dockstarter.appvars.${VAR_LABEL}" || true)
             echo "${SET_VAR}=" >> "${SCRIPTPATH}/compose/.env"
             run_script 'env_set' "${SET_VAR}" "${DEFAULT_VAL}"
         fi
 
-    done < <(run_script 'yml_get' "${APPNAME}" "services.${FILENAME}.labels" || error "Unable to find labels for ${APPNAME}")
+    done < <(run_script 'yml_get' "${APPNAME}" "services.${FILENAME}.labels.com.dockstarter.appvars" || error "Unable to find labels for ${APPNAME}")
     run_script 'env_set' "${APPNAME}_ENABLED" true
 }
 
