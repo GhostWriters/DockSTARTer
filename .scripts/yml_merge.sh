cfc7a1714e55d8921ab7d4dbee43234f33740ae9
Eric Nemchik <eric@nemchik.com>
2020-05-03 11:37:41 -0500

    Quote paths in yml merge

--- .scripts/yml_merge.sh
+++ .scripts/yml_merge.sh
@@ -10,9 +10,9 @@ yml_merge() {
     RUNFILE=$(mktemp) || fatal "Failed to create temporary yml merge script."
     echo "#!/usr/bin/env bash" > "${RUNFILE}"
     {
-        echo '/usr/local/bin/yq-go m '\\
-        echo "${SCRIPTPATH}/compose/.reqs/v1.yml \\"
-        echo "${SCRIPTPATH}/compose/.reqs/v2.yml \\"
+        echo "/usr/local/bin/yq-go m \\"
+        echo "\"${SCRIPTPATH}/compose/.reqs/v1.yml\" \\"
+        echo "\"${SCRIPTPATH}/compose/.reqs/v2.yml\" \\"
     } >> "${RUNFILE}"
     info "Required files included."
     while IFS= read -r line; do
@@ -28,7 +28,7 @@ yml_merge() {
                     continue
                 fi
                 if [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.${ARCH}.yml ]]; then
-                    echo "${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.${ARCH}.yml \\" >> "${RUNFILE}"
+                    echo "\"${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.${ARCH}.yml\" \\" >> "${RUNFILE}"
                 else
                     error "Failed to include ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.${ARCH}.yml file."
                     continue
@@ -37,23 +37,23 @@ yml_merge() {
                 APPNETMODE=$(run_script 'env_get' "${APPNAME}_NETWORK_MODE")
                 if [[ -z ${APPNETMODE} ]] || [[ ${APPNETMODE} == "bridge" ]]; then
                     if [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.hostname.yml ]]; then
-                        echo "${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.hostname.yml \\" >> "${RUNFILE}"
+                        echo "\"${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.hostname.yml\" \\" >> "${RUNFILE}"
                     else
                         warn "Failed to include ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.hostname.yml file."
                     fi
                     if [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.ports.yml ]]; then
-                        echo "${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.ports.yml \\" >> "${RUNFILE}"
+                        echo "\"${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.ports.yml\" \\" >> "${RUNFILE}"
                     else
                         warn "Failed to include ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.ports.yml file."
                     fi
                 elif [[ -n ${APPNETMODE} ]]; then
                     if [[ -f ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.netmode.yml ]]; then
-                        echo "${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.netmode.yml \\" >> "${RUNFILE}"
+                        echo "\"${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.netmode.yml\" \\" >> "${RUNFILE}"
                     else
                         warn "Failed to include ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.netmode.yml file."
                     fi
                 fi
-                echo "${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.yml \\" >> "${RUNFILE}"
+                echo "\"${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.yml\" \\" >> "${RUNFILE}"
                 info "All configurations for ${APPNAME} are included."
             else
                 warn "Failed to include ${SCRIPTPATH}/compose/.apps/${FILENAME}/${FILENAME}.yml file."
@@ -62,7 +62,7 @@ yml_merge() {
             error "Failed to include ${SCRIPTPATH}/compose/.apps/${FILENAME}/ directory."
         fi
     done < <(grep '_ENABLED=true$' < "${SCRIPTPATH}/compose/.env")
-    echo "> ${SCRIPTPATH}/compose/docker-compose.yml" >> "${RUNFILE}"
+    echo "> \"${SCRIPTPATH}/compose/docker-compose.yml\"" >> "${RUNFILE}"
     run_script 'install_yq'
     info "Running compiled script to merge docker-compose.yml file."
     bash "${RUNFILE}" > /dev/null 2>&1 || fatal "Failed to run yml merge script."
