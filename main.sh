553d53e9d4c498984d270ee162a22cf16568cb5c
Eric Nemchik <eric@nemchik.com>
2020-05-20 19:59:02 -0500

    Workaround SC2034 https://github.com/GhostWriters/DockSTARTer/issues/915

--- main.sh
+++ main.sh
@@ -195,7 +195,7 @@ fi
 # Github Token for Travis CI
 if [[ ${CI:-} == true ]] && [[ ${TRAVIS_SECURE_ENV_VARS:-} == true ]]; then
     readonly GH_HEADER="Authorization: token ${GH_TOKEN}"
-    echo "${GH_HEADER}" > /dev/null 2>&1 || true # Ridiculous workaround for SC2034 where the variable is used in other files called by this script
+    echo "${GH_HEADER}" > /dev/null 2>&1 || true # Workaround SC2034
 fi
 
 # Script Information
@@ -218,7 +218,9 @@ readonly SCRIPTNAME="${SCRIPTPATH}/$(basename "$(get_scriptname)")"
 readonly DETECTED_PUID=${SUDO_UID:-$UID}
 readonly DETECTED_UNAME=$(id -un "${DETECTED_PUID}" 2> /dev/null || true)
 readonly DETECTED_PGID=$(id -g "${DETECTED_PUID}" 2> /dev/null || true)
+echo "${DETECTED_PGID}" > /dev/null 2>&1 || true # Workaround SC2034
 readonly DETECTED_UGROUP=$(id -gn "${DETECTED_PUID}" 2> /dev/null || true)
+echo "${DETECTED_UGROUP}" > /dev/null 2>&1 || true # Workaround SC2034
 readonly DETECTED_HOMEDIR=$(eval echo "~${DETECTED_UNAME}" 2> /dev/null || true)
 
 # Terminal Colors
