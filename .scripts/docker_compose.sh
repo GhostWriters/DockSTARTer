ddbcbc515dedebf72ffd4633009e03581ac0f3fd
Eric Nemchik <eric@nemchik.com>
2020-06-02 15:33:12 -0500

    Fix ds -c

--- .scripts/docker_compose.sh
+++ .scripts/docker_compose.sh
@@ -5,7 +5,10 @@ IFS=$'\n\t'
 docker_compose() {
     local COMPOSEINPUT=${1:-}
     local COMMAND=${COMPOSEINPUT%% *}
-    local APPNAME=${COMPOSEINPUT#* }
+    local APPNAME
+    if [[ ${COMPOSEINPUT} == *" "* ]]; then
+        APPNAME=${COMPOSEINPUT#* }
+    fi
     local COMPOSECOMMAND
     local COMMANDINFO
     case ${COMMAND} in
@@ -14,15 +17,15 @@ docker_compose() {
             COMMANDINFO="Stopping and removing containers, networks, volumes, and images created by DockSTARTer."
             ;;
         pull)
-            COMPOSECOMMAND="pull --include-deps ${APPNAME}"
+            COMPOSECOMMAND="pull --include-deps ${APPNAME:-}"
             COMMANDINFO="Pulling the latest images for ${APPNAME:-all enabled services}."
             ;;
         restart)
-            COMPOSECOMMAND="restart ${APPNAME}"
+            COMPOSECOMMAND="restart ${APPNAME:-}"
             COMMANDINFO="Restarting ${APPNAME:-all stopped and running services}."
             ;;
         up)
-            COMPOSECOMMAND="up -d --remove-orphans ${APPNAME}"
+            COMPOSECOMMAND="up -d --remove-orphans ${APPNAME:-}"
             COMMANDINFO="Creating ${APPNAME:-containers for all enabled services}."
             ;;
         *)
