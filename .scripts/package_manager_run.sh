ee0a1f1e98bbbe9545b2c5ed63eef82c71988c12
Dylan M. Taylor <dylan@dylanmtaylor.com>
2020-08-27 16:36:52 -0400

    WIP: Don't fail on Arch linux

--- .scripts/package_manager_run.sh
+++ .scripts/package_manager_run.sh
@@ -10,6 +10,9 @@ package_manager_run() {
         run_script "pm_dnf_${ACTION}"
     elif [[ -n "$(command -v yum)" ]]; then
         run_script "pm_yum_${ACTION}"
+    elif [[ -n "$(command -v pacman)" ]]; then
+        warn "Arch Linux based distributions are not officially supported."
+        warn "You will need to install docker and docker-compose manually."
     else
         fatal "Package manager not detected!"
     fi
