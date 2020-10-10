154eb1d75e4e1cf2beefb0642a6e99c412e9a759
Eric Nemchik <eric@nemchik.com>
2020-10-09 23:51:06 -0500

    Shell fixes

--- .scripts/yml_get.sh
+++ .scripts/yml_get.sh
@@ -7,12 +7,12 @@ yml_get() {
     local GET_VAR=${2:-}
     local FILENAME=${APPNAME,,}
     run_script 'install_yq'
-    yq -y -s 'reduce .[] as $item ({}; . * $item)' \
-        "${SCRIPTPATH}"/compose/.apps/"${FILENAME}"/*.yml 2> /dev/null | \
-        yq -r ".${GET_VAR}" 2> /dev/null || \
+    yq -y -s "reduce .[] as \$item ({}; . * \$item)" \
+        "${SCRIPTPATH}"/compose/.apps/"${FILENAME}"/*.yml 2> /dev/null |
+        yq -y -r ".${GET_VAR}" 2> /dev/null ||
         return 1
 }
 
 test_yml_get() {
-    run_script 'yml_get' WATCHTOWER "services.watchtower.labels[\"com.dockstarter.appinfo.nicename\"]"
+    run_script 'yml_get' WATCHTOWER 'services.watchtower.labels["com.dockstarter.appinfo.nicename"]'
 }
