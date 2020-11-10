fe1fa306a2eb107031a5977472756c5fcb36c8ef
Eric Nemchik <eric@nemchik.com>
2020-11-10 07:55:54 -0600

    Add -r back to yq

--- .scripts/yml_get.sh
+++ .scripts/yml_get.sh
@@ -9,7 +9,7 @@ yml_get() {
     run_script 'install_yq'
     yq -y -s 'reduce .[] as '\$'item ({}; . * '\$'item)' \
         "${SCRIPTPATH}"/compose/.apps/"${FILENAME}"/*.yml 2> /dev/null |
-        yq -y ".${GET_VAR}" 2> /dev/null |
+        yq -y -r ".${GET_VAR}" 2> /dev/null |
         sed -E '$s/^\.\.\.$//;/^$/d' ||
         return 1
 }
