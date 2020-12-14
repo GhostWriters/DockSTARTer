6c56fe87c2accae4e46e861e4b3bd144be822a7a
Eric Nemchik <eric@nemchik.com>
2020-12-14 11:11:08 -0600

    Revert "workaround not using url"
    This reverts commit c24337fa441368b9feaadf60d4233ed29c892175.

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -3,7 +3,6 @@ script.src = "https://www.googletagmanager.com/gtag/js?id=G-QVNLHE95FS";
 document.head.appendChild(script);
 
 app.location$.subscribe(function (url) {
-  url = url; // workaround not using url
   window.dataLayer = window.dataLayer || [];
   function gtag() {
     dataLayer.push(arguments);
