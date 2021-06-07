fc0ce8ea1e07f0abcb8b3c3fd92baa2ede33e1d2
Eric Nemchik <eric@nemchik.com>
2021-06-07 17:14:07 -0500

    Adjust apt repo handling

--- .scripts/pm_apt_install.sh
+++ .scripts/pm_apt_install.sh
@@ -8,7 +8,7 @@ pm_apt_install() {
     if [[ -n ${VERBOSE:-} ]] || run_script 'question_prompt' "${PROMPT:-CLI}" N "Would you like to display the command output?"; then
         REDIRECT=""
     fi
-    eval apt-get -y install apt-transport-https curl git grep sed whiptail "${REDIRECT}" || fatal "Failed to install dependencies from apt.\nFailing command: ${F[C]}apt-get -y install apt-transport-https curl git grep sed whiptail"
+    eval apt-get -y install curl git grep sed whiptail "${REDIRECT}" || fatal "Failed to install dependencies from apt.\nFailing command: ${F[C]}apt-get -y install curl git grep sed whiptail"
 }
 
 test_pm_apt_install() {
