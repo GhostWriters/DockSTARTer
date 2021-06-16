08aa6250e15546b45110a130b2c69641ebbe4124
Eric Nemchik <eric@nemchik.com>
2021-06-16 08:06:38 -0500

    Update update_check.sh

--- .scripts/update_check.sh
+++ .scripts/update_check.sh
@@ -6,29 +6,31 @@ update_check() {
     log "Checking for script updates."
     local REPO
     REPO="https://github.com/GhostWriters/DockSTARTer"
+    cd "${SCRIPTPATH}" || fatal "Failed to change directory.\nFailing command: ${F[C]}cd \"${SCRIPTPATH}\""
     local BRANCH
-    BRANCH=$(git -C "${SCRIPTPATH}/${SCRIPTNAME}" branch --show-current)
+    BRANCH=$(git branch --show-current)
 
-    git -C "${SCRIPTPATH}/${SCRIPTNAME}" fetch origin "${BRANCH}" -q
+    info "Fetching recent changes from git."
+    git fetch --all --prune > /dev/null 2>&1 || fatal "Failed to fetch recent changes from git.\nFailing command: ${F[C]}git fetch --all --prune"
 
     local LOCAL_COMMIT
-    LOCAL_COMMIT=$(git -C "${SCRIPTPATH}/${SCRIPTNAME}" rev-parse @)
+    LOCAL_COMMIT=$(git rev-parse @)
     local LOCAL_DIFF
-    LOCAL_DIFF=$(git -C "${SCRIPTPATH}/${SCRIPTNAME}" diff)
+    LOCAL_DIFF=$(git diff)
     local REMOTE_BRANCH
-    REMOTE_BRANCH=$(git -C "${SCRIPTPATH}/${SCRIPTNAME}" rev-parse --abbrev-ref --symbolic-full-name '@{u}')
+    REMOTE_BRANCH=$(git rev-parse --abbrev-ref --symbolic-full-name '@{u}')
     local REMOTE_DIFF
-    REMOTE_DIFF=$(git -C "${SCRIPTPATH}/${SCRIPTNAME}" diff "${LOCAL_COMMIT}" "${REMOTE_BRANCH}")
+    REMOTE_DIFF=$(git diff "${LOCAL_COMMIT}" "${REMOTE_BRANCH}")
 
     if [ "${LOCAL_DIFF}" != "" ]; then
         error "Your local files have changes that may conflict with performing an update. Please resolve this manually."
     elif [ "${REMOTE_DIFF}" != "" ]; then
-        warn "New changes are available for ${BRANCH} script."
+        warn "New changes are available for ${BRANCH} branch."
         warn "Review the changes: ${REPO%/}/compare/${LOCAL_COMMIT}...${BRANCH}"
         read -r -p "Press any key to continue." || fatal "Update canceled. Exiting."
         run_script 'update_self' "${BRANCH}"
     else
-        info "${BRANCH} script is up to date."
+        info "${BRANCH} branch is up to date."
     fi
 }
 
