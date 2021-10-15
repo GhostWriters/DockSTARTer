a85fe68822c1b62a7c7641ee42d0c3eddb1ec612
Eric Nemchik <eric@nemchik.com>
2021-10-14 22:21:09 -0500

    Use perl grep

--- .scripts/env_get.sh
+++ .scripts/env_get.sh
@@ -6,7 +6,7 @@ env_get() {
     local GET_VAR=${1:-}
     local VAR_FILE=${2:-$SCRIPTPATH/compose/.env}
     eval "${GET_VAR}=\"\""
-    eval "$(grep --color=never -E "^${GET_VAR}=.*" "${VAR_FILE}" || true)"
+    eval "$(grep --color=never -P "^${GET_VAR}=.*" "${VAR_FILE}" || true)"
     echo "${!GET_VAR}" || true
 }
 
