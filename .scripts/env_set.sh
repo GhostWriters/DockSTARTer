867eb43d9552ce27843e8849e0f90034b39b7c83
Eric Nemchik <eric@nemchik.com>
2021-10-14 23:32:51 -0500

    Attempt to preserve quotes with spaces

--- .scripts/env_set.sh
+++ .scripts/env_set.sh
@@ -14,9 +14,9 @@ env_set() {
     local SED_REPLACE
     SED_REPLACE=$(sed 's/[&/\]/\\&/g' <<< "${SET_VAR}=${NEW_VAL}")
     if [[ "${NEW_VAL}" =~ " " ]]; then
-        SED_REPLACE=$(sed 's/[&/\]/\\&/g' <<< "${SET_VAR}=${NEW_VAL@Q}")
+        SED_REPLACE=$(sed 's/[&/\]/\\&/g' <<< "${SET_VAR}=\"${NEW_VAL}\"")
     fi
-    sed -i "s/^${SED_FIND}$/${SED_REPLACE@Q}/" "${VAR_FILE}" || fatal "Failed to set ${SED_REPLACE}\nFailing command: ${F[C]}sed -i \"s/^${SED_FIND}$/${SED_REPLACE}/\" \"${VAR_FILE}\""
+    sed -i "s/^${SED_FIND}$/${SED_REPLACE}/" "${VAR_FILE}" || fatal "Failed to set ${SED_REPLACE}\nFailing command: ${F[C]}sed -i \"s/^${SED_FIND}$/${SED_REPLACE}/\" \"${VAR_FILE}\""
 }
 
 test_env_set() {
