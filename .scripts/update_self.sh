407a44c662a83fc2050ecb6ff2468feab3dcd0d3
Eric Nemchik <eric@nemchik.com>
2022-04-15 14:29:32 -0500

    Run all git commands as user

--- .scripts/update_self.sh
+++ .scripts/update_self.sh
@@ -11,22 +11,18 @@ update_self() {
         return 1
     fi
     cd "${SCRIPTPATH}" || fatal "Failed to change directory.\nFailing command: ${F[C]}cd \"${SCRIPTPATH}\""
-    info "Setting file ownership on current repository files"
-    git ls-tree -r --name-only HEAD | xargs chown "${DETECTED_PUID}":"${DETECTED_PGID}" > /dev/null 2>&1 || true
-    chown -R "${DETECTED_PUID}":"${DETECTED_PGID}" "${SCRIPTPATH}/.git" > /dev/null 2>&1 || true
-    chown "${DETECTED_PUID}":"${DETECTED_PGID}" "${SCRIPTPATH}" > /dev/null 2>&1 || true
     info "Fetching recent changes from git."
-    git fetch --all --prune > /dev/null 2>&1 || fatal "Failed to fetch recent changes from git.\nFailing command: ${F[C]}git fetch --all --prune"
+    sudo -H -u "${DETECTED_UNAME}" bash -c 'git fetch --all --prune' > /dev/null 2>&1 || fatal "Failed to fetch recent changes from git.\nFailing command: ${F[C]}git fetch --all --prune"
     if [[ ${CI:-} != true ]]; then
         info "Resetting to ${BRANCH}."
-        git reset --hard "${BRANCH}" > /dev/null 2>&1 || fatal "Failed to reset to ${BRANCH}.\nFailing command: ${F[C]}git reset --hard \"${BRANCH}\""
+        sudo -H -u "${DETECTED_UNAME}" bash -c 'git reset --hard "${BRANCH}"' > /dev/null 2>&1 || fatal "Failed to reset to ${BRANCH}.\nFailing command: ${F[C]}git reset --hard \"${BRANCH}\""
         info "Pulling recent changes from git."
-        git pull > /dev/null 2>&1 || fatal "Failed to pull recent changes from git.\nFailing command: ${F[C]}git pull"
+        sudo -H -u "${DETECTED_UNAME}" bash -c 'git pull' > /dev/null 2>&1 || fatal "Failed to pull recent changes from git.\nFailing command: ${F[C]}git pull"
     fi
     info "Cleaning up unnecessary files and optimizing the local repository."
-    git gc > /dev/null 2>&1 || true
+    sudo -H -u "${DETECTED_UNAME}" bash -c 'git gc' > /dev/null 2>&1 || true
     info "Setting file ownership on new repository files"
-    git ls-tree -r --name-only "${BRANCH}" | xargs chown "${DETECTED_PUID}":"${DETECTED_PGID}" > /dev/null 2>&1 || true
+    sudo -H -u "${DETECTED_UNAME}" bash -c 'git ls-tree -r --name-only HEAD' | xargs chown "${DETECTED_PUID}":"${DETECTED_PGID}" > /dev/null 2>&1 || true
     chown -R "${DETECTED_PUID}":"${DETECTED_PGID}" "${SCRIPTPATH}/.git" > /dev/null 2>&1 || true
     chown "${DETECTED_PUID}":"${DETECTED_PGID}" "${SCRIPTPATH}" > /dev/null 2>&1 || true
     exec bash "${SCRIPTNAME}" -e
