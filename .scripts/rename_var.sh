6ecf0a7fa21e1315a6cc072c018931b50a8cdf59
Eric Nemchik <eric@nemchik.com>
2024-02-19 17:31:35 -0600

    fix: :bug: only rename vars when FROMVAR exists
    Signed-off-by: Eric Nemchik <eric@nemchik.com>

--- .scripts/rename_var.sh
+++ .scripts/rename_var.sh
@@ -5,8 +5,10 @@ IFS=$'\n\t'
 rename_var() {
     local FROMVAR=${1-}
     local TOVAR=${2-}
-    notice "Renaming ${FROMVAR^^} to ${TOVAR^^} in ${COMPOSE_ENV} file."
-    sed -i "s/^${FROMVAR^^}=/${TOVAR^^}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${FROMVAR^^} to ${TOVAR^^}\nFailing command: ${F[C]}sed -i \"s/^${FROMVAR^^}=/${TOVAR^^}=/\" \"${COMPOSE_ENV}\""
+    if grep -q -P "^${FROMVAR^^}=" "${COMPOSE_ENV}"; then
+        notice "Renaming ${FROMVAR^^} to ${TOVAR^^} in ${COMPOSE_ENV} file."
+        sed -i "s/^${FROMVAR^^}=/${TOVAR^^}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${FROMVAR^^} to ${TOVAR^^}\nFailing command: ${F[C]}sed -i \"s/^${FROMVAR^^}=/${TOVAR^^}=/\" \"${COMPOSE_ENV}\""
+    fi
 }
 
 test_rename_var() {
