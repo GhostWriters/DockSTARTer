a83c926697c656e3d94411a4f1736d9f3527f3d4
Dylan M. Taylor <dylan@dylanmtaylor.com>
2024-02-20 14:35:31 -0500

    Attempt to make DockStarter work with an immutable filesystem

--- .scripts/symlink_ds.sh
+++ .scripts/symlink_ds.sh
@@ -5,25 +5,39 @@ IFS=$'\n\t'
 symlink_ds() {
     run_script 'set_permissions' "${SCRIPTNAME}"
 
-    # /usr/bin/ds
-    if [[ -L "/usr/bin/ds" ]] && [[ ${SCRIPTNAME} != "$(readlink -f /usr/bin/ds)" ]]; then
-        info "Attempting to remove /usr/bin/ds symlink."
-        sudo rm -f "/usr/bin/ds" || fatal "Failed to remove file.\nFailing command: ${F[C]}sudo rm -f \"/usr/bin/ds\""
-    fi
-    if [[ ! -L "/usr/bin/ds" ]]; then
-        info "Creating /usr/bin/ds symbolic link for DockSTARTer."
-        sudo ln -s -T "${SCRIPTNAME}" /usr/bin/ds || fatal "Failed to create symlink.\nFailing command: ${F[C]}sudo ln -s -T \"${SCRIPTNAME}\" /usr/bin/ds"
+    if findmnt -n /usr | egrep "^ro,|,ro,|,ro$"; then
+        echo "Read only /usr filesystem detected. Symlinks will be created in $HOME/bin instead."
+         # $HOME/bin/ds
+        if [[ -L "$HOME/bin/ds" ]] && [[ ${SCRIPTNAME} != "$(readlink -f $HOME/bin/ds)" ]]; then
+            info "Attempting to remove $HOME/bin/ds symlink."
+            sudo rm -f "$HOME/bin/ds" || fatal "Failed to remove file.\nFailing command: ${F[C]}sudo rm -f \"$HOME/bin/ds\""
+        fi
+        if [[ ! -L "$HOME/bin/ds" ]]; then
+            info "Creating $HOME/bin/ds symbolic link for DockSTARTer."
+            sudo ln -s -T "${SCRIPTNAME}" /usr/bin/ds || fatal "Failed to create symlink.\nFailing command: ${F[C]}sudo ln -s -T \"${SCRIPTNAME}\" $HOME/bin/ds"
+        fi
+    else
+        # /usr/bin/ds
+        if [[ -L "/usr/bin/ds" ]] && [[ ${SCRIPTNAME} != "$(readlink -f /usr/bin/ds)" ]]; then
+            info "Attempting to remove /usr/bin/ds symlink."
+            sudo rm -f "/usr/bin/ds" || fatal "Failed to remove file.\nFailing command: ${F[C]}sudo rm -f \"/usr/bin/ds\""
+        fi
+        if [[ ! -L "/usr/bin/ds" ]]; then
+            info "Creating /usr/bin/ds symbolic link for DockSTARTer."
+            sudo ln -s -T "${SCRIPTNAME}" /usr/bin/ds || fatal "Failed to create symlink.\nFailing command: ${F[C]}sudo ln -s -T \"${SCRIPTNAME}\" /usr/bin/ds"
+        fi
+    
+        # /usr/local/bin/ds
+        if [[ -L "/usr/local/bin/ds" ]] && [[ ${SCRIPTNAME} != "$(readlink -f /usr/local/bin/ds)" ]]; then
+            info "Attempting to remove /usr/local/bin/ds symlink."
+            sudo rm -f "/usr/local/bin/ds" || fatal "Failed to remove file.\nFailing command: ${F[C]}sudo rm -f \"/usr/local/bin/ds\""
+        fi
+        if [[ ! -L "/usr/local/bin/ds" ]]; then
+            info "Creating /usr/local/bin/ds symbolic link for DockSTARTer."
+            sudo ln -s -T "${SCRIPTNAME}" /usr/local/bin/ds || fatal "Failed to create symlink.\nFailing command: ${F[C]}sudo ln -s -T \"${SCRIPTNAME}\" /usr/local/bin/ds"
+        fi
     fi
 
-    # /usr/local/bin/ds
-    if [[ -L "/usr/local/bin/ds" ]] && [[ ${SCRIPTNAME} != "$(readlink -f /usr/local/bin/ds)" ]]; then
-        info "Attempting to remove /usr/local/bin/ds symlink."
-        sudo rm -f "/usr/local/bin/ds" || fatal "Failed to remove file.\nFailing command: ${F[C]}sudo rm -f \"/usr/local/bin/ds\""
-    fi
-    if [[ ! -L "/usr/local/bin/ds" ]]; then
-        info "Creating /usr/local/bin/ds symbolic link for DockSTARTer."
-        sudo ln -s -T "${SCRIPTNAME}" /usr/local/bin/ds || fatal "Failed to create symlink.\nFailing command: ${F[C]}sudo ln -s -T \"${SCRIPTNAME}\" /usr/local/bin/ds"
-    fi
 }
 
 test_symlink_ds() {
