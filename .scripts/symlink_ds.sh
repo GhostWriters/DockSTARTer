8c5c21ded5686468e23b2f3ce827bc34309b97f6
Eric Nemchik <eric@nemchik.com>
2024-02-22 18:52:12 -0600

    refactor: :recycle: symlink adjustments
    Signed-off-by: Eric Nemchik <eric@nemchik.com>

--- .scripts/symlink_ds.sh
+++ .scripts/symlink_ds.sh
@@ -5,24 +5,24 @@ IFS=$'\n\t'
 symlink_ds() {
     run_script 'set_permissions' "${SCRIPTNAME}"
 
+    local SYMLINK_TARGETS=("/usr/bin/ds" "/usr/local/bin/ds")
+
     if findmnt -n /usr | grep "ro" > /dev/null; then
-        if [[ $PATH != *"$HOME/bin"* ]]; then
-            warn "Read only /usr filesystem detected. Symlinks will be created in $HOME/bin. You will need to add this to your path."
+        if [[ $PATH != *"${HOME}/bin"* ]]; then
+            warn "Read only /usr filesystem detected. Symlinks will be created in ${HOME}/bin. You will need to add this to your path."
         fi
-        mkdir -p "$HOME/bin" # Make sure the path exists.
-        DS_SYMLINK_TARGETS=("$HOME/bin")
-    else
-        DS_SYMLINK_TARGETS=("/usr/bin/ds" "/usr/local/bin/ds")
+        mkdir -p "${HOME}/bin" # Make sure the path exists.
+        SYMLINK_TARGETS=("${HOME}/bin")
     fi
 
-    for DS_SYMLINK_TARGET in "${DS_SYMLINK_TARGETS[@]}"; do
-        if [[ -L ${DS_SYMLINK_TARGET} ]] && [[ ${SCRIPTNAME} != "$(readlink -f "${DS_SYMLINK_TARGET}")" ]]; then
-            info "Attempting to remove ${DS_SYMLINK_TARGET} symlink."
-            sudo rm -f "${DS_SYMLINK_TARGET}" || fatal "Failed to remove file.\nFailing command: ${F[C]}sudo rm -f \"${DS_SYMLINK_TARGET}\""
+    for SYMLINK_TARGET in "${SYMLINK_TARGETS[@]}"; do
+        if [[ -L ${SYMLINK_TARGET} ]] && [[ ${SCRIPTNAME} != "$(readlink -f "${SYMLINK_TARGET}")" ]]; then
+            info "Attempting to remove ${SYMLINK_TARGET} symlink."
+            sudo rm -f "${SYMLINK_TARGET}" || fatal "Failed to remove file.\nFailing command: ${F[C]}sudo rm -f \"${SYMLINK_TARGET}\""
         fi
-        if [[ ! -L ${DS_SYMLINK_TARGET} ]]; then
-            info "Creating ${DS_SYMLINK_TARGET} symbolic link for DockSTARTer."
-            sudo ln -s -T "${SCRIPTNAME}" "${DS_SYMLINK_TARGET}" || fatal "Failed to create symlink.\nFailing command: ${F[C]}sudo ln -s -T \"${SCRIPTNAME}\" \"${DS_SYMLINK_TARGET}\""
+        if [[ ! -L ${SYMLINK_TARGET} ]]; then
+            info "Creating ${SYMLINK_TARGET} symbolic link for DockSTARTer."
+            sudo ln -s -T "${SCRIPTNAME}" "${SYMLINK_TARGET}" || fatal "Failed to create symlink.\nFailing command: ${F[C]}sudo ln -s -T \"${SCRIPTNAME}\" \"${SYMLINK_TARGET}\""
         fi
     done
 }
