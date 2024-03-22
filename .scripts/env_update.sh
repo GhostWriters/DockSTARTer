dd8ef14f351e778bbcc7bc842c3f2be29ea23393
CLHatch <CLHatch@gmail.com>
2024-03-21 23:48:51 -0500

    Update env_update.sh
    Add `# APPNAME` and `# APPNAME (User Defined)` headings to .env file.
    
    Change logic to files into arrays and search the arrays instead of reading line by line.

--- .scripts/env_update.sh
+++ .scripts/env_update.sh
@@ -9,28 +9,91 @@ env_update() {
     local MKTEMP_ENV_CURRENT
     MKTEMP_ENV_CURRENT=$(mktemp) || fatal "Failed to create temporary current .env file.\nFailing command: ${F[C]}mktemp"
     sort "${COMPOSE_ENV}" > "${MKTEMP_ENV_CURRENT}" || fatal "Failed to sort to new file.\nFailing command: ${F[C]}sort \"${COMPOSE_ENV}\" > \"${MKTEMP_ENV_CURRENT}\""
+    local ARRAY_ENV_CURRENT=()
+    mapfile -t ARRAY_ENV_CURRENT < <(grep -v '^#' "${MKTEMP_ENV_CURRENT}" | grep '=')
     local MKTEMP_ENV_UPDATED
     MKTEMP_ENV_UPDATED=$(mktemp) || fatal "Failed to create temporary update .env file.\nFailing command: ${F[C]}mktemp"
     cp "${COMPOSE_ENV}.example" "${MKTEMP_ENV_UPDATED}" || fatal "Failed to copy file.\nFailing command: ${F[C]}cp \"${COMPOSE_ENV}.example\" \"${MKTEMP_ENV_UPDATED}\""
     info "Merging current values into updated .env file."
-    while IFS= read -r line; do
-        local LAST_APPNAME
-        local VAR_VAL=${line}
-        local SET_VAR=${VAR_VAL%%=*}
-        local APPNAME=${SET_VAR%%_*}
-        local SET_VAL
-        SET_VAL=$(run_script 'env_get' "${SET_VAR}" "${MKTEMP_ENV_CURRENT}")
-        if ! grep -q -P "^${SET_VAR}=" "${MKTEMP_ENV_UPDATED}"; then
-            if [ -z "${LAST_APPNAME-}" ] || [ "${APPNAME}" != "${LAST_APPNAME}" ]; then
-                echo "#" >> "${MKTEMP_ENV_UPDATED}" || error "# could not be written to ${MKTEMP_ENV_UPDATED}"
-                echo "# ${APPNAME}" >> "${MKTEMP_ENV_UPDATED}" || error "# ${APPNAME} could not be written to ${MKTEMP_ENV_UPDATED}"
-                echo "#" >> "${MKTEMP_ENV_UPDATED}" || error "# could not be written to ${MKTEMP_ENV_UPDATED}"
+    local BUILTIN_APPS=()
+    local ENABLED_LINES=()
+    local INSTALLED_APPS=()
+    local ENABLED_APPS=()
+    local APPTEMPLATESFOLDER="${SCRIPTPATH}/compose/.apps"
+    mapfile -t BUILTIN_APPS < <(ls -1d ${APPTEMPLATESFOLDER}/* | xargs -l basename)
+    mapfile -t ENABLED_LINES < <(grep --color=never -P '^[A-Z0-9]\w+_ENABLED=' "${MKTEMP_ENV_CURRENT}")
+    for line in "${ENABLED_LINES[@]}"; do
+        local VAR=${line%%=*}
+        local APPNAME=${VAR%%_*}
+        if [[ ${BUILTIN_APPS[@]^^} =~ "${APPNAME}" ]]; then
+            INSTALLED_APPS+=("${APPNAME}")
+            if $(run_script 'env_get' "${VAR}" "${MKTEMP_ENV_CURRENT}") = 'true'; then
+                ENABLED_APPS+=("${APPNAME}")
             fi
-            echo "${VAR_VAL}" >> "${MKTEMP_ENV_UPDATED}" || error "${VAR_VAL} could not be written to ${MKTEMP_ENV_UPDATED}"
         fi
-        run_script 'env_set' "${SET_VAR}" "${SET_VAL}" "${MKTEMP_ENV_UPDATED}"
+    done
+    
+    while [[ ! -z "${ARRAY_ENV_CURRENT[@]}" ]]; do
+        local ENV_USER_DEFINED_LINES=()
+        local ENV_BUILTIN_LINES=()
+        local APPNAME
+        local LAST_APPNAME
+        for index in "${!ARRAY_ENV_CURRENT[@]}"; do
+            local -a APP_LABEL_LIST
+            local line="${ARRAY_ENV_CURRENT[$index]}"
+            local SET_VAR=${line%%=*}
+            APPNAME=${SET_VAR%%_*}
+            if [ "${APPNAME}" != "${LAST_APPNAME-}" ]; then
+                break
+            fi
+            if grep -q -P "^${SET_VAR}=" "${MKTEMP_ENV_UPDATED}"; then
+                # Variable already exists
+                local SET_VAL=$(run_script 'env_get' "${SET_VAR}" "${MKTEMP_ENV_CURRENT}")
+                run_script 'env_set' "${SET_VAR}" "${SET_VAL}" "${MKTEMP_ENV_UPDATED}"
+            else
+                # Variable does not already exist
+                if [[ -z "${APP_LABEL_LIST[@]}" && "${INSTALLED_APPS[@]}" =~ "${APPNAME}" ]]; then
+                    local APPTEMPLATE="${APPTEMPLATESFOLDER}/${APPNAME,,}/${APPNAME,,}.labels.yml"
+                    mapfile -t APP_LABEL_LIST < <(grep --color=never -Po "\scom\.dockstarter\.appvars\.\K[\w]+" "${APPTEMPLATE}" || "")
+                    APP_LABEL_LIST=("${APP_LABEL_LIST[@]^^}")
+                fi
+                if [[ ${APP_LABEL_LIST[@]} =~ "${SET_VAR}" ]]; then
+                    # Variable is built in
+                    ENV_BUILTIN_LINES+=(${line})
+                else
+                    # Variable is user defined
+                    ENV_USER_DEFINED_LINES+=(${line})
+                fi
+            fi
+            unset ARRAY_ENV_CURRENT[$index]
+        done
+        if [[ ! -z "${ENV_BUILTIN_LINES[@]}" ]]; then
+            # Add all built in variables for app
+            echo "#" >> "${MKTEMP_ENV_UPDATED}" || error "# could not be written to ${MKTEMP_ENV_UPDATED}"
+            echo "# ${LAST_APPNAME}" >> "${MKTEMP_ENV_UPDATED}" || error "# ${LAST_APPNAME} could not be written to ${MKTEMP_ENV_UPDATED}"
+            echo "#" >> "${MKTEMP_ENV_UPDATED}" || error "# could not be written to ${MKTEMP_ENV_UPDATED}"
+            for line in "${ENV_BUILTIN_LINES[@]}"; do
+                local SET_VAR=${line%%=*}
+                local SET_VAL=$(run_script 'env_get' "${SET_VAR}" "${MKTEMP_ENV_CURRENT}")
+                echo "${line}" >> "${MKTEMP_ENV_UPDATED}" || error "${line} could not be written to ${MKTEMP_ENV_UPDATED}"
+                run_script 'env_set' "${SET_VAR}" "${SET_VAL}" "${MKTEMP_ENV_UPDATED}"
+            done
+        fi
+        if [[ ! -z "${ENV_USER_DEFINED_LINES[@]}" ]]; then
+            # Add all user defined variables for app
+            echo "#" >> "${MKTEMP_ENV_UPDATED}" || error "# could not be written to ${MKTEMP_ENV_UPDATED}"
+            echo "# ${LAST_APPNAME} (User Defined)" >> "${MKTEMP_ENV_UPDATED}" || error "# ${LAST_APPNAME} (User Defined) could not be written to ${MKTEMP_ENV_UPDATED}"
+            echo "#" >> "${MKTEMP_ENV_UPDATED}" || error "# could not be written to ${MKTEMP_ENV_UPDATED}"
+            for line in "${ENV_USER_DEFINED_LINES[@]}"; do
+                local SET_VAR=${line%%=*}
+                local SET_VAL=$(run_script 'env_get' "${SET_VAR}" "${MKTEMP_ENV_CURRENT}")
+                echo "${line}" >> "${MKTEMP_ENV_UPDATED}" || error "${line} could not be written to ${MKTEMP_ENV_UPDATED}"
+                run_script 'env_set' "${SET_VAR}" "${SET_VAL}" "${MKTEMP_ENV_UPDATED}"
+            done
+        fi
         LAST_APPNAME=${APPNAME}
-    done < <(grep -v -P '^#' "${MKTEMP_ENV_CURRENT}" | grep '=')
+        ARRAY_ENV_CURRENT=("${ARRAY_ENV_CURRENT[@]}")
+    done
     rm -f "${MKTEMP_ENV_CURRENT}" || warn "Failed to remove temporary .env update file.\nFailing command: ${F[C]}rm -f \"${MKTEMP_ENV_CURRENT}\""
     cp -f "${MKTEMP_ENV_UPDATED}" "${COMPOSE_ENV}" || fatal "Failed to copy file.\nFailing command: ${F[C]}cp -f \"${MKTEMP_ENV_UPDATED}\" \"${COMPOSE_ENV}\""
     rm -f "${MKTEMP_ENV_UPDATED}" || warn "Failed to remove temporary .env update file.\nFailing command: ${F[C]}rm -f \"${MKTEMP_ENV_UPDATED}\""
