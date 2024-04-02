7abd76cb69810272aecd74f99108b1608e7854f2
CLHatch <CLHatch@gmail.com>
2024-04-02 01:52:28 -0500

    Make `env_set` create the variable if it doesn't already exist.
    Make `env_set` create the variable if it doesn't already exist.

--- .scripts/env_set.sh
+++ .scripts/env_set.sh
@@ -9,13 +9,18 @@ env_set() {
     NEW_VAL=$(printf "%s\n" "${2-}" | sed -e "s/'/'\"'\"'/g" -e "1s/^/'/" -e "\$s/\$/'/")
     local VAR_FILE=${3:-$COMPOSE_ENV}
     local VAR_VAL
-    VAR_VAL=$(grep --color=never -P "^${SET_VAR}=" "${VAR_FILE}") || fatal "Failed to find ${SET_VAR} in ${VAR_FILE}\nFailing command: ${F[C]}grep --color=never -P \"^${SET_VAR}=\" \"${VAR_FILE}\""
-    # https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed/29613573#29613573
-    local SED_FIND
-    SED_FIND=$(sed 's/[^^]/[&]/g; s/\^/\\^/g' <<< "${VAR_VAL}")
-    local SED_REPLACE
-    SED_REPLACE=$(sed 's/[&/\]/\\&/g' <<< "${SET_VAR}=${NEW_VAL}")
-    sed -i "s/^${SED_FIND}$/${SED_REPLACE}/" "${VAR_FILE}" || fatal "Failed to set ${SED_REPLACE}\nFailing command: ${F[C]}sed -i \"s/^${SED_FIND}$/${SED_REPLACE}/\" \"${VAR_FILE}\""
+    if VAR_VAL=$(grep --color=never -P "^${SET_VAR}=" "${VAR_FILE}"); then
+        # Variable exists, change its value
+        # https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed/29613573#29613573
+        local SED_FIND
+        SED_FIND=$(sed 's/[^^]/[&]/g; s/\^/\\^/g' <<< "${VAR_VAL}")
+        local SED_REPLACE
+        SED_REPLACE=$(sed 's/[&/\]/\\&/g' <<< "${SET_VAR}=${NEW_VAL}")
+        sed -i "s/^${SED_FIND}$/${SED_REPLACE}/" "${VAR_FILE}" || fatal "Failed to set ${SED_REPLACE}\nFailing command: ${F[C]}sed -i \"s/^${SED_FIND}$/${SED_REPLACE}/\" \"${VAR_FILE}\""
+    else
+        # Variable doesn't exist, add it
+        echo "${SET_VAR}=${NEW_VAL}" >> "${VAR_FILE}" || fatal "Failed to set ${SET_VAR}=${NEW_VAL}\nFailing command: ${F[C]} \"${SET_VAR}=${NEW_VAL}\" >> \"${VAR_FILE}\""
+    fi
 }
 
 test_env_set() {
