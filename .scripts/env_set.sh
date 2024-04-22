8ee043dc50cd8ac459c1e661583680aecc7674d7
CLHatch <CLHatch@gmail.com>
2024-04-22 00:37:17 -0500

    Update env_set.sh
    Remove unused variable.

--- .scripts/env_set.sh
+++ .scripts/env_set.sh
@@ -8,7 +8,7 @@ env_set() {
     # https://unix.stackexchange.com/questions/422165/escape-double-quotes-in-variable/422170#422170
     NEW_VAL=$(printf "%s\n" "${2-}" | sed -e "s/'/'\"'\"'/g" -e "1s/^/'/" -e "\$s/\$/'/")
     local VAR_FILE=${3:-$COMPOSE_ENV}
-    local VAR_VAL
+    
     sed -i "/^\s*${SET_VAR}\s*=/d" "${VAR_FILE}" || true
     echo "${SET_VAR}=${NEW_VAL}" >> "${VAR_FILE}" || fatal "Failed to set ${SET_VAR}=${NEW_VAL}\nFailing command: ${F[C]} \"echo ${SET_VAR}=${NEW_VAL}\" >> \"${VAR_FILE}\""
 }
