fa46e23f9f296de496306dcbedea13f7decdddde
CLHatch <CLHatch@gmail.com>
2024-04-22 02:14:38 -0500

    Update env_rename.sh
    Ignore whitespace before and after variables.

--- .scripts/env_rename.sh
+++ .scripts/env_rename.sh
@@ -5,9 +5,9 @@ IFS=$'\n\t'
 env_rename() {
     local FROMVAR=${1-}
     local TOVAR=${2-}
-    if grep -q -P "^${FROMVAR^^}=" "${COMPOSE_ENV}"; then
+    if grep -q -P "^\s*${FROMVAR^^}\s*=" "${COMPOSE_ENV}"; then
         notice "Renaming ${FROMVAR^^} to ${TOVAR^^} in ${COMPOSE_ENV} file."
-        sed -i "s/^${FROMVAR^^}=/${TOVAR^^}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${FROMVAR^^} to ${TOVAR^^}\nFailing command: ${F[C]}sed -i \"s/^${FROMVAR^^}=/${TOVAR^^}=/\" \"${COMPOSE_ENV}\""
+        sed -i "s/^\s*${FROMVAR^^}\s*=/${TOVAR^^}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${FROMVAR^^} to ${TOVAR^^}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROMVAR^^}\\s*=/${TOVAR^^}=/\" \"${COMPOSE_ENV}\""
     fi
 }
 
