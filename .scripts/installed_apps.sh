2faaa5e0506cfe130e35390316ed942a7a66d2c2
CLHatch <CLHatch@gmail.com>
2025-01-30 03:33:12 -0600

    Allow whitespace to right of variables in search functions

--- .scripts/installed_apps.sh
+++ .scripts/installed_apps.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 installed_apps() {
     #grep --color=never -o -P '^\s*\K([A-Z][A-Z0-9]*(__[A-Z0-9]+)?)(?==)' "${COMPOSE_ENV}"
-    grep --color=never -o -P '^\s*\K[A-Z][A-Z0-9]*(?=_ENABLED=)' "${COMPOSE_ENV}" | tr '[:upper:]' '[:lower:]' | sort || true
+    grep --color=never -o -P '^\s*\K[A-Z][A-Z0-9]*(?=_ENABLED\s*=)' "${COMPOSE_ENV}" | tr '[:upper:]' '[:lower:]' | sort || true
 }
 
 test_installed_apps() {
