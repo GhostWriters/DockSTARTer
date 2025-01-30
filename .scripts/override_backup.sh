1072ab696501f7e34399fe36d710101d36514199
CLHatch <CLHatch@gmail.com>
2025-01-29 21:54:49 -0600

    Add global var for compose override file name/location

--- .scripts/override_backup.sh
+++ .scripts/override_backup.sh
@@ -3,17 +3,17 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 override_backup() {
-    if [[ -f "${SCRIPTPATH}/compose/docker-compose.override.yml" ]]; then
+    if [[ -f ${COMPOSE_OVERRIDE} ]]; then
         local DOCKER_VOLUME_CONFIG
         DOCKER_VOLUME_CONFIG=$(run_script 'env_get' DOCKER_VOLUME_CONFIG)
         local BACKUPTIME
         BACKUPTIME=$(date +"%Y%m%d%H%M%S")
-        info "Copying docker-compose.override.yml file to ${DOCKER_VOLUME_CONFIG}/.compose.backups/docker-compose.override.yml.${BACKUPTIME}"
+        info "Copying ${COMPOSE_OVERRIDE_NAME} file to ${DOCKER_VOLUME_CONFIG}/.compose.backups/${COMPOSE_OVERRIDE_NAME}.${BACKUPTIME}"
         mkdir -p "${DOCKER_VOLUME_CONFIG}/.compose.backups" || fatal "Failed to make directory.\nFailing command: ${F[C]}mkdir -p \"${DOCKER_VOLUME_CONFIG}/.compose.backups\""
-        cp "${COMPOSE_FOLDER}/docker-compose.override.yml" "${DOCKER_VOLUME_CONFIG}/.compose.backups/docker-compose.override.yml.${BACKUPTIME}" || fatal "Failed to copy file.\nFailing command: ${F[C]}cp \"${COMPOSE_FOLDER}/docker-compose.override.yml\" \"${DOCKER_VOLUME_CONFIG}/.compose.backups/docker-compose.override.yml.${BACKUPTIME}\""
+        cp "${COMPOSE_OVERRIDE}" "${DOCKER_VOLUME_CONFIG}/.compose.backups/${COMPOSE_OVERRIDE_NAME}.${BACKUPTIME}" || fatal "Failed to copy file.\nFailing command: ${F[C]}cp \"${COMPOSE_OVERRIDE}\" \"${DOCKER_VOLUME_CONFIG}/.compose.backups/${COMPOSE_OVERRIDE_NAME}.${BACKUPTIME}\""
         run_script 'set_permissions' "${DOCKER_VOLUME_CONFIG}/.compose.backups"
-        info "Removing old docker-compose.override.yml backups."
-        find "${DOCKER_VOLUME_CONFIG}/.compose.backups" -type f -name "docker-compose.override.yml.*" -mtime +3 -delete > /dev/null 2>&1 || warn "Old docker-compose.override.yml backups not removed."
+        info "Removing old ${COMPOSE_OVERRIDE_NAME} backups."
+        find "${DOCKER_VOLUME_CONFIG}/.compose.backups" -type f -name "${COMPOSE_OVERRIDE_NAME}.*" -mtime +3 -delete > /dev/null 2>&1 || warn "Old ${COMPOSE_OVERRIDE_NAME} backups not removed."
     fi
 }
 
