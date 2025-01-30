641c1b7933523a88b29d58509a31e0a3b55e3e40
CLHatch <CLHatch@gmail.com>
2025-01-30 02:57:02 -0600

    Add `env_vars` function
    `env_vars [ENV_FILE]` returns a list of variable names in a `.env` file.  If optional `ENV_FILE` parameter is not used, defaults to the global `.env` file.

---
+++ .scripts/env_vars.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+env_vars() {
+    local VAR_FILE=${1:-$COMPOSE_ENV}
+    grep --color=never -o -P '^\s*\K\w+(?==)' "${VAR_FILE}" || true
+}
+
+test_env_vars() {
+    run_script 'env_vars'
+    #warn "CI does not test env_vars."
+}
