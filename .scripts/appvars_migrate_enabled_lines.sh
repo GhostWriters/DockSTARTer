d9b4b79099d28548226a6260337eabaa90541e6f
CLHatch <CLHatch@gmail.com>
2025-02-01 08:10:51 -0600

    Fix shellcheck errors

--- .scripts/appvars_migrate_enabled_lines.sh
+++ .scripts/appvars_migrate_enabled_lines.sh
@@ -7,8 +7,8 @@ appvars_migrate_enabled_lines() {
     local NEW_SUFFIX="__ENABLED"
     local -a APPS
     readarray -t APPS < <(grep --color=never -o -P "^\s*\K[A-Z][A-Z0-9]*(?=${OLD_SUFFIX}\s*=)" "${COMPOSE_ENV}" | sort || true)
-    if [[ -n ${APPS[@]} ]]; then
-        for APPNAME in ${APPS[@]}; do
+    if [[ -n ${APPS[*]} ]]; then
+        for APPNAME in "${APPS[@]}"; do
             notice "Renaming ${APPNAME}${OLD_SUFFIX} to ${APPNAME}${NEW_SUFFIX} in ${COMPOSE_ENV} file."
             sed -i "s/^\s*${APPNAME}${OLD_SUFFIX}\s*=/${APPNAME}${NEW_SUFFIX}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${APPNAME}${OLD_SUFFIX} to ${APPNAME}${NEW_SUFFIX}\nFailing command: ${F[C]}sed -i \"s/^\\s*${APPNAME}${OLD_SUFFIX}\\s*=/${APPNAME}${NEW_SUFFIX}=/\" \"${COMPOSE_ENV}\""
         done
