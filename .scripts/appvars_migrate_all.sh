8496bf65369c9be3ff1a577efc40c50b2cd402d0
CLHatch <CLHatch@gmail.com>
2025-02-01 05:27:22 -0600

    Begin adding "migrate" functions
    Migration starts by renaming `APPNAME_ENABLED` variables to `APPNAME__ENABLED`.
    
    The full migration is not yet implemented.

---
+++ .scripts/appvars_migrate_all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+appvars_migrate_all() {
+    run_script 'appvars_migrate_enabled_lines'
+    local APPS=$( run_script 'installed_apps' )
+    for APPNAME in ${APPS}; do
+        run_script 'appvars_migrate' "${APPNAME}"
+    done
+}
+
+test_migrate_all() {
+    # run_script 'appvars_migrate_all'
+    warn "CI does not test appvars_migrate_all."
+}
---
+++ .scripts/appvars_migrate_enabled_lines.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+appvars_migrate_enabled_lines() {
+    local -a VARS
+    local OLD_SUFFIX="_ENABLED"
+    local NEW_SUFFIX="__ENABLED"
+    readarray -t APPS < <(grep --color=never -o -P "^\s*\K[A-Z][A-Z0-9]*(?=${OLD_SUFFIX}\s*=)" "${COMPOSE_ENV}" | sort || true)
+    if [[ ! -z ${APPS[@]} ]]; then
+        for APPNAME in ${APPS[@]}; do
+            notice "Renaming ${APPNAME}${OLD_SUFFIX} to ${APPNAME}${NEW_SUFFIX} in ${COMPOSE_ENV} file."
+            sed -i "s/^\s*${APPNAME}${OLD_SUFFIX}\s*=/${APPNAME}${NEW_SUFFIX}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${APPNAME}${OLD_SUFFIX} to ${APPNAME}${NEW_SUFFIX}\nFailing command: ${F[C]}sed -i \"s/^\\s*${APPNAME}${OLD_SUFFIX}\\s*=/${APPNAME}${NEW_SUFFIX}=/\" \"${COMPOSE_ENV}\""
+        done
+    fi
+}
+
+test_appvars_migrate_enabled_lines() {
+    # run_script 'appvars_migrate_enabled_lines'
+    warn "CI does not test appvars_migrate_enabled_lines."
+}
