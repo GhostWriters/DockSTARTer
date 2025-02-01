75dfb10e1877bf2e3e0640a2ec45e6dc9e637b2a
CLHatch <CLHatch@gmail.com>
2025-02-01 05:22:34 -0600

    Change functions to search for `APPNAME__ENABLED` instead of `APPNAME_ENABLED`

--- .scripts/appvars_rename.sh
+++ .scripts/appvars_rename.sh
@@ -6,9 +6,9 @@ appvars_rename() {
     local FROMAPP=${1-}
     local TOAPP=${2-}
     local FROMAPP_ENABLED
-    FROMAPP_ENABLED=$(run_script 'env_get' "${FROMAPP^^}_ENABLED")
+    FROMAPP_ENABLED=$(run_script 'env_get' "${FROMAPP^^}__ENABLED")
     local TOAPP_ENABLED
-    TOAPP_ENABLED=$(run_script 'env_get' "${TOAPP^^}_ENABLED")
+    TOAPP_ENABLED=$(run_script 'env_get' "${TOAPP^^}__ENABLED")
     if [[ ${FROMAPP_ENABLED} == true ]] && [[ ${TOAPP_ENABLED} != true ]]; then
         notice "Migrating from ${FROMAPP^^} to ${TOAPP^^}."
         docker stop "${FROMAPP,,}" || warn "Failed to stop ${FROMAPP,,} container.\nFailing command: ${F[C]}docker stop ${FROMAPP,,}"
@@ -17,7 +17,7 @@ appvars_rename() {
         DOCKER_VOLUME_CONFIG=$(run_script 'env_get' DOCKER_VOLUME_CONFIG)
         mv "${DOCKER_VOLUME_CONFIG}/${FROMAPP,,}" "${DOCKER_VOLUME_CONFIG}/${TOAPP,,}" || warn "Failed to move folder.\nFailing command: ${F[C]}mv \"${DOCKER_VOLUME_CONFIG}/${FROMAPP,,}\" \"${DOCKER_VOLUME_CONFIG}/${TOAPP,,}\""
         notice "Migrating vars."
-        sed -i "s/^\s*${FROMAPP^^}_/${TOAPP^^}_/" "${COMPOSE_ENV}" || fatal "Failed to migrate vars from ${FROMAPP^^}_ to ${TOAPP^^}_\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROMAPP^^}_/${TOAPP^^}_/\" \"${COMPOSE_ENV}\""
+        sed -i "s/^\s*${FROMAPP^^}__/${TOAPP^^}__/" "${COMPOSE_ENV}" || fatal "Failed to migrate vars from ${FROMAPP^^}__ to ${TOAPP^^}__\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROMAPP^^}__/${TOAPP^^}__/\" \"${COMPOSE_ENV}\""
         run_script 'appvars_create' "${TOAPP^^}"
         notice "Completed migrating from ${FROMAPP^^} to ${TOAPP^^}. Run ${F[C]}ds -c${NC} to create the new container."
     fi
