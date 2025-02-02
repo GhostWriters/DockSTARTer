27bb540924ee24cc1c5257d16ff0fa4a5a21c0c0
CLHatch <CLHatch@gmail.com>
2025-02-02 04:03:33 -0600

    Take out optional "envfile" param

--- .scripts/app_is_enabled.sh
+++ .scripts/app_is_enabled.sh
@@ -4,7 +4,6 @@ IFS=$'\n\t'
 
 app_is_enabled() {
     local APPNAME=${1-}
-    local VAR_FILE=${2:-$COMPOSE_ENV}
     local APP_ENABLED=$(run_script 'env_get' "${APPNAME}__ENABLED")
     if [[ ${APP_ENABLED} = "true" ]]; then
         return 0
