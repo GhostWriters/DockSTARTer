6d47eccd426d785481b6799acaa3190f5c01f192
CLHatch <CLHatch@gmail.com>
2025-02-01 22:37:23 -0600

    Fix `test_appvars_migrate` function name

--- .scripts/appvars_migrate.sh
+++ .scripts/appvars_migrate.sh
@@ -8,7 +8,7 @@ appvars_migrate() {
     warn "appvars_migrate ${APPNAME} not implemented yet."
 }
 
-test_appvars_migrate_all() {
+test_appvars_migrate() {
     # run_script 'appvars_migrate'
     warn "CI does not test appvars_migrate."
 }
