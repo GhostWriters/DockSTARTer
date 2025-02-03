cb779f727616da38ab92647f26302f9005f6a580
CLHatch <CLHatch@gmail.com>
2025-02-02 23:27:10 -0600

    Use `app_is_depreciated` function in `yml_mergs`

--- .scripts/yml_merge.sh
+++ .scripts/yml_merge.sh
@@ -16,7 +16,7 @@ yml_merge() {
             if [[ -f ${APP_FOLDER}/${FILENAME}.yml ]]; then
                 local APPDEPRECATED
                 APPDEPRECATED=$(grep --color=never -Po "\scom\.dockstarter\.appinfo\.deprecated: \K.*" "${APP_FOLDER}/${FILENAME}.labels.yml" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo false)
-                if [[ ${APPDEPRECATED} == true ]]; then
+                if run_script 'app_is_depreciated' "${APPNAME}"; then
                     warn "${APPNAME} IS DEPRECATED!"
                     warn "Please edit ${COMPOSE_ENV} and set ${APPNAME}__ENABLED to false."
                     continue
