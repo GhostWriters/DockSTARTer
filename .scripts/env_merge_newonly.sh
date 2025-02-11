622f2423e12fc8225e76d0d77bd29da99a034d43
CLHatch <CLHatch@gmail.com>
2025-02-11 00:49:23 -0600

    Initial re-write of `appvars_create` to merge from env files instead of label files

---
+++ .scripts/env_merge_newonly.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+env_merge_newonly() {
+    # env_merge_newonly "MERGE_TO_FILE" "MERGE_FROM_FILE"
+    #
+    # Merges all new variables from "MERGE_FROM_FILE" to "MERGE_TO_FILE".
+    # Skips all variables that already exist in "MERGE_TO_FILE"
+    local MERGE_TO_FILE=${1-}
+    local MERGE_FROM_FILE=${2-}
+
+    local MERGE_FROM_LINES=()
+    readarray -t MERGE_FROM_LINES < <(sed -n "s/^\s*\([A-Za-z0-9_]*\)\s*=/\1=/p" "${MERGE_FROM_FILE}" || true)
+    for line in "${MERGE_FROM_LINES[@]}"; do
+        local VARNAME
+        VARNAME="${line%%=*}"
+        if ! grep --color=never -q -P "^\s*${VARNAME}\s*=\K.*" "${MERGE_TO_FILE}"; then
+            notice "Adding ${line} in ${MERGE_TO_FILE} file."
+            printf '\n%s\n' "${line}" >> "${MERGE_TO_FILE}"
+        fi
+    done
+}
+
+test_env_merge_newonly() {
+    #run_script 'env_merge_newonly'
+    warn "CI does not test env_merge_newonly."
+}
