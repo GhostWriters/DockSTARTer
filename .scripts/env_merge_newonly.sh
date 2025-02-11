c5882a33377d7fece2d910495b8d4d17d38754f0
CLHatch <CLHatch@gmail.com>
2025-02-11 01:24:04 -0600

    Update env_merge_newonly.sh
    Handle files not existing

--- .scripts/env_merge_newonly.sh
+++ .scripts/env_merge_newonly.sh
@@ -10,16 +10,27 @@ env_merge_newonly() {
     local MERGE_TO_FILE=${1-}
     local MERGE_FROM_FILE=${2-}
 
-    local MERGE_FROM_LINES=()
-    readarray -t MERGE_FROM_LINES < <(sed -n "s/^\s*\([A-Za-z0-9_]*\)\s*=/\1=/p" "${MERGE_FROM_FILE}" || true)
-    for line in "${MERGE_FROM_LINES[@]}"; do
-        local VARNAME
-        VARNAME="${line%%=*}"
-        if ! grep --color=never -q -P "^\s*${VARNAME}\s*=\K.*" "${MERGE_TO_FILE}"; then
-            notice "Adding ${line} in ${MERGE_TO_FILE} file."
-            printf '\n%s\n' "${line}" >> "${MERGE_TO_FILE}"
-        fi
-    done
+    # If "MERGE_TO_FILE" doesn't exists, create it
+    if [[ ! -f ${MERGE_TO_FILE} ]]; then
+        touch "${MERGE_TO_FILE}"
+    fi
+
+    # If "MERGE_FROM_FILE" doesn't exists, give a warning
+    if [[ ! -f ${MERGE_FROM_FILE} ]]; then
+        warn "File ${MERGE_FROM_FILE} does not exist."
+    else
+        local MERGE_FROM_LINES=()
+        readarray -t MERGE_FROM_LINES < <(sed -n "s/^\s*\([A-Za-z0-9_]*\)\s*=/\1=/p" "${MERGE_FROM_FILE}" || true)
+        for line in "${MERGE_FROM_LINES[@]}"; do
+            local VARNAME
+            VARNAME="${line%%=*}"
+            if ! grep --color=never -q -P "^\s*${VARNAME}\s*=\K.*" "${MERGE_TO_FILE}"; then
+                notice "Adding ${line} in ${MERGE_TO_FILE} file."
+                printf '\n%s\n' "${line}" >> "${MERGE_TO_FILE}"
+            fi
+        done
+    fi
+
 }
 
 test_env_merge_newonly() {
