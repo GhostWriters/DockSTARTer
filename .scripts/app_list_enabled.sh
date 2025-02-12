c021957272a958bcd4db5810dc8ca8d9f290ec79
CLHatch <CLHatch@gmail.com>
2025-02-11 23:12:03 -0600

    Update "list" functions
    Only consider an app to be "installed", "enabled" or "disabled" if it also builtin (ignore `APPNAME__ENABLED` variable in this case).

--- .scripts/app_list_enabled.sh
+++ .scripts/app_list_enabled.sh
@@ -7,9 +7,14 @@ app_list_enabled() {
     local TRUE_REGEX="('?true'?)"
     local ENABLED_REGEX="__ENABLED\s*=${TRUE_REGEX}"
     local ENABLED_APPS_REGEX="${APPNAME_REGEX}(?=${ENABLED_REGEX})"
+    local -a ENABLED_APPS
+    local -a BUILTIN_APPS
 
     #notice "ENABLED_APPS_REGEX [ ${ENABLED_APPS_REGEX} ]"
-    grep --color=never -o -P "${ENABLED_APPS_REGEX}" "${COMPOSE_ENV}" | sort || true
+    readarray -t ENABLED_APPS < <(grep --color=never -o -P "${ENABLED_APPS_REGEX}" "${COMPOSE_ENV}" | sort || true)
+    readarray -t BUILTIN_APPS < <(run_script 'app_list_builtin')
+    local -a COMBINED=("${ENABLED_APPS[@]}" "${BUILTIN_APPS[@]}")
+    printf "%s\n" "${COMBINED[@]}" | sort | uniq -d
 }
 
 test_app_list_enabled() {
--- .scripts/app_list_installed.sh
+++ .scripts/app_list_installed.sh
@@ -5,9 +5,14 @@ IFS=$'\n\t'
 app_list_installed() {
     local APPNAME_REGEX='^[A-Z][A-Z0-9]*(__[A-Z0-9]+)?'
     local INSTALLED_APPS_REGEX="${APPNAME_REGEX}(?=__ENABLED\s*=)"
+    local -a INSTALLED_APPS
+    local -a BUILTIN_APPS
 
     #notice "INSTALLED_APPS_REGEX [ ${INSTALLED_APPS_REGEX} ]"
-    grep --color=never -o -P "${INSTALLED_APPS_REGEX}" "${COMPOSE_ENV}" | sort || true
+    readarray -t INSTALLED_APPS < <(grep --color=never -o -P "${INSTALLED_APPS_REGEX}" "${COMPOSE_ENV}" || true)
+    readarray -t BUILTIN_APPS < <(run_script 'app_list_builtin')
+    local -a COMBINED=("${INSTALLED_APPS[@]}" "${BUILTIN_APPS[@]}")
+    printf "%s\n" "${COMBINED[@]}" | sort | uniq -d
 }
 
 test_app_list_installed() {
