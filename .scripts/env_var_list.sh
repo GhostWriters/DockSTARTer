f6b65d6f81560f88444f2909dd7ab32216197563
CLHatch <CLHatch@gmail.com>
2025-02-13 03:31:56 -0600

    Rename function `env_vars` to `env_var_list`

--- .scripts/env_var_list.sh
+++ .scripts/env_var_list.sh
@@ -2,13 +2,12 @@
 set -Eeuo pipefail
 IFS=$'\n\t'
 
-env_vars() {
+env_var_list() {
     local VAR_FILE=${1:-$COMPOSE_ENV}
     local VAR_REGEX="\w+"
     grep --color=never -o -P "^\s*\K${VAR_REGEX}(?=\s*=)" "${VAR_FILE}" || true
 }
 
-test_env_vars() {
-    run_script 'env_vars'
-    #warn "CI does not test env_vars."
+test_env_var_list() {
+    run_script 'env_var_list'
 }
