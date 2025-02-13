ad78276d77e4e128d3d118256aea4bcbec509331
CLHatch <CLHatch@gmail.com>
2025-02-13 05:39:17 -0600

    Revert "Update appvars_purge.sh"
    This reverts commit d93b82e0aa21c1c0c37f81b248266c4badba4746.

--- .scripts/appvars_purge.sh
+++ .scripts/appvars_purge.sh
@@ -35,18 +35,18 @@ EOF
     if [[ ${CI-} == true ]] || run_script 'question_prompt' "${PROMPT:-CLI}" Y "${QUESTION}\\n"; then
         info "Purging ${APPNAME} .env variables."
 
-        local APPVARS
+        local -a APPVARS
         local APPVARS_REGEX
 
-        APPVARS=$(run_script 'appvars_list' "${APPNAME}") # Get list of app's variables in global .env file
-        APPVARS_REGEX=${APPVARS//\n/\|}                   # Make a string of variables seperated by "|"
-        notice "APPVARS_REGEX=[${APPVARS_REGEX}]"
+        readarray -t APPVARS < <(run_script 'appvars_list' "${APPNAME}") # Get list of app's variables in global .env file
+        printf -v APPVARS_REGEX "%s|" "${APPVARS[@]}"                    # Make a string of variables seperated by "|"
+        APPVARS_REGEX="${APPVARS_REGEX%|}"                               # Remove the final "| at end of the string
         # Remove variables from file
         sed -i -E "/^\s*(${APPVARS_REGEX})\s*=/d" "${COMPOSE_ENV}" || fatal "Failed to purge ${APPNAME} variables.\nFailing command: ${F[C]}sed -i -E \"/^\\\*(${APPVARS_REGEX})\\\*/d\" \"${COMPOSE_ENV}\""
 
-        APPVARS=$(run_script 'env_var_list' "${APP_ENV_FILE}") # Get list of variables in appname.env file
-        APPVARS_REGEX=${APPVARS//\n/\|}                        # Make a string of variables seperated by "|"
-        notice "APPVARS_REGEX=[${APPVARS_REGEX}]"
+        readarray -t APPVARS < <(run_script 'env_var_list' "${APP_ENV_FILE}") # Get list of variables in appname.env file
+        printf -v APPVARS_REGEX "%s|" "${APPVARS[@]}"                         # Make a string of variables seperated by "|"
+        APPVARS_REGEX="${APPVARS_REGEX%|}"                                    # Remove the final "| at end of the string
         # Remove variables from file
         sed -i -E "/^\s*(${APPVARS_REGEX})\s*=/d" "${APP_ENV_FILE}" || fatal "Failed to purge ${APPNAME} variables.\nFailing command: ${F[C]}sed -i -E \"/^\\\*(${APPVARS_REGEX})\\\*/d\" \"${APP_ENV_FILE}\""
     else
