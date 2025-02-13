a538c3e9d9a74293adb47648d225246e2e288979
CLHatch <CLHatch@gmail.com>
2025-02-13 00:46:08 -0600

    Switch to using `env_var_exists` instead of a return value from `get_env` to check variable existence.

--- .scripts/env_get.sh
+++ .scripts/env_get.sh
@@ -8,9 +8,6 @@ env_get() {
     #
     # Returns the variable "GET_VAR"  If no "VAR_FILE" is given, uses the global .env file
     # If "APPNAME:" is provided, gets variable from "env_files/appname.env"
-    #
-    # Function will also return success/fail in the return value based on if the variable exists
-
     local GET_VAR=${1-}
     local VAR_FILE=${2:-$COMPOSE_ENV}
     if [[ ${GET_VAR} =~ ^[A-Za-z0-9_]+: ]]; then
@@ -20,12 +17,10 @@ env_get() {
         GET_VAR=${GET_VAR#"${APPNAME}:"}
     fi
     if [[ -f ${VAR_FILE} ]]; then
-        grep --color=never -Po "^\s*${GET_VAR}\s*=\K.*" "${VAR_FILE}" | tail -1 | xargs
-        return ${PIPESTATUS[0]}
+        grep --color=never -Po "^\s*${GET_VAR}\s*=\K.*" "${VAR_FILE}" | tail -1 | xargs || true
     else
         # VAR_FILE does not exist, give a warning
         warn "${VAR_FILE} does not exist."
-        return 1
     fi
 
 }
@@ -33,10 +28,6 @@ env_get() {
 test_env_get() {
     run_script 'appvars_create' WATCHTOWER
     run_script 'env_get' WATCHTOWER__ENABLED
-    notice "'env_get' WATCHTOWER returned $?"
     run_script 'env_get' WATCHTOWER:WATCHTOWER_NOTIFICATIONS
-    notice "'env_get' WATCHTOWER:WATCHTOWER_NOTIFICATIONS returned $?"
-    run_script 'env_get' VARTHATDOESNOTEXIST
-    notice "'env_get' VARTHATDOESNOTEXIST returned $?"
     run_script 'appvars_purge' WATCHTOWER
 }
