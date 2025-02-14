cadd612d2549639a7195d3b92d8c58b3bc6de066
CLHatch <CLHatch@gmail.com>
2025-02-14 12:30:33 -0600

    Update env_rename.sh

--- .scripts/env_rename.sh
+++ .scripts/env_rename.sh
@@ -38,7 +38,7 @@ env_rename() {
         readarray -t FOUND_VAR_LIST < <(grep -o -P "^\s*\K${FROM_VAR}(?=\s*=)" "${FROM_VAR_FILE}" || true)
         notice "FOUND_VAR_LIST=[${FOUND_VAR_LIST[@]}]"
         for FOUND_VAR in "${FOUND_VAR_LIST[@]}"; do
-            notice "Moving variable\n${FOUND_VAR} in ${FROM_VAR_FILE} to\n${TO_VAR} in ${TO_VAR_FILE}"
+            notice "Moving variable\n${FROM_VAR_FILE}:${FOUND_VAR} to\n${TO_VAR_FILE}${TO_VAR}"
             #sed -i "s/^\s*${FOUND_VAR}\s*=/${TO_VAR}=/" "${VAR_FILE}" || fatal "Failed to rename var from ${FOUND_VAR} to ${TO_VAR} in ${VAR_FILE}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FOUND_VAR}\\s*=/${TO_VAR}=/\" \"${VAR_FILE}\""
         done
     fi
