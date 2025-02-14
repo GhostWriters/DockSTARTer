569ea47adcdca87064f63679a10b74a7af5ccc65
CLHatch <CLHatch@gmail.com>
2025-02-14 04:52:20 -0600

    Update env_rename.sh
    Fix position of `VAR_FILE` in argument list.

--- .scripts/env_rename.sh
+++ .scripts/env_rename.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 env_rename() {
     local FROMVAR=${1-}
     local TOVAR=${2-}
-    local VAR_FILE=${1:-$COMPOSE_ENV}
+    local VAR_FILE=${3:-$COMPOSE_ENV}
     if grep -q -P "^\s*${FROMVAR}\s*=" "${VAR_FILE}"; then
         notice "Renaming ${FROMVAR} to ${TOVAR} in ${VAR_FILE} file."
         sed -i "s/^\s*${FROMVAR}\s*=/${TOVAR}=/" "${VAR_FILE}" || fatal "Failed to rename var from ${FROMVAR} to ${TOVAR} in ${VAR_FILE}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROMVAR}\\s*=/${TOVAR}=/\" \"${VAR_FILE}\""
