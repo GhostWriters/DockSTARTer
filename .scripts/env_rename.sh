17e67b14799115b29f7dd9ca55e94e584e27fe55
CLHatch <CLHatch@gmail.com>
2025-02-14 04:47:55 -0600

    Update env_rename.sh
    Add optional parameter for .env file to use.

--- .scripts/env_rename.sh
+++ .scripts/env_rename.sh
@@ -5,9 +5,10 @@ IFS=$'\n\t'
 env_rename() {
     local FROMVAR=${1-}
     local TOVAR=${2-}
-    if grep -q -P "^\s*${FROMVAR}\s*=" "${COMPOSE_ENV}"; then
-        notice "Renaming ${FROMVAR} to ${TOVAR} in ${COMPOSE_ENV} file."
-        sed -i "s/^\s*${FROMVAR}\s*=/${TOVAR}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${FROMVAR} to ${TOVAR}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROMVAR}\\s*=/${TOVAR}=/\" \"${COMPOSE_ENV}\""
+    local VAR_FILE=${1:-$COMPOSE_ENV}
+    if grep -q -P "^\s*${FROMVAR}\s*=" "${VAR_FILE}"; then
+        notice "Renaming ${FROMVAR} to ${TOVAR} in ${VAR_FILE} file."
+        sed -i "s/^\s*${FROMVAR}\s*=/${TOVAR}=/" "${VAR_FILE}" || fatal "Failed to rename var from ${FROMVAR} to ${TOVAR} in ${VAR_FILE}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROMVAR}\\s*=/${TOVAR}=/\" \"${VAR_FILE}\""
     fi
 }
 
