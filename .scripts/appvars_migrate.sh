4802f08f21d6572e8b23674958d22c7aa147f03e
CLHatch <CLHatch@gmail.com>
2025-02-14 11:10:56 -0600

    Update appvars_migrate.sh

--- .scripts/appvars_migrate.sh
+++ .scripts/appvars_migrate.sh
@@ -15,18 +15,20 @@ appvars_migrate() {
 
         # Read "migrate" file into an array. Remove comments. Convert whitespace to single spaces. Remove empty lines.
         readarray -t MIGRATE_LINES < <(sed -E 's/#.*$//g ; s/\s+/ /g ; /^\s*$/d' "${MIGRATE_FILE}" || true)
-        for line in "${MIGRATE_LINES[@]}"; do
-            local MIGRATE_TO_VAR
-            local MIGRATE_FROM_VAR
+        if [[ -n ${MIGRATE_LINES[*]} ]]; then
+            for line in "${MIGRATE_LINES[@]}"; do
+                local MIGRATE_TO_VAR
+                local MIGRATE_FROM_VAR
 
-            MIGRATE_TO_VAR=${line%% *}
-            MIGRATE_FROM_VAR=${line##"${MIGRATE_TO_VAR}" }
-            MIGRATE_TO_VAR=${MIGRATE_TO_VAR/app:/${FILENAME}:}
-            MIGRATE_FROM_VAR=${MIGRATE_FROM_VAR/app:/${FILENAME}:}
+                MIGRATE_TO_VAR=${line%% *}
+                MIGRATE_FROM_VAR=${line##"${MIGRATE_TO_VAR}" }
+                MIGRATE_TO_VAR=${MIGRATE_TO_VAR/app:/${FILENAME}:}
+                MIGRATE_FROM_VAR=${MIGRATE_FROM_VAR/app:/${FILENAME}:}
 
-            notice "run_script 'env_rename' \"${MIGRATE_FROM_VAR}\" \"${MIGRATE_TO_VAR}\""
-            run_script 'env_rename' "${MIGRATE_FROM_VAR}" "${MIGRATE_TO_VAR}"
-        done
+                notice "run_script 'env_rename' \"${MIGRATE_FROM_VAR}\" \"${MIGRATE_TO_VAR}\""
+                run_script 'env_rename' "${MIGRATE_FROM_VAR}" "${MIGRATE_TO_VAR}"
+            done
+        fi
     fi
 }
 
