522c132f807fce76e4a043ee959e0387321a8b36
CLHatch <CLHatch@gmail.com>
2025-02-14 12:32:19 -0600

    Update env_rename.sh

--- .scripts/env_rename.sh
+++ .scripts/env_rename.sh
@@ -8,8 +8,6 @@ env_rename() {
     local FROM_VAR_FILE=${3:-$COMPOSE_ENV}
     local TO_VAR_FILE=${4:-$COMPOSE_ENV}
 
-    #notice "[${FROM_VAR}] [${TO_VAR}] [${FROM_VAR_FILE}] [${TO_VAR_FILE}]"
-
     # Change the .env file to use if specified in the variable, and remove the appname from the string
     if [[ ${FROM_VAR} == *":"* ]]; then
         FROM_VAR_FILE="${APP_ENV_FOLDER}/${FROM_VAR%:*}.env"
@@ -20,14 +18,11 @@ env_rename() {
         TO_VAR="${TO_VAR#*:}"
     fi
 
-    #notice "[${FROM_VAR}] [${TO_VAR}] [${FROM_VAR_FILE}] [${TO_VAR_FILE}]"
-
     if [[ ${FROM_VAR_FILE} == "${TO_VAR_FILE}" ]]; then
         # Renaming variables in the same file, do a replace
         local VAR_FILE=${FROM_VAR_FILE}
         local -a FOUND_VAR_LIST=()
         readarray -t FOUND_VAR_LIST < <(grep -o -P "^\s*\K${FROM_VAR}(?=\s*=)" "${VAR_FILE}" || true)
-        notice "FOUND_VAR_LIST=[${FOUND_VAR_LIST[@]}]"
         for FOUND_VAR in "${FOUND_VAR_LIST[@]}"; do
             notice "Renaming ${FOUND_VAR} to ${TO_VAR} in ${VAR_FILE}"
             sed -i "s/^\s*${FOUND_VAR}\s*=/${TO_VAR}=/" "${VAR_FILE}" || fatal "Failed to rename var from ${FOUND_VAR} to ${TO_VAR} in ${VAR_FILE}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FOUND_VAR}\\s*=/${TO_VAR}=/\" \"${VAR_FILE}\""
