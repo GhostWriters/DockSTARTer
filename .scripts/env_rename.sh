d29e9d5f7f1591f3c0eca2f91468f042219159e6
CLHatch <CLHatch@gmail.com>
2025-02-14 09:02:15 -0600

    Update env_rename.sh
    Change to allow `appname:variable` format in arguments.  Initial code to allow "renaming" between files (not completed yet).

--- .scripts/env_rename.sh
+++ .scripts/env_rename.sh
@@ -3,12 +3,34 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 env_rename() {
-    local FROMVAR=${1-}
-    local TOVAR=${2-}
-    local VAR_FILE=${3:-$COMPOSE_ENV}
-    if grep -q -P "^\s*${FROMVAR}\s*=" "${VAR_FILE}"; then
-        notice "Renaming ${FROMVAR} to ${TOVAR} in ${VAR_FILE} file."
-        sed -i "s/^\s*${FROMVAR}\s*=/${TOVAR}=/" "${VAR_FILE}" || fatal "Failed to rename var from ${FROMVAR} to ${TOVAR} in ${VAR_FILE}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROMVAR}\\s*=/${TOVAR}=/\" \"${VAR_FILE}\""
+    local FROM_VAR=${1-}
+    local TO_VAR=${2-}
+    local FROM_VAR_FILE=${3:-$COMPOSE_ENV}
+    local TO_VAR_FILE=${4:-$COMPOSE_ENV}
+
+    # Change the .env file to use if specified in the variable, and remove the appname from the string
+    if [[ ${TO_VAR} == *":"* ]]; then
+        TO_VAR_FILE="${APP_ENV_FOLDER}/${TO_VAR%:*}.env"
+        TO_VAR="${TO_VAR#*:}"
+    fi
+    if [[ ${FROM_VAR} == *":"* ]]; then
+        FROM_VAR_FILE="${APP_ENV_FOLDER}/${FROM_VAR%:*}.env"
+        FROM_VAR="${FROM_VAR#*:}"
+    fi
+
+    if [[ ${FROM_VAR_FILE-} == "${TO_VAR_FILE-}" ]]; then
+        # Renaming variables in the same file, do a replace
+        local VAR_FILE=${FROM_VAR_FILE}
+        if grep -q -P "^\s*${FROM_VAR}\s*=" "${VAR_FILE}"; then
+            notice "Renaming ${FROM_VAR} to ${TO_VAR} in ${VAR_FILE}"
+            sed -i "s/^\s*${FROM_VAR}\s*=/${TO_VAR}=/" "${VAR_FILE}" || fatal "Failed to rename var from ${FROM_VAR} to ${TO_VAR} in ${VAR_FILE}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROM_VAR}\\s*=/${TO_VAR}=/\" \"${VAR_FILE}\""
+        fi
+    else
+        # Renaming variables in different files, do a copy and delete
+        if grep -q -P "^\s*${FROM_VAR}\s*=" "${FROM_VAR_FILE}"; then
+            notice "Moving ${FROM_VAR} in ${FROM_VAR_FILE} to:\n ${TO_VAR} in ${TO_VAR_FILE}"
+            #sed -i "s/^\s*${FROM_VAR}\s*=/${TO_VAR}=/" "${VAR_FILE}" || fatal "Failed to rename var from ${FROM_VAR} to ${TO_VAR} in ${VAR_FILE}\nFailing command: ${F[C]}sed -i \"s/^\\s*${FROM_VAR}\\s*=/${TO_VAR}=/\" \"${VAR_FILE}\""
+        fi
     fi
 }
 
