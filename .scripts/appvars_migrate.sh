3c78621920020fe80d17adde1a8f8bc30debd5ce
CLHatch <CLHatch@gmail.com>
2025-02-14 01:59:26 -0600

    Update appvars_migrate.sh
    Start on migrate function.  Read the migrate files.

--- .scripts/appvars_migrate.sh
+++ .scripts/appvars_migrate.sh
@@ -5,10 +5,26 @@ IFS=$'\n\t'
 appvars_migrate() {
     local APPNAME=${1-}
     APPNAME=${APPNAME^^}
-    warn "appvars_migrate ${APPNAME} not implemented yet."
+
+    local FILENAME=${APPNAME,,}
+    local APP_FOLDER="${TEMPLATES_FOLDER}/${FILENAME}"
+    local MIGRATE_FILE="${APP_FOLDER}/${FILENAME}.migrate"
+
+    if [[ -f ${MIGRATE_FILE} ]]; then
+        notice "Checking migrations for ${APPNAME}"
+        local -a MIGRATE_LINES=()
+        # Read "migrate" file into an array. Remove comments. Convert whitespace to single spaces. Remove empty lines.
+        readarray -t MIGRATE_LINES < <(sed -E 's/#.*$//g ; s/\s+/ /g ; /^\s*$/d' "${MIGRATE_FILE}")
+        if [[ -n ${MIGRATE_LINES[*]-} ]]; then
+            notice "${APPNAME} Migrate Lines:\n[${MIGRATE_LINES[@]}]\n"
+        else
+            notice "No migrate lines for ${APPNAME}"
+        fi
+    else
+        notice "No migrate file for ${APPNAME}"
+    fi
 }
 
 test_appvars_migrate() {
-    # run_script 'appvars_migrate'
-    warn "CI does not test appvars_migrate."
+    run_script 'appvars_migrate' WATCHTOWER
 }
