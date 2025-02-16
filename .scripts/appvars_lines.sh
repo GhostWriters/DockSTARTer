56e7953b7ea73227ec0bb0cdd2a1b15ad783ed0c
CLHatch <CLHatch@gmail.com>
2025-02-16 17:18:16 -0600

    Update appvars_lines.sh
    Change to return all lines not for an app if `APPNAME` is empty.

--- .scripts/appvars_lines.sh
+++ .scripts/appvars_lines.sh
@@ -3,14 +3,29 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 appvars_lines() {
+    # Return all lines in `.env` file for app APPNAME
+    # If APPNAME is empty, return all lines in `.env` file that are not for an app
     local APPNAME=${1-}
-    local VAR_REGEX="${APPNAME}__(?![A-Za-z0-9]+__)\w+"
-    local APP_VARS_REGEX="\s*${VAR_REGEX}\s*="
-    grep --color=never -P "${APP_VARS_REGEX}" "${COMPOSE_ENV}" || true
+    APPNAME=${APPNAME^^}
+    if [[ -n ${APPNAME} ]]; then
+        # Search for all variables for app "APPNAME"
+        local VAR_REGEX="${APPNAME}__(?![A-Za-z0-9]+__)\w+"
+        local APP_VARS_REGEX="^\s*${VAR_REGEX}\s*="
+        grep --color=never -P "${APP_VARS_REGEX}" || true
+    else
+        # Search for all variables not for an app
+        local VAR_REGEX='^[A-Z][A-Z0-9]*(__[A-Z0-9]+)+\w+'
+        local APP_VARS_REGEX="^\s*${VAR_REGEX}\s*="
+        grep -v -P '^\s*$|^\s*\#' "${COMPOSE_ENV}" | grep --color=never -v -P "${APP_VARS_REGEX}" || true
+    fi
 }
 
 test_appvars_lines() {
+    notice "[WATCHTOWER]"
     run_script 'appvars_lines' WATCHTOWER
+    notice "[RADARR__4K]"
     run_script 'appvars_lines' RADARR__4K
+    notice "[]"
+    run_script 'appvars_lines' ''
     #warn "CI does not test appvars_lines."
 }
