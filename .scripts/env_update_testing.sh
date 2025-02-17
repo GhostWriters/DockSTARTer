3de407a46c915c1f6d6ed57ac50aef9b92e175ac
CLHatch <CLHatch@gmail.com>
2025-02-17 17:00:52 -0600

    Update env_update_testing.sh

--- .scripts/env_update_testing.sh
+++ .scripts/env_update_testing.sh
@@ -17,27 +17,32 @@ env_update_testing() {
             APP_DEFAULT_GLOBAL_ENV_FILE="${TEMPLATES_FOLDER}/${appname}/.env"
         fi
         run_script 'appvars_lines' "${APPNAME}" > "${ENV_LINES_FILE}"
-        run_script 'env_format_lines' "${ENV_LINES_FILE}" "${APP_DEFAULT_GLOBAL_ENV_FILE}" "${APPNAME}"
+        run_script 'env_format_lines' "${ENV_LINES_FILE}" "${APP_DEFAULT_GLOBAL_ENV_FILE}" "${APPNAME}" > "${MKTEMP_ENV_UPDATED}" \
+            || fatal "Failed to write temporary .env update file."
     done
+
+    cp -f "${MKTEMP_ENV_UPDATED}" "${COMPOSE_ENV}" || fatal "Failed to copy file.\nFailing command: ${F[C]}cp -f \"${MKTEMP_ENV_UPDATED}\" \"${COMPOSE_ENV}\""
+    rm -f "${MKTEMP_ENV_UPDATED}" || warn "Failed to remove temporary .env update file.\nFailing command: ${F[C]}rm -f \"${MKTEMP_ENV_UPDATED}\""
+    run_script 'set_permissions' "${COMPOSE_ENV}"
+
     # Process all referenced appname.env files
     for APPNAME in ${APPS^^}; do
         local appname=${APPNAME,,}
         local APP_ENV_FILE="${APP_ENV_FOLDER}/${appname}.env"
         local APP_DEFAULT_ENV_FILE=""
+        local MKTEMP_ENV_UPDATED
+        MKTEMP_ENV_UPDATED=$(mktemp) || fatal "Failed to create temporary update .env file.\nFailing command: ${F[C]}mktemp"
         if run_script 'app_is_installed' "${APPNAME}"; then
             APP_DEFAULT_ENV_FILE="${TEMPLATES_FOLDER}/${appname}/${appname}.env"
         fi
-        run_script 'env_format_lines' "${APP_ENV_FILE}" "${APP_DEFAULT_ENV_FILE}" "${APPNAME}"
+        run_script 'env_format_lines' "${APP_ENV_FILE}" "${APP_DEFAULT_ENV_FILE}" "${APPNAME}" > "${MKTEMP_ENV_UPDATED}" \
+            || fatal "Failed to write temporary ${appname}.env update file."
+        cp -f "${MKTEMP_ENV_UPDATED}" "${APP_ENV_FILE}" || fatal "Failed to copy file.\nFailing command: ${F[C]}cp -f \"${MKTEMP_ENV_UPDATED}\" \"${APP_ENV_FILE}\""
+        rm -f "${MKTEMP_ENV_UPDATED}" || warn "Failed to remove temporary ${appname}.env update file.\nFailing command: ${F[C]}rm -f \"${MKTEMP_ENV_UPDATED}\""
+        run_script 'set_permissions' "${APP_ENV_FILE}"
     done
 
-    #local MKTEMP_ENV_UPDATED
-    #MKTEMP_ENV_UPDATED=$(mktemp) || fatal "Failed to create temporary update .env file.\nFailing command: ${F[C]}mktemp"
-    #printf '%s\n' "${UPDATED_ENV_LINES[@]}" > "${MKTEMP_ENV_UPDATED}" || fatal "Failed to write temporary ${FILENAME}.env update file."
-
-    #cp -f "${MKTEMP_ENV_UPDATED}" "${APP_ENV_FILE}" || fatal "Failed to copy file.\nFailing command: ${F[C]}cp -f \"${MKTEMP_ENV_UPDATED}\" \"${APP_ENV_FILE}\""
-    #rm -f "${MKTEMP_ENV_UPDATED}" || warn "Failed to remove temporary ${FILENAME}.env update file.\nFailing command: ${F[C]}rm -f \"${MKTEMP_ENV_UPDATED}\""
-    #run_script 'set_permissions' "${APP_ENV_FILE}"
-    #run_script 'env_sanitize'
+    run_script 'env_sanitize'
     info "Environment file update complete."
 }
 
