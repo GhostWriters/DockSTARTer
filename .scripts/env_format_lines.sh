260353905d17dca95dc241bbada31b4ee2bc2109
CLHatch <CLHatch@gmail.com>
2025-02-17 00:39:20 -0600

    Update env_format_lines.sh

--- .scripts/env_format_lines.sh
+++ .scripts/env_format_lines.sh
@@ -7,12 +7,12 @@ env_format_lines() {
     local ENV_DEFAULT_FILE=${2-}
     local APPNAME=${3-}
     APPNAME=${APPNAME^^}
-    local appname={APPNAME,,}
+    local appname=${APPNAME,,}
 
-    local -a ENV_LINES_ARRAY=()
-    readarray -t ENV_LINES_ARRAY < <(run_script 'env_lines' "${ENV_FILE}" || true)
+    local -a CURRENT_ENV_LINES=()
+    readarray -t CURRENT_ENV_LINES < <(run_script 'env_lines' "${ENV_FILE}" || true)
 
-    local -a FORMATTED_ENV_ARRAY=()
+    local -a FORMATTED_ENV_LINES=()
     if [[ -n ${APPNAME} ]] && run_script 'app_is_builtin' "${APPNAME}"; then
         # APPNAME is specified and builtin, output main heading
         local HEADING_TITLE="${APPNAME}"
@@ -27,40 +27,35 @@ env_format_lines() {
     fi
     if [[ -n ${ENV_DEFAULT_FILE} && -f ${ENV_DEFAULT_FILE} ]]; then
         # Default file is specified and exists, add the contents verbatim
-        readarray -t -O ${#FORMATTED_ENV_ARRAY[@]} FORMATTED_ENV_ARRAY < "${ENV_DEFAULT_FILE}"
+        readarray -t -O ${#FORMATTED_ENV_LINES[@]} FORMATTED_ENV_LINES < "${ENV_DEFAULT_FILE}"
     fi
-    
 
     # VAR_TO_ENV_LINE["VAR"]="line"
     local -A VAR_TO_ENV_LINE=()
-    for line in $(printf '%s\n' "${FORMATTED_ENV_ARRAY[@]}" | grep -v -P '^\s*#|^\s*$' | grep '=' || true); do
+    for line in $(printf '%s\n' "${FORMATTED_ENV_LINES[@]}" | grep -v -P '^\s*#|^\s*$' | grep '=' || true); do
         local VAR=${line%%=*}
         VAR_TO_ENV_LINE[$VAR]="$line"
     done
-    
+
     # FORMATTED_ENV_VAR_INDEX["VAR"]=index position of line in FORMATTED_ENV_LINE
     local -A FORMATTED_ENV_VAR_INDEX
-    {
-        local -a VAR_LINES
-        # Make an array with the contents "line number:VARIABLE" in each element
-        readarray -t VAR_LINES < <(printf '%s\n' "${FORMATTED_ENV_ARRAY[@]}" | grep -n -o -P '^[A-Za-z0-9_]*(?=[=])')
-        for line in "${VAR_LINES[@]}"; do
-            local index=${line%:*}
-            index=$((index - 1))
-            local VAR=${line#*:}
-            FORMATTED_ENV_VAR_INDEX[$VAR]=$index
-        done
-    }
+    local -a VAR_LINES
+    # Make an array with the contents "line number:VARIABLE" in each element
+    readarray -t VAR_LINES < <(printf '%s\n' "${FORMATTED_ENV_LINES[@]}" | grep -n -o -P '^[A-Za-z0-9_]*(?=[=])')
+    for line in "${VAR_LINES[@]}"; do
+        local index=${line%:*}
+        index=$((index - 1))
+        local VAR=${line#*:}
+        FORMATTED_ENV_VAR_INDEX[$VAR]=$index
+    done
 
-    # Create sorted array of vars in current .env file.  Sort `_` to the top.
-    local -a CURRENT_ENV_VARS
-    readarray -t CURRENT_ENV_VARS < <(printf '%s\n' "${!VAR_TO_ENV_LINE[@]}" | tr "_" "." | env LC_ALL=C sort | tr "." "_")
-
-    # Process each variable, replacing any exising variables in the formatted .env file
-    for index in "${!CURRENT_ENV_VARS[@]}"; do
+    # Update the default variables
+    for index in "${!CURRENT_ENV_LINES[@]}"; do
+        local line=${CURRENT_ENV_LINES[index]}
+        local VAR=${line%%=*}
         if [[ -n ${FORMATTED_ENV_VAR_INDEX["$VAR"]-} ]]; then
             # Variable already exists, update its value
-            FORMATTED_ENV_ARRAY[${FORMATTED_ENV_VAR_INDEX["$VAR"]}]=${VAR_TO_ENV_LINE["$VAR"]}
+            FORMATTED_ENV_LINES[${FORMATTED_ENV_VAR_INDEX["$VAR"]}]=${VAR_TO_ENV_LINE["$VAR"]}
             unset 'CURRENT_ENV_VARS[index]'
         fi
     done
@@ -70,13 +65,23 @@ env_format_lines() {
     HEADING_TITLE+="(User Defined)"
     local HEADING
     printf -v HEADING "##\n## %s\n##" "${HEADING_TITLE}"
-    FORMATTED_ENV_ARRAY+=("${HEADING}")
+    FORMATTED_ENV_LINES+=("${HEADING}")
 
-    for VAR in "${CURRENT_ENV_VARS[@]}"; do
-        # There are still variables to process, add to the end of the file
-        FORMATTED_ENV_ARRAY+=("${VAR_TO_ENV_LINE[$VAR]}")
-        #FORMATTED_ENV_VAR_INDEX[$VAR]=$((${#FORMATTED_ENV_ARRAY[@]} - 1))
+    # Add the user defined variables
+    for index in "${!CURRENT_ENV_LINES[@]}"; do
+        local line=${CURRENT_ENV_LINES[index]}
+        local VAR=${line%%=*}
+        if [[ -n ${FORMATTED_ENV_VAR_INDEX["$VAR"]-} ]]; then
+            # Variable already exists, update its value
+            FORMATTED_ENV_LINES[${FORMATTED_ENV_VAR_INDEX["$VAR"]}]=${VAR_TO_ENV_LINE["$VAR"]}
+        else
+            # Variable is new, add it
+            FORMATTED_ENV_LINES+=("${VAR_TO_ENV_LINE[$VAR]}")
+            FORMATTED_ENV_VAR_INDEX[$VAR]=$((${#FORMATTED_ENV_LINES[@]} - 1))
+        fi
     done
+
+    printf '%s\n' "${FORMATTED_ENV_LINES}"
 }
 
 test_env_format_lines() {
