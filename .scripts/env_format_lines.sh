1eaa485c8dc6d780b604af1733c75189c33cefe2
CLHatch <CLHatch@gmail.com>
2025-02-17 02:43:34 -0600

    Update env_format_lines.sh

--- .scripts/env_format_lines.sh
+++ .scripts/env_format_lines.sh
@@ -59,9 +59,9 @@ env_format_lines() {
         done
 
         if [[ -n ${CURRENT_ENV_LINES[@]} ]]; then
-            if [[ TOP_HEADING == true ]]; then
+            if [[ ${TOP_HEADING} == true ]]; then
                 # Add a blank if there was a previous heading
-                FORMATTED_ENV_LINES+=("" "")
+                FORMATTED_ENV_LINES+=(""
             fi
             # Add the "User Defined" heading
             local HEADING_TITLE="${APPNAME}"
