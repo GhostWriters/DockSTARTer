cb822ed8ded20e51889c1cc1b13c9d0a8001783e
CLHatch <CLHatch@gmail.com>
2025-03-02 14:23:53 -0600

    Update app_nicename.sh
    `grep -s` was still outputting a blank line on error.  Change to only doing the `grep` if the file exists.

--- .scripts/app_nicename.sh
+++ .scripts/app_nicename.sh
@@ -6,8 +6,12 @@ app_nicename() {
     # Return the "NiceName" of the appname passed. If there is no "NiceName", return the lower case "appname"
     local appname=${1-}
     appname=${appname,,}
-
-    grep --color=never -s -Po "\scom\.dockstarter\.appinfo\.nicename: \K.*" "${TEMPLATES_FOLDER}/${appname}/${appname}.labels.yml" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo "${appname}"
+    local LABELS_FILE="${TEMPLATES_FOLDER}/${appname}/${appname}.labels.yml"
+    if [[ -f ${LABELS_FILE} ]]; then
+        grep --color=never -Po "\scom\.dockstarter\.appinfo\.nicename: \K.*" "${LABELS_FILE}" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo "${appname}"
+    else
+        echo "${appname}"
+    fi
 
 }
 
