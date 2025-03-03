0ec486cd8ce5d5f8dd52852a9c850f618be8a1aa
CLHatch <CLHatch@gmail.com>
2025-03-03 00:08:14 -0600

    Update appvars_create.sh
    Make `appvars_create` accept multiple appnames as arguments

--- .scripts/appvars_create.sh
+++ .scripts/appvars_create.sh
@@ -3,25 +3,28 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 appvars_create() {
-    local APPNAME=${1-}
-    APPNAME=${APPNAME^^}
+    local AppList
+    AppList=$(xargs -n 1 <<< "$*")
+    for AppName in ${AppList}; do
+        local APPNAME=${AppName^^}
+        local appname=${APPNAME,,}
 
-    local FILENAME=${APPNAME,,}
-    local APP_FOLDER="${TEMPLATES_FOLDER}/${FILENAME}"
-    local APP_DEFAULT_GLOBAL_ENV_FILE="${APP_FOLDER}/.env"
-    local APP_DEFAULT_ENV_FILE="${APP_FOLDER}/${FILENAME}.env"
-    local APP_ENV_FILE="${APP_ENV_FOLDER}/${FILENAME}.env"
+        local APP_FOLDER="${TEMPLATES_FOLDER}/${appname}"
+        local APP_DEFAULT_GLOBAL_ENV_FILE="${APP_FOLDER}/.env"
+        local APP_DEFAULT_ENV_FILE="${APP_FOLDER}/${appname}.env"
+        local APP_ENV_FILE="${APP_ENV_FOLDER}/${appname}.env"
 
-    info "Creating environment variables for ${APPNAME}."
+        info "Creating environment variables for ${APPNAME}."
 
-    if ! run_script 'env_var_exists' "${APPNAME}__ENABLED"; then
-        run_script 'env_set' "${APPNAME}__ENABLED" true
-    fi
+        if ! run_script 'env_var_exists' "${APPNAME}__ENABLED"; then
+            run_script 'env_set' "${APPNAME}__ENABLED" true
+        fi
 
-    run_script 'appvars_migrate' "${APPNAME}"
+        run_script 'appvars_migrate' "${APPNAME}"
 
-    run_script 'env_merge_newonly' "${COMPOSE_ENV}" "${APP_DEFAULT_GLOBAL_ENV_FILE}"
-    run_script 'env_merge_newonly' "${APP_ENV_FILE}" "${APP_DEFAULT_ENV_FILE}"
+        run_script 'env_merge_newonly' "${COMPOSE_ENV}" "${APP_DEFAULT_GLOBAL_ENV_FILE}"
+        run_script 'env_merge_newonly' "${APP_ENV_FILE}" "${APP_DEFAULT_ENV_FILE}"
+    done
 }
 
 test_appvars_create() {
