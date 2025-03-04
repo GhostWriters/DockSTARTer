d49c43d4bc191de03c33e9b9d17612ea75f238ed
CLHatch <CLHatch@gmail.com>
2025-03-03 18:55:00 -0600

    Update config_apps.sh
    Get list of enabled apps using a function.

--- .scripts/config_apps.sh
+++ .scripts/config_apps.sh
@@ -5,10 +5,9 @@ IFS=$'\n\t'
 config_apps() {
     info "Configuring .env variables for enabled apps."
     run_script 'appvars_create_all'
-    while IFS= read -r line; do
-        local APPNAME=${line%%_ENABLED=*}
+    while IFS= read -r APPNAME; do
         run_script 'menu_app_vars' "${APPNAME}" || return 1
-    done < <(grep --color=never -P '_ENABLED='"'"'?true'"'"'?$' "${COMPOSE_ENV}")
+    done < <(run_script 'app_list_enabled')
 }
 
 test_config_apps() {
