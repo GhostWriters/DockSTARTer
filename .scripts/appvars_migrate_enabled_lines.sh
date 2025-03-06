668cbb3316c10c70b9a8c02a15f837489280a28a
CLHatch <CLHatch@gmail.com>
2025-03-06 02:33:31 -0600

    Update appvars_migrate_enabled_lines.sh
    Use `sort -u` to remove any duplicates found.
    Use `env_rename` function to do the renaming.

--- .scripts/appvars_migrate_enabled_lines.sh
+++ .scripts/appvars_migrate_enabled_lines.sh
@@ -6,11 +6,10 @@ appvars_migrate_enabled_lines() {
     local OLD_SUFFIX="_ENABLED"
     local NEW_SUFFIX="__ENABLED"
     local -a APPS
-    readarray -t APPS < <(grep --color=never -o -P "^\s*\K[A-Z][A-Z0-9]*(?=${OLD_SUFFIX}\s*=)" "${COMPOSE_ENV}" | sort || true)
+    readarray -t APPS < <(grep --color=never -o -P "^\s*\K[A-Z][A-Z0-9]*(?=${OLD_SUFFIX}\s*=)" "${COMPOSE_ENV}" | sort -u || true)
     if [[ -n ${APPS[*]} ]]; then
         for APPNAME in "${APPS[@]}"; do
-            notice "Renaming ${APPNAME}${OLD_SUFFIX} to ${APPNAME}${NEW_SUFFIX} in ${COMPOSE_ENV} file."
-            sed -i "s/^\s*${APPNAME}${OLD_SUFFIX}\s*=/${APPNAME}${NEW_SUFFIX}=/" "${COMPOSE_ENV}" || fatal "Failed to rename var from ${APPNAME}${OLD_SUFFIX} to ${APPNAME}${NEW_SUFFIX}\nFailing command: ${F[C]}sed -i \"s/^\\s*${APPNAME}${OLD_SUFFIX}\\s*=/${APPNAME}${NEW_SUFFIX}=/\" \"${COMPOSE_ENV}\""
+            run_script 'env_rename' "${APPNAME}${OLD_SUFFIX}" "${APPNAME}${NEW_SUFFIX}"
         done
     fi
 }
