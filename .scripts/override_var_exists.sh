7acf59d14731bcff226f8b38c471dac2f5542608
CLHatch <CLHatch@gmail.com>
2025-03-06 01:30:14 -0600

    Update override_var_exists.sh
    Escape the first `\` in the grep.

--- .scripts/override_var_exists.sh
+++ .scripts/override_var_exists.sh
@@ -10,7 +10,7 @@ override_var_exists() {
         return 1
     fi
     # Search for $VarName or ${VarName followed by a word break
-    grep -P "\$\{?\K${VarName}\b" "${COMPOSE_OVERRIDE}"
+    grep -P "\\$\{?\K${VarName}\b" "${COMPOSE_OVERRIDE}"
 }
 
 test_override_var_exists() {
