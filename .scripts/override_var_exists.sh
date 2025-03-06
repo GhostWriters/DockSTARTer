cc8f8acd466b93fdc04409a43cab58e712f30d2a
CLHatch <CLHatch@gmail.com>
2025-03-06 01:02:39 -0600

    Update override_var_exists.sh
    Return true if the variable exists in the override file.

--- .scripts/override_var_exists.sh
+++ .scripts/override_var_exists.sh
@@ -3,8 +3,14 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 override_var_exists() {
-    # Stub.  For now, always return false
-    return 1
+    local VarName=${1-}
+
+    if [[ ! -f ${COMPOSE_OVERRIDE} ]]; then
+        # No override file exists, return false
+        return 1
+    fi
+    # Search for $VarName or ${VarName followed by a word break
+    grep -P "\$\{?\K${VarName}\b" "${COMPOSE_OVERRIDE}"
 }
 
 test_override_var_exists() {
