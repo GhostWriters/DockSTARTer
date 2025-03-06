71cb49f24e497e86ca94cf3459486e3dcac2bdd6
CLHatch <CLHatch@gmail.com>
2025-03-06 05:26:34 -0600

    Update appvars_migrate.sh
    Change variable names.

--- .scripts/appvars_migrate.sh
+++ .scripts/appvars_migrate.sh
@@ -5,23 +5,22 @@ IFS=$'\n\t'
 appvars_migrate() {
     local APPNAME=${1-}
     APPNAME=${APPNAME^^}
+    local appname=${APPNAME,,}
 
-    local FILENAME=${APPNAME,,}
-    local APP_FOLDER="${TEMPLATES_FOLDER}/${FILENAME}"
-    local MIGRATE_FILE="${APP_FOLDER}/${FILENAME}.migrate"
+    local APP_FOLDER="${TEMPLATES_FOLDER}/${appname}"
+    local MIGRATE_FILE="${APP_FOLDER}/${appname}.migrate"
 
     if [[ -f ${MIGRATE_FILE} ]]; then
-        local -a MIGRATE_LINES=()
-
+        local -a MigrateLines=()
         # Read "migrate" file into an array. Remove comments. Convert whitespace to single spaces. Remove empty lines.
-        readarray -t MIGRATE_LINES < <(sed -E 's/#.*$//g ; s/\s+/ /g ; /^\s*$/d' "${MIGRATE_FILE}" || true)
-        for line in "${MIGRATE_LINES[@]}"; do
-            local MIGRATE_TO_VAR
-            local MIGRATE_FROM_VAR
-            MIGRATE_TO_VAR=${line%% *}
-            MIGRATE_FROM_VAR=${line##"${MIGRATE_TO_VAR}" }
+        readarray -t MigrateLines < <(sed -E 's/#.*$//g ; s/\s+/ /g ; /^\s*$/d' "${MIGRATE_FILE}" || true)
+        for line in "${MigrateLines[@]}"; do
+            local ToVar
+            local FromVar
+            ToVar=${line%% *}
+            FromVar=${line##"${ToVar}" }
 
-            run_script 'env_migrate' "${MIGRATE_FROM_VAR}" "${MIGRATE_TO_VAR}"
+            run_script 'env_migrate' "${FromVar}" "${ToVar}"
         done
     fi
 }
