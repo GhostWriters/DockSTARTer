fd58d155f9653472cb8722f7780b0c3079f5a514
CLHatch <CLHatch@gmail.com>
2025-03-06 06:08:53 -0600

    Update appvars_create_all.sh
    Rename variables

--- .scripts/appvars_create_all.sh
+++ .scripts/appvars_create_all.sh
@@ -3,12 +3,12 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 appvars_create_all() {
-    local ADDED_APPS
+    local AddedApps
     run_script 'appvars_migrate_enabled_lines'
-    ADDED_APPS=$(run_script 'app_list_added')
-    if [[ -n ${ADDED_APPS-} ]]; then
+    AddedApps=$(run_script 'app_list_added')
+    if [[ -n ${AddedApps-} ]]; then
         notice "Creating environment variables for added apps. Please be patient, this can take a while."
-        run_script 'appvars_create' "${ADDED_APPS}"
+        run_script 'appvars_create' "${AddedApps}"
     else
         notice "${COMPOSE_ENV} does not contain any added apps."
     fi
