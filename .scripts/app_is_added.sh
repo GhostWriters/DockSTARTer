38c89f76f2a268e59fd687a1f0c1feac45fce9c5
CLHatch <CLHatch@gmail.com>
2025-03-16 01:17:46 -0500

    Update app_is_added.sh
    Simplify the check for an added app.

--- .scripts/app_is_added.sh
+++ .scripts/app_is_added.sh
@@ -3,17 +3,10 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 app_is_added() {
+    # Returns if the passed app is both built in and an APPNAME__ENABLED variable exists
     local APPNAME=${1-}
-    local VAR_FILE=${2:-$COMPOSE_ENV}
-    local APPNAME_REGEX="^\s*${APPNAME^^}"
-    local ADDED_APPS_REGEX="${APPNAME_REGEX}(?=__ENABLED\s*=)"
-
-    if run_script 'app_is_builtin' "${APPNAME}"; then
-        grep --color=never -q -P "${ADDED_APPS_REGEX}" "${VAR_FILE}"
-        return $?
-    else
-        return 1
-    fi
+    local ENABLED_VAR="${APPNAME^^}__ENABLED"
+    run_script 'app_is_builtin' "${APPNAME}" && run_script 'env_var_exists' "${ENABLED_VAR}"
 }
 
 test_app_is_added() {
