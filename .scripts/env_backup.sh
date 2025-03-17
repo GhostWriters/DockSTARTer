15a15cffb347c704e514a09bafd5cc641c70bce8
CLHatch <CLHatch@gmail.com>
2025-03-16 23:53:50 -0500

    Update env_backup.sh
    Change from just backup up the `.env` file to `.env.<time>` to backing up the `.env` file, `env_files` folder, and override compose file to `compose.<time>/`

--- .scripts/env_backup.sh
+++ .scripts/env_backup.sh
@@ -8,19 +8,41 @@ env_backup() {
     DOCKER_VOLUME_CONFIG=$(run_script 'env_get' DOCKER_VOLUME_CONFIG)
     info "Taking ownership of ${DOCKER_VOLUME_CONFIG} (non-recursive)."
     sudo chown "${DETECTED_PUID}":"${DETECTED_PGID}" "${DOCKER_VOLUME_CONFIG}" > /dev/null 2>&1 || true
+
+    local COMPOSE_BACKUPS_FOLDER="${DOCKER_VOLUME_CONFIG}/.compose.backups"
     local BACKUPTIME
     BACKUPTIME=$(date +"%Y%m%d%H%M%S")
-    info "Copying .env file to ${DOCKER_VOLUME_CONFIG}/.compose.backups/.env.${BACKUPTIME}"
-    mkdir -p "${DOCKER_VOLUME_CONFIG}/.compose.backups" || fatal "Failed to make directory.\nFailing command: ${F[C]}mkdir -p \"${DOCKER_VOLUME_CONFIG}/.compose.backups\""
-    cp "${COMPOSE_ENV}" "${DOCKER_VOLUME_CONFIG}/.compose.backups/.env.${BACKUPTIME}" || fatal "Failed to copy backup.\nFailing command: ${F[C]}cp \"${COMPOSE_ENV}\" \"${DOCKER_VOLUME_CONFIG}/.compose.backups/.env.${BACKUPTIME}\""
-    run_script 'set_permissions' "${DOCKER_VOLUME_CONFIG}/.compose.backups"
-    info "Removing old .env backups."
-    find "${DOCKER_VOLUME_CONFIG}/.compose.backups" -type f -name ".env.*" -mtime +3 -delete > /dev/null 2>&1 || warn "Old .env backups not removed."
+    local BACKUP_FOLDER="${COMPOSE_BACKUPS_FOLDER}/${COMPOSE_FOLDER_NAME}.${BACKUPTIME}"
+
+    info "Copying .env file to ${BACKUP_FOLDER}/.env"
+    mkdir -p "${BACKUP_FOLDER}" ||
+        fatal "Failed to make directory.\nFailing command: ${F[C]}mkdir -p \"${BACKUP_FOLDER}\""
+    cp "${COMPOSE_ENV}" "${BACKUP_FOLDER}/" ||
+        fatal "Failed to copy backup.\nFailing command: ${F[C]}cp \"${COMPOSE_ENV}\" \"${BACKUP_FOLDER}/\""
+
+    info "Copying appplication env folder to ${BACKUP_FOLDER}/${APP_ENV_FOLDER_NAME}"
+    cp -r "${APP_ENV_FOLDER}" "${BACKUP_FOLDER}/" ||
+        fatal "Failed to copy backup.\nFailing command: ${F[C]}cp -r \"${APP_ENV_FOLDER}\" \"${BACKUP_FOLDER}/\""
+
+    if [[ -f ${COMPOSE_OVERRIDE} ]]; then
+        info "Copying override file to ${BACKUP_FOLDER}/${COMPOSE_OVERRIDE_NAME}"
+        cp "${COMPOSE_OVERRIDE}" "${BACKUP_FOLDER}/" ||
+            fatal "Failed to copy backup.\nFailing command: ${F[C]}cp \"${COMPOSE_OVERRIDE}\" \"${BACKUP_FOLDER}/\""
+    fi
+
+    run_script 'set_permissions' "${COMPOSE_BACKUPS_FOLDER}"
+
+    info "Removing old compose backups."
+    find "${COMPOSE_BACKUPS_FOLDER}" -type f -name ".env.*" -mtime +3 -delete > /dev/null 2>&1 ||
+        warn "Old .env backups not removed."
+    find "${COMPOSE_BACKUPS_FOLDER}" -type d -name "${COMPOSE_FOLDER_NAME}.*" -mtime +3 -delete > /dev/null 2>&1 ||
+        warn "Old compose backups not removed."
 
     # Backup location has moved
     if [[ -d "${DOCKER_VOLUME_CONFIG}/.env.backups" ]]; then
         info "Removing old backup location."
-        rm -rf "${DOCKER_VOLUME_CONFIG}/.env.backups" || fatal "Failed to remove directory.\nFailing command: ${F[C]}rm -rf \"${DOCKER_VOLUME_CONFIG}/.env.backups\""
+        rm -rf "${DOCKER_VOLUME_CONFIG}/.env.backups" ||
+            fatal "Failed to remove directory.\nFailing command: ${F[C]}rm -rf \"${DOCKER_VOLUME_CONFIG}/.env.backups\""
     fi
 }
 
