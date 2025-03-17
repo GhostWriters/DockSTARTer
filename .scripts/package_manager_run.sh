45260c44e32be36c0e2acc5b3e6cf1de985a76e0
CLHatch <CLHatch@gmail.com>
2025-03-17 18:58:15 -0500

    Update package_manager_run.sh
    Add `dialog` to dependencies list.

--- .scripts/package_manager_run.sh
+++ .scripts/package_manager_run.sh
@@ -16,7 +16,7 @@ package_manager_run() {
         run_script "pm_yum_${ACTION}"
     else
         if [[ ${ACTION} == "install" ]]; then
-            local COMMAND_DEPS=("curl" "git" "grep" "sed" "whiptail")
+            local COMMAND_DEPS=("curl" "git" "grep" "sed" "whiptail" "dialog")
             for COMMAND_DEP in "${COMMAND_DEPS[@]}"; do
                 if [[ -z "$(command -v "${COMMAND_DEP}")" ]]; then
                     fatal "${F[C]}${COMMAND_DEP}${NC} is not available. Please install ${F[C]}${COMMAND_DEP}${NC} and try again."
