9d14e0b75f0179b14625f2b05057c6488e23c842
CLHatch <CLHatch@gmail.com>
2025-03-17 09:11:30 -0500

    Revert "Update app_list_added.sh"
    This reverts commit 3a8f9ff475f1b62f2433103ec934a91790e6488a.

--- .scripts/app_list_added.sh
+++ .scripts/app_list_added.sh
@@ -3,9 +3,15 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 app_list_added() {
-    readarray -t REFERENCED_APPS < <(run_script 'app_list_referenced')
+    local APPNAME_REGEX='^[A-Z][A-Z0-9]*(__[A-Z0-9]+)?'
+    local ADDED_APPS_REGEX="${APPNAME_REGEX}(?=__ENABLED\s*=)"
+    local -a ADDED_APPS
+    local -a BUILTIN_APPS
+
+    #notice "ADDED_APPS_REGEX [ ${ADDED_APPS_REGEX} ]"
+    readarray -t ADDED_APPS < <(grep --color=never -o -P "${ADDED_APPS_REGEX}" "${COMPOSE_ENV}" || true)
     readarray -t BUILTIN_APPS < <(run_script 'app_list_builtin')
-    local -a COMBINED=("${REFERENCED_APPS[@]}" "${BUILTIN_APPS[@]}")
+    local -a COMBINED=("${ADDED_APPS[@]}" "${BUILTIN_APPS[@]}")
     printf "%s\n" "${COMBINED[@]}" | sort | uniq -d
 }
 
