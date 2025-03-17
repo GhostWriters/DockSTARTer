d1cdb9d1e0515e2a9edb0d99d701902c9ec400ae
CLHatch <CLHatch@gmail.com>
2025-03-17 00:04:50 -0500

    Update env_migrate_global.sh
    Call backup function before migrating global variables.  This is one of the first things done.

--- .scripts/env_migrate_global.sh
+++ .scripts/env_migrate_global.sh
@@ -3,7 +3,10 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 env_migrate_global() {
-    # Rename vars
+    # Backup user files
+    run_script 'env_backup'
+
+    # Rename global vars
     run_script 'env_migrate' DOCKERCONFDIR DOCKER_VOLUME_CONFIG
     run_script 'env_migrate' DOCKERGID DOCKER_GID
     run_script 'env_migrate' DOCKERHOSTNAME DOCKER_HOSTNAME
