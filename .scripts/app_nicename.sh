395bda55f0139bef5350ea32fae8b87b9f390923
CLHatch <CLHatch@gmail.com>
2025-03-17 02:24:40 -0500

    Update app_nicename.sh
    All multiple arguments to be passed to `app_nicename`

--- .scripts/app_nicename.sh
+++ .scripts/app_nicename.sh
@@ -3,15 +3,18 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 app_nicename() {
-    # Return the "NiceName" of the appname passed. If there is no "NiceName", return the lower case "appname"
-    local appname=${1-}
-    appname=${appname,,}
-    local LABELS_FILE="${TEMPLATES_FOLDER}/${appname}/${appname}.labels.yml"
-    if [[ -f ${LABELS_FILE} ]]; then
-        grep --color=never -Po "\scom\.dockstarter\.appinfo\.nicename: \K.*" "${LABELS_FILE}" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo "${appname}"
-    else
-        echo "${appname}"
-    fi
+    # Return the "NiceName" of the appname(s) passed. If there is no "NiceName", return the lower case "appname"
+    local AppList
+    AppList=$(xargs -n 1 <<< "$*")
+    for APPNAME in ${AppList}; do
+        local appname=${APPNAME,,}
+        local LABELS_FILE="${TEMPLATES_FOLDER}/${appname}/${appname}.labels.yml"
+        if [[ -f ${LABELS_FILE} ]]; then
+            grep --color=never -Po "\scom\.dockstarter\.appinfo\.nicename: \K.*" "${LABELS_FILE}" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo "${appname}"
+        else
+            echo "${appname}"
+        fi
+    done
 
 }
 
