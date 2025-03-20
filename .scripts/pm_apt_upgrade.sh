07a52bd90a0e00da61cc42f7b37f9479b25321cf
CLHatch <CLHatch@gmail.com>
2025-03-20 00:20:10 -0500

    Update pm_apt_upgrade.sh
    Test delayed expansion of "DIALOG_COMMAND" to "COMMAND" to use in other functions.

--- .scripts/pm_apt_upgrade.sh
+++ .scripts/pm_apt_upgrade.sh
@@ -6,13 +6,14 @@ pm_apt_upgrade() {
     local Title="Upgrade Packages"
     if [[ ${CI-} != true ]]; then
         notice "Upgrading packages. Please be patient, this can take a while."
+        local DIALOG_COMMAND="COMMAND"
         local COMMAND='sudo apt-get -y dist-upgrade'
         local REDIRECT="> /dev/null 2>&1"
         if [[ -n ${VERBOSE-} ]] || run_script 'question_prompt' "${PROMPT:-CLI}" N "Would you like to display the command output?" "${Title}"; then
             if [[ ${PROMPT:-CLI} == CLI ]]; then
                 REDIRECT=""
             else
-                REDIRECT="| dialog --fb --clear --backtitle \"${BACKTITLE}\" --title \"${Title}\" --programbox \"${COMMAND}\" -1 -1"
+                REDIRECT="| dialog --fb --clear --backtitle \"${BACKTITLE}\" --title \"${Title}\" --programbox \"${!DIALOG_COMMAND}\" -1 -1"
             fi
         fi
         eval "${COMMAND} ${REDIRECT}" || fatal "Failed to upgrade packages from apt.\nFailing command: ${F[C]}${COMMAND}"
