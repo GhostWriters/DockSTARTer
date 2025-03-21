6797763f2f2d7751c318fac78d6dd39a5eed0643
CLHatch <CLHatch@gmail.com>
2025-03-20 22:02:27 -0500

    Update menu_main.sh
    Update handling of `dialog` processing.

--- .scripts/menu_main.sh
+++ .scripts/menu_main.sh
@@ -5,14 +5,15 @@ IFS=$'\n\t'
 menu_main() {
     local Title="Main Menu"
     local MainOpts=()
-    MainOpts+=("Configuration " "Setup and start applications")
-    MainOpts+=("Install Dependencies " "Install required components")
-    MainOpts+=("Update DockSTARTer " "Get the latest version of DockSTARTer")
-    MainOpts+=("Prune Docker System " "Remove all unused containers, networks, volumes, images and build cache")
+    MainOpts+=("Configuration" "Setup and start applications")
+    MainOpts+=("Install Dependencies" "Install required components")
+    MainOpts+=("Update DockSTARTer" "Get the latest version of DockSTARTer")
+    MainOpts+=("Prune Docker System" "Remove all unused containers, networks, volumes, images and build cache")
 
+    local DIALOG_BUTTON_PRESSED
     local MainChoice
     if [[ ${CI-} == true ]]; then
-        MainChoice="Cancel"
+        DIALOG_BUTTON_PRESSED=${DIALOG_CANCEL}
     else
         local -a MainChoiceDialog=(
             --clear
@@ -21,31 +22,50 @@ menu_main() {
             --menu "What would you like to do?" 0 0 0
             "${MainOpts[@]}"
         )
-        MainChoice=$(dialog "${MainChoiceDialog[@]}" 3>&1 1>&2 2>&3 || echo "Cancel")
-        clear
+        MainChoice=$(dialog "${MainChoiceDialog[@]}" 3>&1 1>&2 2>&3)
+        DIALOG_BUTTON_PRESSED=$?
     fi
-
-    case "${MainChoice}" in
-        "Configuration ")
-            run_script 'menu_config' || run_script 'menu_main'
+    case ${DIALOG_BUTTON_PRESSED} in
+        "${DIALOG_OK}")
+            case "${MainChoice}" in
+                "Configuration")
+                    clear
+                    run_script 'menu_config' || run_script 'menu_main'
+                    ;;
+                "Install Dependencies")
+                    clear
+                    run_script 'run_install' || run_script 'menu_main'
+                    ;;
+                "Update DockSTARTer")
+                    clear
+                    run_script 'update_self' || run_script 'menu_main'
+                    ;;
+                "Prune Docker System")
+                    clear
+                    run_script 'docker_prune' || run_script 'menu_main'
+                    ;;
+                *)
+                    clear
+                    error "Invalid Option"
+                    ;;
+            esac
             ;;
-        "Install Dependencies ")
-            run_script 'run_install' || run_script 'menu_main'
-            ;;
-        "Update DockSTARTer ")
-            run_script 'update_self' || run_script 'menu_main'
-            ;;
-        "Prune Docker System ")
-            run_script 'docker_prune' || run_script 'menu_main'
-            ;;
-        "Cancel")
+        "${DIALOG_CANCEL}")
+            clear
             info "Exiting DockSTARTer."
             return
             ;;
         *)
-            error "Invalid Option"
+            if [[ -n ${DIALOG_BUTTONS[DIALOG_BUTTON_PRESSED]-} ]]; then
+                clear
+                fatal "Unexpected dialog button '${DIALOG_BUTTONS[DIALOG_BUTTON_PRESSED]}' pressed."
+            else
+                clear
+                fatal "Unexpected dialog button value'${DIALOG_BUTTON_PRESSED}' pressed."
+            fi
             ;;
     esac
+
 }
 
 test_menu_main() {
