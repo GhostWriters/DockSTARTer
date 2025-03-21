5943301d7d79663c9cb489bab4f23e4364012109
CLHatch <CLHatch@gmail.com>
2025-03-20 19:37:52 -0500

    Update main.sh
    Change a couple of values for DIALOG button values. `-1` can be problematic for `ERROR`, and `HELP` and `ITEM_HELP` shouldn't be the same value.
    
    Create a `DIALOG_BUTTONS` array that returns the name of the buttons.

--- main.sh
+++ main.sh
@@ -99,12 +99,22 @@ declare -rx DIALOGRC="${SCRIPTPATH}/.dialogrc"
 declare -rx BACKTITLE="DockSTARTer"
 declare -rx DIALOGOPTS="--backtitle ${BACKTITLE}"
 declare -rx DIALOG_CANCEL=1
-declare -rx DIALOG_ERROR=-1
+declare -rx DIALOG_ERROR=254
 declare -rx DIALOG_ESC=255
 declare -rx DIALOG_EXTRA=3
 declare -rx DIALOG_HELP=2
-declare -rx DIALOG_ITEM_HELP=2
+declare -rx DIALOG_ITEM_HELP=20
 declare -rx DIALOG_OK=0
+readonly -A DIALOG_BUTTONS=(
+    [DIALOG_CANCEL]="CANCEL"
+    [DIALOG_ERROR]="ERROR"
+    [DIALOG_ESC]="ESC"
+    [DIALOG_EXTRA]="EXTRA"
+    [DIALOG_HELP]="HELP"
+    [DIALOG_ITEM_HELP]="ITEM_HELP"
+    [DIALOG_OK]="OK"
+)
+export DIALOG_BUTTONS
 
 # Cleanup Function
 cleanup() {
