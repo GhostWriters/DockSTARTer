d9a116c34353753aea36fdd0b6f058f5bed4ccb3
CLHatch <CLHatch@gmail.com>
2025-03-21 23:32:03 -0500

    Create env_get_literal.sh
    Gets the literal value of a variable, including quotes.
    
    It will also include any comments on the line, just returning everything after the first `=`.

---
+++ .scripts/env_get_literal.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+env_get_literal() {
+    # env_get_literal GET_VAR [VAR_FILE]
+    # env_get_literal APPNAME:GET_VAR
+    #
+    # The string returned will be the literal value after `=`, including quotes and comments
+    #
+    # Returns the variable "GET_VAR"  If no "VAR_FILE" is given, uses the global .env file
+    # If "APPNAME:" is provided, gets variable from "env_files/appname.env"
+    local GET_VAR=${1-}
+    local VAR_FILE=${2:-$COMPOSE_ENV}
+    if [[ ${GET_VAR} =~ ^[A-Za-z0-9_]+: ]]; then
+        # GET_VAR is in the form of "APPNAME:VARIABLE", set new file to use
+        local APPNAME=${GET_VAR%%:*}
+        VAR_FILE="${APP_ENV_FOLDER}/${APPNAME,,}.env"
+        GET_VAR=${GET_VAR#"${APPNAME}:"}
+    fi
+    if [[ -f ${VAR_FILE} ]]; then
+        grep --color=never -Po "^\s*${GET_VAR}\s*=\K.*" "${VAR_FILE}" | tail -1 || true
+    else
+        # VAR_FILE does not exist, give a warning
+        warn "${VAR_FILE} does not exist."
+    fi
+
+}
+
+test_env_get_literal() {
+    run_script 'appvars_create' WATCHTOWER
+    run_script 'env_get_literal' WATCHTOWER__ENABLED
+    run_script 'env_get_literal' WATCHTOWER:WATCHTOWER_NOTIFICATIONS
+    run_script 'appvars_purge' WATCHTOWER
+}
