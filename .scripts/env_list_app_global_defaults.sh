ff1c8c6e5fcd2336be0b0e57f556bf87d044e433
CLHatch <CLHatch@gmail.com>
2025-03-22 02:41:55 -0500

    Create env_list_app_global_defaults.sh
    Return the list of "global" default variables for a builtin app (the ones that go in `.env`).

---
+++ .scripts/env_list_app_global_defaults.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+env_list_app_global_defaults() {
+    local APPNAME=${1-}
+    local appname=${APPNAME,,}
+    run_script 'env_var_list' "${TEMPLATES_FOLDER}/${appname}/.env"
+}
+
+test_env_list_app_global_defaults() {
+    run_script 'env_list_app_global_defaults' WATCHTOWER
+}
