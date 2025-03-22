712ce80dfca9c26887384c2e2da5b876b8291f6e
CLHatch <CLHatch@gmail.com>
2025-03-22 04:25:13 -0500

    Update menu_config.sh
    Remember last choice in the menu.
    
    Always run `yml_merge`.

--- .scripts/menu_config.sh
+++ .scripts/menu_config.sh
@@ -3,85 +3,87 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 menu_config() {
-    local Title="Configuration Menu"
-    local ConfigOpts=()
-    ConfigOpts+=("Full Setup" "This goes through all menus below. Recommended for first run")
-    ConfigOpts+=("Select Apps" "Select which apps to run. Previously enabled apps are remembered")
-    ConfigOpts+=("Set App Variables" "Review and adjust variables for enabled apps")
-    ConfigOpts+=("Set Global Variables" "Review and adjust global variables")
-
-    local DIALOG_BUTTON_PRESSED
-    local ConfigChoice
     if [[ ${CI-} == true ]]; then
-        DIALOG_BUTTON_PRESSED=${DIALOG_CANCEL}
-    else
-        local -a ConfigChoiceDialog=(
-            --clear
-            --stdout
-            --title "${Title}"
-            --cancel-button "Exit"
-            --menu "What would you like to do?" 0 0 0
-            "${ConfigOpts[@]}"
-        )
-        DIALOG_BUTTON_PRESSED=0 && ConfigChoice=$(dialog "${ConfigChoiceDialog[@]}") || DIALOG_BUTTON_PRESSED=$?
+        return
     fi
-    case ${DIALOG_BUTTON_PRESSED} in
-        "${DIALOG_OK}")
-            case "${ConfigChoice}" in
-                "Full Setup")
-                    clear
-                    run_script 'env_migrate_global'
-                    run_script 'env_update'
-                    run_script 'config_global'
-                    run_script 'menu_app_select'
-                    run_script 'menu_config_apps'
-                    run_script 'yml_merge'
-                    run_script 'docker_compose'
-                    ;;
-                "Set Global Variables")
-                    clear
-                    run_script 'env_migrate_global'
-                    run_script 'env_update'
-                    run_script 'config_global'
-                    run_script 'yml_merge'
-                    run_script 'docker_compose'
-                    ;;
-                "Select Apps")
-                    clear
-                    run_script 'env_migrate_global'
-                    run_script 'env_update'
-                    if run_script 'menu_app_select'; then
+
+    local Title="Configuration Menu"
+    local ConfigOpts=(
+        "Full Setup" "This goes through all menus below. Recommended for first run"
+        "Select Apps" "Select which apps to run. Previously enabled apps are remembered"
+        "Set App Variables" "Review and adjust variables for enabled apps"
+        "Set Global Variables" "Review and adjust global variables"
+    )
+    local -a ConfigChoiceDialog=(
+        --clear
+        --stdout
+        --title "${Title}"
+        --cancel-button "Exit"
+        --menu "What would you like to do?" 0 0 0
+        "${ConfigOpts[@]}"
+    )
+
+    local LastConfigChoice=""
+    while true; do
+        local ConfigChoice
+        local ConfigDialogButtonPressed=0
+        ConfigChoice=$(dialog --default-item "${LastConfigChoice}" "${ConfigChoiceDialog[@]}") || ConfigDialogButtonPressed=$?
+        LastConfigChoice=${ConfigChoice}
+        case ${ConfigDialogButtonPressed} in
+            "${DIALOG_OK}")
+                case "${ConfigChoice}" in
+                    "Full Setup")
+                        clear
+                        run_script 'env_migrate_global'
+                        run_script 'env_update'
+                        run_script 'config_global'
+                        run_script 'menu_app_select'
+                        run_script 'menu_config_apps'
                         run_script 'yml_merge'
                         run_script 'docker_compose'
-                    fi
-                    ;;
-                "Set App Variables")
+                        ;;
+                    "Set Global Variables")
+                        clear
+                        run_script 'env_migrate_global'
+                        run_script 'env_update'
+                        run_script 'config_global'
+                        run_script 'yml_merge'
+                        run_script 'docker_compose'
+                        ;;
+                    "Select Apps")
+                        clear
+                        run_script 'env_migrate_global'
+                        run_script 'env_update'
+                        run_script 'yml_merge'
+                        run_script 'docker_compose'
+                        ;;
+                    "Set App Variables")
+                        clear
+                        run_script 'env_migrate_global'
+                        run_script 'env_update'
+                        run_script 'menu_config_apps'
+                        run_script 'yml_merge'
+                        run_script 'docker_compose'
+                        ;;
+                    *)
+                        error "Invalid Option"
+                        ;;
+                esac
+                ;;
+            "${DIALOG_CANCEL}" | "${DIALOG_ESC}")
+                return
+                ;;
+            *)
+                if [[ -n ${DIALOG_BUTTONS[$ConfigDialogButtonPressed]-} ]]; then
                     clear
-                    run_script 'env_migrate_global'
-                    run_script 'env_update'
-                    run_script 'menu_config_apps'
-                    run_script 'yml_merge'
-                    run_script 'docker_compose'
-                    ;;
-                *)
-                    error "Invalid Option"
-                    ;;
-            esac
-            ;;
-        "${DIALOG_CANCEL}" | "${DIALOG_ESC}")
-            info "Returning to Main Menu."
-            return 1
-            ;;
-        *)
-            if [[ -n ${DIALOG_BUTTONS[$DIALOG_BUTTON_PRESSED]-} ]]; then
-                clear
-                fatal "Unexpected dialog button '${DIALOG_BUTTONS[$DIALOG_BUTTON_PRESSED]}' pressed."
-            else
-                clear
-                fatal "Unexpected dialog button value'${DIALOG_BUTTON_PRESSED}' pressed."
-            fi
-            ;;
-    esac
+                    fatal "Unexpected dialog button '${DIALOG_BUTTONS[$ConfigDialogButtonPressed]}' pressed."
+                else
+                    clear
+                    fatal "Unexpected dialog button value'${ConfigDialogButtonPressed}' pressed."
+                fi
+                ;;
+        esac
+    done
 }
 
 test_menu_config() {
