f80e4e80aeabd2fd27b4758967fd0c8dda705d44
CLHatch <CLHatch@gmail.com>
2025-03-21 22:23:23 -0500

    Update menu_config_apps.sh
    Let the user pick from a list of added apps to set their variables. `CANCEL` or `ESC` returns to the previous menu.

--- .scripts/menu_config_apps.sh
+++ .scripts/menu_config_apps.sh
@@ -6,9 +6,49 @@ menu_config_apps() {
     info "Configuring .env variables for enabled apps."
     run_script 'appvars_create_all'
 
-    while IFS= read -r APPNAME; do
-        run_script 'menu_app_vars' "${APPNAME}" || return 1
-    done < <(run_script 'app_list_enabled')
+    local AddedApps
+    AddedApps=$(run_script 'app_nice_name' "$(run_script 'app_list_added')")
+    if [[ -z ${AddedApps} ]]; then
+        dialog --msgbox "There are no apps added."
+        return
+    fi
+    local -a AppOptions
+    for AppName in ${AddedApps}; do
+        local AppDescription
+        AppDescription="$(run_script 'app_description' "${AppName}")"
+        AppOptions+=("${AppName}" "${AppDescription}")
+    done
+    local -a AppChoiceDialog
+    AppChoiceDialog=(
+        --clear
+        --stdout
+        --title "Set App Variables"
+        --cancel-button "Exit"
+        --menu "Select the application to configure" 0 0 0
+        "${AppOptions[@]}"
+    )
+    while true; do
+        local AppChoice
+        local AppChoiceButtonPressed=0
+        AppChoice=$(dialog "${AppChoiceDialog[@]}") || AppChoiceButtonPressed=$?
+        case ${AppChoiceButtonPressed} in
+            "${DIALOG_OK}")
+                run_script 'menu_app_vars' "${AppChoice}"
+                ;;
+            "${DIALOG_CANCEL}" | "${DIALOG_ESC}")
+                return
+                ;;
+            *)
+                if [[ -n ${DIALOG_BUTTONS[$AppChoiceButtonPressed]-} ]]; then
+                    clear
+                    fatal "Unexpected dialog button '${DIALOG_BUTTONS[$AppChoiceButtonPressed]}' pressed."
+                else
+                    clear
+                    fatal "Unexpected dialog button value'${AppChoiceButtonPressed}' pressed."
+                fi
+                ;;
+        esac
+    done
 }
 
 test_menu_config_apps() {
