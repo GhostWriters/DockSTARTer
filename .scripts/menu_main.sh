eecefff7e168f6971f0027958e661c2d93999280
CLHatch <CLHatch@gmail.com>
2025-03-22 04:54:20 -0500

    Update menu_main.sh
    Remember last choice in menu.
    
    Remove unnecessary recursion, use an infinite loop with "return"  instead.

--- .scripts/menu_main.sh
+++ .scripts/menu_main.sh
@@ -3,69 +3,74 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 menu_main() {
-    local Title="Main Menu"
-    local MainOpts=()
-    MainOpts+=("Configuration" "Setup and start applications")
-    MainOpts+=("Install Dependencies" "Install required components")
-    MainOpts+=("Update DockSTARTer" "Get the latest version of DockSTARTer")
-    MainOpts+=("Prune Docker System" "Remove all unused containers, networks, volumes, images and build cache")
-
-    local DIALOG_BUTTON_PRESSED
-    local MainChoice
     if [[ ${CI-} == true ]]; then
-        DIALOG_BUTTON_PRESSED=${DIALOG_CANCEL}
-    else
-        local -a MainChoiceDialog=(
-            --clear
-            --stdout
-            --title "${Title}"
-            --cancel-button "Exit"
-            --menu "What would you like to do?" 0 0 0
-            "${MainOpts[@]}"
-        )
-        DIALOG_BUTTON_PRESSED=0 && MainChoice=$(dialog "${MainChoiceDialog[@]}") || DIALOG_BUTTON_PRESSED=$?
+        return
     fi
-    case ${DIALOG_BUTTON_PRESSED} in
-        "${DIALOG_OK}")
-            case "${MainChoice}" in
-                "Configuration")
-                    clear
-                    run_script 'menu_config' || run_script 'menu_main'
-                    ;;
-                "Install Dependencies")
-                    clear
-                    run_script 'run_install' || run_script 'menu_main'
-                    ;;
-                "Update DockSTARTer")
-                    clear
-                    run_script 'update_self' || run_script 'menu_main'
-                    ;;
-                "Prune Docker System")
-                    clear
-                    run_script 'docker_prune' || run_script 'menu_main'
-                    ;;
-                *)
-                    clear
-                    error "Invalid Option"
-                    ;;
-            esac
-            ;;
-        "${DIALOG_CANCEL}" | "${DIALOG_ESC}")
-            clear
-            info "Exiting DockSTARTer."
-            return
-            ;;
-        *)
-            if [[ -n ${DIALOG_BUTTONS[$DIALOG_BUTTON_PRESSED]-} ]]; then
-                clear
-                fatal "Unexpected dialog button '${DIALOG_BUTTONS[$DIALOG_BUTTON_PRESSED]}' pressed."
-            else
-                clear
-                fatal "Unexpected dialog button value'${DIALOG_BUTTON_PRESSED}' pressed."
-            fi
-            ;;
-    esac
 
+    local Title="Main Menu"
+    local MainOpts=(
+        "Configuration" "Setup and start applications"
+        "Install Dependencies" "Install required components"
+        "Update DockSTARTer" "Get the latest version of DockSTARTer"
+        "Prune Docker System" "Remove all unused containers, networks, volumes, images and build cache"
+    )
+    local -a MainChoiceDialog=(
+        --clear
+        --stdout
+        --title "${Title}"
+        --cancel-button "Exit"
+        --menu "What would you like to do?" 0 0 0
+        "${MainOpts[@]}"
+    )
+
+    local LastMainChoice=""
+    while true; do
+        local MainChoice
+        local MainDialogButtonPressed=0
+        MainDialogButtonPressed=0
+        MainChoice=$(dialog --default-item "${LastMainChoice}" "${MainChoiceDialog[@]}") || MainDialogButtonPressed=$?
+        LastMainChoice=${MainChoice}
+        case ${MainDialogButtonPressed} in
+            "${DIALOG_OK}")
+                case "${MainChoice}" in
+                    "Configuration")
+                        clear
+                        run_script 'menu_config'
+                        ;;
+                    "Install Dependencies")
+                        clear
+                        run_script 'run_install'
+                        ;;
+                    "Update DockSTARTer")
+                        clear
+                        run_script 'update_self'
+                        ;;
+                    "Prune Docker System")
+                        clear
+                        run_script 'docker_prune'
+                        ;;
+                    *)
+                        clear
+                        error "Invalid Option"
+                        ;;
+                esac
+                ;;
+            "${DIALOG_CANCEL}" | "${DIALOG_ESC}")
+                clear
+                info "Exiting DockSTARTer."
+                return
+                ;;
+            *)
+                if [[ -n ${DIALOG_BUTTONS[$MainDialogButtonPressed]-} ]]; then
+                    clear
+                    fatal "Unexpected dialog button '${DIALOG_BUTTONS[$MainDialogButtonPressed]}' pressed."
+                else
+                    clear
+                    fatal "Unexpected dialog button value'${MainDialogButtonPressed}' pressed."
+                fi
+                ;;
+        esac
+    done
 }
 
 test_menu_main() {
