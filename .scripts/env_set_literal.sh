60d74cf964e2b82fe351be25a45e4342fd3f843d
CLHatch <CLHatch@gmail.com>
2025-03-21 23:39:57 -0500

    Create env_set_literal.sh
    Sets a variable to a literal value.  No processing is done on the value, it will include any quotes as is if they are included.

---
+++ .scripts/env_set_literal.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+env_set_literal() {
+    # env_set_literal SET_VAR NEW_VAL [VAR_FILE]
+    # env_set_literal APPNAME:SET_VAR NEW_VAL
+    #
+    # Sets the variable "SET_VAR"  If no "VAR_FILE" is given, uses the global .env file
+    # If "APPNAME:" is provided, gets variable from "env_files/appname.env"
+    local SET_VAR=${1-}
+    local NEW_VAL=${2-}
+    local VAR_FILE=${3:-$COMPOSE_ENV}
+    if [[ ${SET_VAR} =~ ^[A-Za-z0-9_]+: ]]; then
+        # SET_VAR is in the form of "APPNAME:VARIABLE", set new file to use
+        local APPNAME=${SET_VAR%%:*}
+        VAR_FILE="${APP_ENV_FOLDER}/${APPNAME,,}.env"
+        SET_VAR=${SET_VAR#"${APPNAME}:"}
+    fi
+    if [[ ! -f ${VAR_FILE} ]]; then
+        # VAR_FILE does not exist, create it
+        mkdir -p "${VAR_FILE%/*}" && touch "${VAR_FILE}"
+    fi
+    sed -i "/^\s*${SET_VAR}\s*=/d" "${VAR_FILE}" || true
+    echo "${SET_VAR}=${NEW_VAL}" >> "${VAR_FILE}" || fatal "Failed to set ${SET_VAR}=${NEW_VAL}\nFailing command: ${F[C]} \"echo ${SET_VAR}=${NEW_VAL}\" >> \"${VAR_FILE}\""
+}
+
+test_env_set_literal() {
+    run_script 'appvars_create' WATCHTOWER
+    run_script 'env_set_literal' WATCHTOWER__ENABLED false
+    run_script 'env_set_literal' WATCHTOWER:WATCHTOWER_NOTIFICATIONS newvalue
+    run_script 'env_get_literal' WATCHTOWER__ENABLED
+    run_script 'env_get_literal' WATCHTOWER:WATCHTOWER_NOTIFICATIONS
+    run_script 'appvars_purge' WATCHTOWER
+}
