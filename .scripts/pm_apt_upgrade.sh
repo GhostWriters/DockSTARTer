bfcf878813d65886d2c78930935e9abee46f6564
CLHatch <CLHatch@gmail.com>
2025-03-31 23:39:57 -0500

    Revert "Update pm_apt_upgrade.sh"
    This reverts commit 7c85930eabc04d532dc7539166a4842401a87832.

--- .scripts/pm_apt_upgrade.sh
+++ .scripts/pm_apt_upgrade.sh
@@ -10,7 +10,7 @@ pm_apt_upgrade() {
         local REDIRECT="> /dev/null 2>&1"
         if [[ -n ${VERBOSE-} ]] || run_script 'question_prompt' N "Would you like to display the command output?" "${Title}"; then
             if [[ ${PROMPT:-CLI} == GUI && -t 1 ]]; then
-                REDIRECT="|& run_script 'dialog_output' \"${Title}\" \"\${COMMAND}\""
+                REDIRECT="|& dialog --begin 2 2 --title \"${Title}\" --programbox \"\${COMMAND}\" $((LINES - 4)) $((COLUMNS - 5))"
             else
                 REDIRECT=""
             fi
