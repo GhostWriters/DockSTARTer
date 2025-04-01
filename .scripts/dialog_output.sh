9e439b3f9fb2c0312063ea55678ac66cd568298f
CLHatch <CLHatch@gmail.com>
2025-03-31 23:24:17 -0500

    Create dialog_output.sh
    Create a generic function to pipe the output of commands to, with optional "Title", "SubTitle" and "TimeOut" arguments.

---
+++ .scripts/dialog_output.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+dialog_output() {
+    local Title=${1:-}
+    local SubTitle=${2:-}
+    local TimeOut=${3:-0}
+    dialog --begin 2 2 --timeout "${TimeOut}" --title "${Title}" --programbox "${SubTitle}" $((LINES - 4)) $((COLUMNS - 5))
+}
+
+test_dialog_output() {
+    # run_script 'dialog_output'
+    warn "CI does not test dialog_output."
+}
