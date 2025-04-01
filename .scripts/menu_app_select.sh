e219ba6f2cda679800b06b5f377361214a49f9d0
CLHatch <CLHatch@gmail.com>
2025-03-31 21:19:40 -0500

    Update menu_app_select.sh
    Change command output in dialog box to show in a "window", instead of full-screen.

--- .scripts/menu_app_select.sh
+++ .scripts/menu_app_select.sh
@@ -60,7 +60,7 @@ menu_app_select() {
 
                 run_script 'appvars_purge_all'
                 run_script 'env_update'
-            } |& dialog --timeout 1 --title "${BACKTITLE}" --programbox "${Title}" -1 -1
+            } |& dialog --timeout 1 --begin 2 2 --title "${Title}" --programbox "Enabling Selected Applications" $((LINES - 4)) $((COLUMNS - 5))
             return 0
             ;;
         "${DIALOG_CANCEL}" | "${DIALOG_ESC}")
