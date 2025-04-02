8de9b3b7639e135e7df2f12a5f2a1040bea4b904
CLHatch <CLHatch@gmail.com>
2025-04-02 08:51:47 -0500

    Update dialog_output.sh

--- .scripts/dialog_output.sh
+++ .scripts/dialog_output.sh
@@ -6,7 +6,7 @@ dialog_output() {
     local Title=${1:-}
     local SubTitle=${2:-}
     local TimeOut=${3:-0}
-    dialog --title "dialog_output" --msgbox "Title=${Title}\nSubTitle=${SubTitle}\nTimeOut=${TimeOut}\n" 0 0
+    #dialog --title "dialog_output" --msgbox "Title=${Title}\nSubTitle=${SubTitle}\nTimeOut=${TimeOut}\n" 0 0
     dialog --begin 2 2 --timeout "${TimeOut}" --title "${Title}" --programbox "${SubTitle}" $((LINES - 4)) $((COLUMNS - 5))
 }
 
