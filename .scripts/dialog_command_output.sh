9f5e4afa9550914d655cb40d93c1deeb85d79807
CLHatch <CLHatch@gmail.com>
2025-04-07 10:39:39 -0500

    Update dialog_command_output.sh

--- .scripts/dialog_command_output.sh
+++ .scripts/dialog_command_output.sh
@@ -8,7 +8,7 @@ dialog_command_output() {
     local TimeOut=${3:-0}
     shift 3
     #local CommandLine=${*:-true}
-    dialog --title "dialog_command_output" --msgbox "CommandLine=$*\nTitle=${Title}\nSubTitle=${SubTitle}\nTimeOut=${TimeOut}\n" 0 0
+    dialog --title "dialog_command_output" --msgbox "Title=${Title}\nSubTitle=${SubTitle}\nTimeOut=${TimeOut}\nCommandLine=$*\n" 0 0
     if [[ -t 1 ]]; then
         {
             eval "$@"
