2ec8b35a48065c76ad6be8f0cd0eaa2be7044793
CLHatch <CLHatch@gmail.com>
2025-04-07 10:25:19 -0500

    Update dialog_command_output.sh

--- .scripts/dialog_command_output.sh
+++ .scripts/dialog_command_output.sh
@@ -10,8 +10,12 @@ dialog_command_output() {
     #local CommandLine=${*:-true}
     dialog --title "dialog_command_output" --msgbox "CommandLine=$*\nTitle=${Title}\nSubTitle=${SubTitle}\nTimeOut=${TimeOut}\n" 0 0
     if [[ -t 1 ]]; then
-        eval "$* |& run_script 'dialog_output' \"${Title}\" \"${SubTitle}\" \"${TimeOut}\""
-        #return "${PIPESTATUS[0]}"
+        local status
+        {
+            eval $*
+            status=$?
+        } |& run_script 'dialog_output' "${Title}" "${SubTitle}" "${TimeOut}"
+        return ${status}
     else
         "$@"
     fi
