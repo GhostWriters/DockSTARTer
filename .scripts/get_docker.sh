bda3969a9a1a740fc48aedb021f5e31fcb878bf4
CLHatch <CLHatch@gmail.com>
2025-04-08 03:09:33 -0500

    Update get_docker.sh
    Also show the output of the `curl` command if the user chooses to show command output.

--- .scripts/get_docker.sh
+++ .scripts/get_docker.sh
@@ -4,17 +4,17 @@ IFS=$'\n\t'
 
 get_docker() {
     Title="Install Docker"
-    # https://github.com/docker/docker-install
     notice "Installing docker. Please be patient, this can take a while."
-    local MKTEMP_GET_DOCKER
-    MKTEMP_GET_DOCKER=$(mktemp) || fatal "Failed to create temporary docker install script.\nFailing command: ${F[C]}mktemp"
-    info "Downloading docker install script."
-    curl -fsSL https://get.docker.com -o "${MKTEMP_GET_DOCKER}" > /dev/null 2>&1 || fatal "Failed to get docker install script.\nFailing command: ${F[C]}curl -fsSL https://get.docker.com -o \"${MKTEMP_GET_DOCKER}\""
-    info "Running docker install script."
     local REDIRECT="> /dev/null 2>&1"
     if run_script 'question_prompt' N "Would you like to display the command output?" "${Title}" "${VERBOSE:+Y}"; then
         REDIRECT=""
     fi
+    # https://github.com/docker/docker-install
+    local MKTEMP_GET_DOCKER
+    MKTEMP_GET_DOCKER=$(mktemp) || fatal "Failed to create temporary docker install script.\nFailing command: ${F[C]}mktemp"
+    info "Downloading docker install script."
+    eval "curl -fsSL https://get.docker.com -o \"${MKTEMP_GET_DOCKER}\" ${REDIRECT}" || fatal "Failed to get docker install script.\nFailing command: ${F[C]}curl -fsSL https://get.docker.com -o \"${MKTEMP_GET_DOCKER}\""
+    info "Running docker install script."
     eval "sh ${MKTEMP_GET_DOCKER} ${REDIRECT}" || fatal "Failed to install docker.\nFailing command: ${F[C]}sh ${MKTEMP_GET_DOCKER}"
     rm -f "${MKTEMP_GET_DOCKER}" || warn "Failed to remove temporary docker install script.\nFailing command: ${F[C]}rm -f \"${MKTEMP_GET_DOCKER}\""
 }
