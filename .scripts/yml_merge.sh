7845d5abca8390be0632a6b5474f588a431a7c81
CLHatch <CLHatch@gmail.com>
2025-05-05 13:00:02 -0500

    Update yml_merge.sh
    Use the app `nicename` for notices

--- .scripts/yml_merge.sh
+++ .scripts/yml_merge.sh
@@ -13,44 +13,46 @@ commands_yml_merge() {
     local ENABLED_APPS
     ENABLED_APPS=$(run_script 'app_list_enabled')
     for APPNAME in ${ENABLED_APPS-}; do
-        local FILENAME=${APPNAME,,}
-        local APP_FOLDER="${TEMPLATES_FOLDER}/${FILENAME}"
+        local appname=${APPNAME,,}
+        local AppName
+        AppName=$(run_script 'app_nicename' "${APPNAME}")
+        local APP_FOLDER="${TEMPLATES_FOLDER}/${appname}"
         if [[ -d ${APP_FOLDER}/ ]]; then
-            if [[ -f ${APP_FOLDER}/${FILENAME}.yml ]]; then
+            if [[ -f ${APP_FOLDER}/${appname}.yml ]]; then
                 if run_script 'app_is_depreciated' "${APPNAME}"; then
-                    warn "${APPNAME} IS DEPRECATED!"
+                    warn "${AppName} IS DEPRECATED!"
                     warn "Please edit ${COMPOSE_ENV} and set ${APPNAME}__ENABLED to false."
                     continue
                 fi
-                if [[ ! -f ${APP_FOLDER}/${FILENAME}.${ARCH}.yml ]]; then
-                    error "${APP_FOLDER}/${FILENAME}.${ARCH}.yml does not exist."
+                if [[ ! -f ${APP_FOLDER}/${appname}.${ARCH}.yml ]]; then
+                    error "${APP_FOLDER}/${appname}.${ARCH}.yml does not exist."
                     continue
                 fi
-                COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${FILENAME}.${ARCH}.yml"
+                COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${appname}.${ARCH}.yml"
                 local APPNETMODE
                 APPNETMODE=$(run_script 'env_get' "${APPNAME}__NETWORK_MODE")
                 if [[ -z ${APPNETMODE} ]] || [[ ${APPNETMODE} == "bridge" ]]; then
-                    if [[ -f ${APP_FOLDER}/${FILENAME}.hostname.yml ]]; then
-                        COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${FILENAME}.hostname.yml"
+                    if [[ -f ${APP_FOLDER}/${appname}.hostname.yml ]]; then
+                        COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${appname}.hostname.yml"
                     else
-                        info "${APP_FOLDER}/${FILENAME}.hostname.yml does not exist."
+                        info "${APP_FOLDER}/${appname}.hostname.yml does not exist."
                     fi
-                    if [[ -f ${APP_FOLDER}/${FILENAME}.ports.yml ]]; then
-                        COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${FILENAME}.ports.yml"
+                    if [[ -f ${APP_FOLDER}/${appname}.ports.yml ]]; then
+                        COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${appname}.ports.yml"
                     else
-                        info "${APP_FOLDER}/${FILENAME}.ports.yml does not exist."
+                        info "${APP_FOLDER}/${appname}.ports.yml does not exist."
                     fi
                 elif [[ -n ${APPNETMODE} ]]; then
-                    if [[ -f ${APP_FOLDER}/${FILENAME}.netmode.yml ]]; then
-                        COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${FILENAME}.netmode.yml"
+                    if [[ -f ${APP_FOLDER}/${appname}.netmode.yml ]]; then
+                        COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${appname}.netmode.yml"
                     else
-                        info "${APP_FOLDER}/${FILENAME}.netmode.yml does not exist."
+                        info "${APP_FOLDER}/${appname}.netmode.yml does not exist."
                     fi
                 fi
-                COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${FILENAME}.yml"
-                info "All configurations for ${APPNAME} are included."
+                COMPOSE_FILE="${COMPOSE_FILE}:${APP_FOLDER}/${appname}.yml"
+                info "All configurations for ${AppName} are included."
             else
-                warn "${APP_FOLDER}/${FILENAME}.yml does not exist."
+                warn "${APP_FOLDER}/${appname}.yml does not exist."
             fi
             run_script 'appfolders_create' "${APPNAME}"
         else
