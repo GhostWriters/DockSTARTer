4bfb866ac7707750372085b554fca513996b30cf
CLHatch <CLHatch@gmail.com>
2025-05-05 12:01:16 -0500

    Update appvars_create.sh
    Use the app `nicename` for notices

--- .scripts/appvars_create.sh
+++ .scripts/appvars_create.sh
@@ -5,9 +5,10 @@ IFS=$'\n\t'
 appvars_create() {
     local AppList
     AppList=$(xargs -n 1 <<< "$*")
-    for AppName in ${AppList}; do
-        local APPNAME=${AppName^^}
+    for APPNAME in ${AppList^^}; do
         local appname=${APPNAME,,}
+        local AppName
+        AppName=$(run_script 'app_nicename' "${APPNAME}")
 
         if run_script 'app_is_builtin' "${AppName}"; then
             local APP_FOLDER="${TEMPLATES_FOLDER}/${appname}"
@@ -15,7 +16,7 @@ appvars_create() {
             local APP_DEFAULT_ENV_FILE="${APP_FOLDER}/${appname}.env"
             local APP_ENV_FILE="${APP_ENV_FOLDER}/${appname}.env"
 
-            info "Creating environment variables for ${APPNAME}."
+            info "Creating environment variables for ${AppName}."
 
             if [[ ! -d ${APP_ENV_FOLDER} ]]; then
                 warn "Folder ${APP_ENV_FOLDER} not found. Creating it."
@@ -31,9 +32,9 @@ appvars_create() {
 
             run_script 'env_merge_newonly' "${COMPOSE_ENV}" "${APP_DEFAULT_GLOBAL_ENV_FILE}"
             run_script 'env_merge_newonly' "${APP_ENV_FILE}" "${APP_DEFAULT_ENV_FILE}"
-            info "Environment variables created for ${APPNAME}."
+            info "Environment variables created for ${AppName}."
         else
-            warn "Application ${AppName^^} does not exist."
+            warn "Application ${APPNAME} does not exist."
         fi
 
     done
