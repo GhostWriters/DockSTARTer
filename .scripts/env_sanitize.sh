4375705826a42882a1fc585d6209baafd7ec4c07
CLHatch <CLHatch@gmail.com>
2025-05-08 15:05:40 -0500

    Update env_sanitize.sh

--- .scripts/env_sanitize.sh
+++ .scripts/env_sanitize.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 env_sanitize() {
     local GLOBAL_LAN_NETWORK
     GLOBAL_LAN_NETWORK=$(run_script 'env_get' GLOBAL_LAN_NETWORK)
-    if echo "${GLOBAL_LAN_NETWORK}" | grep -q 'x'; then
+    if echo "${GLOBAL_LAN_NETWORK-}" | grep -q 'x'; then
         local DETECTED_LAN_NETWORK
         DETECTED_LAN_NETWORK=$(run_script 'detect_lan_network')
         run_script 'env_set' GLOBAL_LAN_NETWORK "${DETECTED_LAN_NETWORK}"
