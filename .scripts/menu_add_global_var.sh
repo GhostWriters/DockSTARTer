bd69e2c35da80c4c23c7061eed310063e47d8dca
CLHatch <CLHatch@gmail.com>
2025-05-13 21:23:45 -0500

    Update menu_add_global_var.sh

--- .scripts/menu_add_global_var.sh
+++ .scripts/menu_add_global_var.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 menu_add_global_var() {
     local Title="Add Global Variable"
-    dialog --colors --title "${Title}" --msgbox "Add a variable global variable" 0 0
+    dialog --colors --title "${Title}" --msgbox "Add a global variable" 0 0
 }
 
 test_menu_add_global_var() {
