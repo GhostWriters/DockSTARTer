a5f87dbf2880b49bcdbf870f28b505b461c3588e
CLHatch <CLHatch@gmail.com>
2025-05-15 14:04:06 -0500

    Create menu_color_definitions.sh
    Create an "include" file with color code variables to be used in the `menu_*` scripts.

---
+++ .scripts/menu_color_definitions.sh
@@ -0,0 +1,30 @@
+# Dialog color codes to be used in the GUI menu
+
+# shellcheck disable=SC2168 # local is only valid in functions
+local \
+    ColorHeading \
+    ColorHeadingValue \
+    ColorHighlight
+# shellcheck disable=SC2034 # variable appears unused. Verify it or export it.
+{
+    ColorHeading='\Zr'
+    ColorHeadingValue='\Zb\Zr'
+    ColorHighlight='\Z3\Zb'
+}
+
+# shellcheck disable=SC2168 # local is only valid in functions
+local \
+    ColorHeadingLine \
+    ColorCommentLine \
+    ColorOtherLine \
+    ColorVarLine \
+    ColorAddVariableLine
+# shellcheck disable=SC2034 # variable appears unused. Verify it or export it.
+{
+    ColorHeadingLine='\Zn'
+    ColorCommentLine='\Z0\Zb\Zr'
+    ColorOtherLine="${ColorCommentLine}"
+    ColorVarLine='\Z0\ZB\Zr'
+    ColorAddVariableLine=="${ColorVarLine}"
+}
+
