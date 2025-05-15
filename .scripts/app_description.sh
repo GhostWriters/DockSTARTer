60475e8081a2ba9a8d48fa1dc3fa4c8b2f26db4f
CLHatch <CLHatch@gmail.com>
2025-05-15 13:17:21 -0500

    Update app_description.sh
    Return a description for "User Defined" apps

--- .scripts/app_description.sh
+++ .scripts/app_description.sh
@@ -6,13 +6,18 @@ app_description() {
     # Return the description of the appname passed.
     local appname=${1-}
     appname=${appname,,}
-    local LABELS_FILE="${TEMPLATES_FOLDER}/${appname}/${appname}.labels.yml"
-    if [[ -f ${LABELS_FILE} ]]; then
-        grep --color=never -Po "\scom\.dockstarter\.appinfo\.description: \K.*" "${LABELS_FILE}" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo "! Missing description !"
+    if run_script 'app_is_builtin' "${appname}"; then
+        local LABELS_FILE="${TEMPLATES_FOLDER}/${appname}/${appname}.labels.yml"
+        if [[ -f ${LABELS_FILE} ]]; then
+            grep --color=never -Po "\scom\.dockstarter\.appinfo\.description: \K.*" "${LABELS_FILE}" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo "! Missing description !"
+        else
+            echo "! Missing application !"
+        fi
     else
-        echo "! Missing application !"
+        local AppName
+        AppName="$(run_script 'app_nicename' "${appname}")"
+        echo "${AppName} is a user defined application"
     fi
-
 }
 
 test_app_description() {
