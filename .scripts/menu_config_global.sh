27cdd950975c41b9bc8e3721158f2bce72bd7579
CLHatch <CLHatch@gmail.com>
2025-05-15 20:15:31 -0500

    Update menu_config_global.sh

--- .scripts/menu_config_global.sh
+++ .scripts/menu_config_global.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 menu_config_global() {
     local Title="Edit Global Variables"
 
-    # shellcheck source=./menu_color_definitions.sh
+    # shellcheck source-path=/.scripts
     source "${SCRIPTPATH}/.scripts/menu_color_definitions.sh"
 
     local AddVariableText='<ADD VARIABLE>'
