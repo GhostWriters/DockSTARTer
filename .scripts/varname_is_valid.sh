95e464c737264f8c600119a07072484c5b81d6ed
CLHatch <CLHatch@gmail.com>
2025-05-17 08:15:21 -0500

    Update varname_is_valid.sh

--- .scripts/varname_is_valid.sh
+++ .scripts/varname_is_valid.sh
@@ -60,7 +60,7 @@ varname_is_valid() {
 test_varname_is_valid() {
     for VarType in "" _BARE_ _GLOBAL_ _APPNAME_ "_APPNAME_:" "radarr:" "radarr"; do
         notice "[${VarType}]"
-        for VarName in "radarr:varname" TZ RADARR_4K RADARR__4K__TAG RADARR__4K__tag; do
+        for VarName in "radarr:varname" TZ RADARR_4K RADARR__TAG Radarr__TAG RADARR__4K__TAG RADARR__4K__tag; do
             if run_script 'varname_is_valid' "${VarName}" "${VarType}"; then
                 notice "             [${VarName}] is valid"
             else
