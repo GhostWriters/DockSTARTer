b7ae2a4becb3924bacb9bb81b39ae6e5e9bb55e8
CLHatch <CLHatch@gmail.com>
2025-05-17 23:43:28 -0500

    Update appname_is_valid.sh
    Don't allow certain instance names.

--- .scripts/appname_is_valid.sh
+++ .scripts/appname_is_valid.sh
@@ -4,11 +4,19 @@ IFS=$'\n\t'
 
 appname_is_valid() {
     local AppName=${1-}
-    [[ ${AppName-} =~ ^[a-zA-Z][a-zA-Z0-9]*(__[a-zA-Z0-9]+)?$ ]]
+    if [[ ${AppName} =~ ^[a-zA-Z][a-zA-Z0-9]*(__[a-zA-Z0-9]+)?$ ]]; then
+        local InvalidInstanceNames="CONTAINER|ENABLED|ENVIRONMENT|HOSTNAME|PORT|NETWORK|RESTART|TAG"
+        local InstanceName
+        InstanceName="$(run_script 'appname_to_instancename' "${AppName}")"
+        [[ ! ${InstanceName} =~ ${InvalidInstanceNames} ]]
+        return
+    fi
+    false
+    return
 }
 
 test_appname_is_valid() {
-    for AppName in SONARR Sonarr SONARR_4K SONARR__4K "SONARR 4K" "SONARR:"; do
+    for AppName in SONARR Sonarr SONARR_4K SONARR__4K "SONARR 4K" "SONARR:" SONARR__TAG; do
         if run_script 'appname_is_valid' "${AppName}"; then
             notice "[${AppName}] is valid"
         else
