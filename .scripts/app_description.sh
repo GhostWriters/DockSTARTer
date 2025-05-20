77f41de27888507808c4b5c795ffabc2a45271f9
CLHatch <CLHatch@gmail.com>
2025-05-20 00:13:59 -0500

    Reapply "Update app_nicename.sh"
    This reverts commit 99a565f61485eb7e9406d1657f464a752cf89a8d.

--- .scripts/app_description.sh
+++ .scripts/app_description.sh
@@ -6,7 +6,7 @@ app_description() {
     # Return the description of the appname passed.
     local appname=${1-}
     appname=${appname,,}
-    if run_script 'app_is_use_defined' "${appname}"; then
+    if run_script 'app_is_user_defined' "${appname}"; then
         local AppName
         AppName="$(run_script 'app_nicename' "${appname}")"
         echo "${AppName} is a user defined application"
@@ -18,7 +18,7 @@ app_description() {
         else
             echo "! Missing application !"
         fi
-    else
+    fi
 }
 
 test_app_description() {
