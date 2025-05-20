970d545740426aa6dd02a4804b538184a0b857f2
CLHatch <CLHatch@gmail.com>
2025-05-19 22:20:25 -0500

    Update app_description.sh
    Use `app_is+user_defined`

--- .scripts/app_description.sh
+++ .scripts/app_description.sh
@@ -6,7 +6,11 @@ app_description() {
     # Return the description of the appname passed.
     local appname=${1-}
     appname=${appname,,}
-    if run_script 'app_is_builtin' "${appname}"; then
+    if run_script 'app_is_use_defined' "${appname}"; then
+        local AppName
+        AppName="$(run_script 'app_nicename' "${appname}")"
+        echo "${AppName} is a user defined application"
+    else
         local LABELS_FILE
         LABELS_FILE="$(run_script 'app_instance_file' "${appname}" ".labels.yml")"
         if [[ -f ${LABELS_FILE} ]]; then
@@ -15,10 +19,6 @@ app_description() {
             echo "! Missing application !"
         fi
     else
-        local AppName
-        AppName="$(run_script 'app_nicename' "${appname}")"
-        echo "${AppName} is a user defined application"
-    fi
 }
 
 test_app_description() {
