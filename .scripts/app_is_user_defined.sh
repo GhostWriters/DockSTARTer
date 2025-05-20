8bfbc0f953862016eaf90f9a6ec96a238eeb6c36
CLHatch <CLHatch@gmail.com>
2025-05-19 23:42:35 -0500

    Update app_is_user_defined.sh

--- .scripts/app_is_user_defined.sh
+++ .scripts/app_is_user_defined.sh
@@ -5,19 +5,15 @@ IFS=$'\n\t'
 app_is_user_defined() {
     local APPNAME=${1-}
 
-    if ! run_script 'app_is_referenced' "${APPNAME}"; then
-        false
-        return
+    if ! run_script 'app_is_builtin' "${APPNAME}"; then
+       true
+       return
     fi
     if ! run_script 'env_var_exists' "${APPNAME^^}__ENABLED"; then
-        false
+        true
         return
     fi
-    if ! run_script 'app_is_builtin' "${APPNAME}"; then
-        false
-        return
-    fi
-    true
+    false
     return
 }
 
