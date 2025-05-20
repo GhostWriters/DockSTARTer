6c79eb00970b15fd593e122c79dff8a03c137c82
CLHatch <CLHatch@gmail.com>
2025-05-19 21:52:41 -0500

    Update app_is_referenced.sh

--- .scripts/app_is_referenced.sh
+++ .scripts/app_is_referenced.sh
@@ -3,15 +3,20 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 app_is_referenced() {
-    local APPNAME=${1-)
+    local APPNAME=${1-}
     APPNAME=${APPNAME^^}
-    local APPNAME_REGEX="${APPNAME^^})"
-    local REFERENCED_APPS_REGEX="^\s*${APPNAME_REGEX}(?=__[A-Za-z0-9]\w*\s*=)"
+    local APPNAME_REGEX="${APPNAME^^}"
+    local REFERENCED_APPS_REGEX="^\s*${APPNAME_REGEX}__[A-Za-z0-9]*"
 
     grep -q -P "${REFERENCED_APPS_REGEX}" "${COMPOSE_ENV}" &> /dev/null
 }
 
 test_app_is_referenced() {
-    run_script 'app_list_referenced'
-    #warn "CI does not test app_list_referenced."
+    for AppName in WATCHTOWER SAMBA RADARR NONEXISTENTAPP; do
+        local Referenced="no"
+        if run_script 'app_is_referenced' "${AppName}"; then
+            Referenced="YES"
+        fi
+        notice "[${AppName}] [${Referenced}]"
+    done
 }
