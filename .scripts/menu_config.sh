c3ebb8b0a9c046b423aced68ca6128cb3e2c6871
CLHatch <CLHatch@gmail.com>
2025-05-22 01:05:19 -0500

    Update menu_config.sh
    Re-order menu options.

--- .scripts/menu_config.sh
+++ .scripts/menu_config.sh
@@ -13,17 +13,17 @@ menu_config() {
         run_script 'appvars_create_all'
     } |& dialog_pipe "Updating Variable Files" "" "${DIALOGTIMEOUT}"
     local OptionFullSetup="Full Setup"
+    local OptionEditGlobalVars="Edit Global Variables"
     local OptionSelectApps="Select Apps"
     local OptionEditAppVars="Edit App Variables"
-    local OptionEditGlobalVars="Edit Global Variables"
     local OptionComposeUp="Start All Applications"
     local OptionComposeDown="Stop All Applications"
     local OptionDockerPrune="Prune Docker System"
     local ConfigOpts=(
         "${OptionFullSetup}" "This goes through selecting apps and editing variables. Recommended for first run"
+        "${OptionEditGlobalVars}" "Review and adjust global variables"
         "${OptionSelectApps}" "Select which apps to run. Previously installed apps are remembered"
         "${OptionEditAppVars}" "Review and adjust variables for installed apps"
-        "${OptionEditGlobalVars}" "Review and adjust global variables"
         "${OptionComposeUp}" "Run Docker Compose to start all applications"
         "${OptionComposeDown}" "Run Docker Compose to stop all applications"
         "${OptionDockerPrune}" "Remove all unused containers, networks, volumes, images and build cache"
@@ -51,14 +51,14 @@ menu_config() {
                         run_script 'menu_app_select' || true
                         run_script 'menu_config_apps' || true
                         ;;
+                    "${OptionEditGlobalVars}")
+                        run_script 'menu_config_vars' || true
+                        ;;
                     "${OptionSelectApps}")
                         run_script 'menu_app_select' || true
                         ;;
                     "${OptionEditAppVars}")
                         run_script 'menu_config_apps' || true
-                        ;;
-                    "${OptionEditGlobalVars}")
-                        run_script 'menu_config_vars' || true
                         ;;
                     "${OptionComposeUp}")
                         {
