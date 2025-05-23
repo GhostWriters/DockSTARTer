f08cd4b56942f04868c7b28ecb730a27219ecbe5
CLHatch <CLHatch@gmail.com>
2025-05-22 20:24:23 -0500

    Update env_migrate_global.sh

--- .scripts/env_migrate_global.sh
+++ .scripts/env_migrate_global.sh
@@ -3,12 +3,9 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 env_migrate_global() {
-    # Backup user files
-    run_script 'env_backup'
-
     # Rename global vars
     run_script 'env_migrate' DOCKERCONFDIR DOCKER_VOLUME_CONFIG
-    run_script 'env_migrate' DOCKERGID DOCKER_GID
+    run_script 'env_migrate' DOCKERGID +
     run_script 'env_migrate' DOCKERHOSTNAME DOCKER_HOSTNAME
     run_script 'env_migrate' DOCKERSTORAGEDIR DOCKER_VOLUME_STORAGE
     run_script 'env_migrate' DOCKERSHAREDDIR DOCKER_VOLUME_STORAGE
