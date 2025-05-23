6b94bcea7c09436dea26103476acff78516a246e
CLHatch <CLHatch@gmail.com>
2025-05-22 22:52:49 -0500

    Update env_sanitize.sh

--- .scripts/env_sanitize.sh
+++ .scripts/env_sanitize.sh
@@ -36,12 +36,10 @@ env_sanitize() {
     # Add any "APPNAME__VOLUME_*" variables to the list
     local -a AppList
     readarray -t AppList < <(run_script 'app_list_referenced')
-    if [[ -n ${AppList-} ]]
-        notice "AppList=${AppList[@]}"
-        for AppName in "${AppList[@]-}"; do
-            notice "${AppName}"
-            readarray -t -O ${#VarList[@]} VarList < <(grep -o -P "^\s*\K${AppName^^}__VOLUME_[a-zA-Z0-9]+[a-zA-Z0-9_](?=\s*=)" "${COMPOSE_ENV}" || true)
-        done
+    notice "AppList=${AppList[@]}"
+    for AppName in "${AppList[@]-}"; do
+        notice "${AppName}"
+        readarray -t -O ${#VarList[@]} VarList < <(grep -o -P "^\s*\K${AppName^^}__VOLUME_[a-zA-Z0-9]+[a-zA-Z0-9_](?=\s*=)" "${COMPOSE_ENV}" || true)
     done
     notice "VarList=${VarList[@]}"
     for VarName in "${VarList[@]-}"; do
