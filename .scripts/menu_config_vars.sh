6f6ea0422c3952df3d130cdee041da5addaad1a1
CLHatch <CLHatch@gmail.com>
2025-05-24 21:01:39 -0500

    Update menu_config_vars.sh
    Attempt to dynamically size the dialog box.
    It should be full width (minus the normal margin), and as few rows as it can be, but a max of the normal top and bottom margin areas.

--- .scripts/menu_config_vars.sh
+++ .scripts/menu_config_vars.sh
@@ -157,19 +157,30 @@ menu_config_vars() {
             LastLineChoice=${TotalLines}
         fi
         while true; do
-            local -i WindowHeight WindowWidth MaxWindowListHeight WindowListHeight MenuTextLines
-            MenuTextLines=2
-            WindowHeight=$((LINES - DC["WindowHeightAdjust"]))
-            WindowWidth=$((COLUMNS - DC["WindowWidthAdjust"]))
-            MaxWindowListHeight="$((LINES - DC["TextHeightAdjust"] - MenuTextLines))"
-            WindowListHeight=${MaxWindowListHeight}
-            local -a LineDialog=(
+            local MenuText="\n${DialogHeading}"
+            local -i ScreenRows ScreenCols ScreenHelplineRows
+            local -i WindowRowsMax WindowColsMax
+            local -i WindowRows WindowCols WindowListRows WindowListRowsMax
+            local -i MenuTextRows
+            ScreenRows="${LINES}"
+            ScreenCols="${COLUMNS}"
+            ScreenHelplineRows=0
+            WindowRowsMax=$((ScreenRows - ScreenHelplineRows - DC["WindowHeightAdjust"]))
+            WindowColsMax="$((ScreenCols - DC["WindowWidthAdjust"]))"
+            local -a LineDialogParams=(
                 --stdout
-                --begin "${DC[OffsetTop]}" "${DC[OffsetLeft]}"
+            )
+            MenuTextRows="$(dialog "${LineDialogParams[@]}" --print-text-size "${MenuText}" "${WindowRowsMax}" "${WindowColsMax}" | cut -d ' ' -f 1)"
+            WindowListRowsMax="$((WindowRowsMax - MenuTextRows))"
+            WindowListRows=$((TotalLines < WindowListRowsMax ? TotalLines : WindowListRowsMax))
+            WindowRows=0
+            WindowCols=0
+            local -a LineDialog=(
+                "${LineDialogParams[@]}"
                 --ok-label "Select"
                 --cancel-label "Done"
                 --title "${DC["Title"]}${Title}"
-                --menu "\n${DialogHeading}" "${WindowHeight}" "${WindowWidth}" "${WindowListHeight}"
+                --menu "${MenuText}" "${WindowRows}" "${WindowCols}" "${WindowListRows}"
                 "${LineOptions[@]}"
             )
             local -i LineDialogButtonPressed=0
