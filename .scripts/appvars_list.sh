1c14d0ec6c8e0c06461c7712f7526d5115607c68
CLHatch <CLHatch@gmail.com>
2025-05-27 03:35:37 -0500

    Update appvars_list.sh

--- .scripts/appvars_list.sh
+++ .scripts/appvars_list.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 appvars_list() {
     local APPNAME=${1-}
-    if [[ ${APPNAME} = *":" ]]; then
+    if [[ ${APPNAME} == *":" ]]; then
         # APPNAME is in the form of "APPNAME:", list all variable in "env_files/appname.env"
         APPNAME=${APPNAME%%:*}
         local VAR_FILE
