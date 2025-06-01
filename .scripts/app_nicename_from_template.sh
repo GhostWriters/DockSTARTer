c6c3a94c196eb2b7668c642d151875edd0f70082
CLHatch <CLHatch@gmail.com>
2025-06-01 07:34:00 -0500

    Update app_nicename_from_template.sh

--- .scripts/app_nicename_from_template.sh
+++ .scripts/app_nicename_from_template.sh
@@ -7,9 +7,10 @@ app_nicename_from_template() {
     local AppList
     AppList=$(xargs -n 1 <<< "$*")
     for APPNAME in ${AppList}; do
-        local appname=${APPNAME,,}
+        local AppName"${APPNAME%:*}"
+        local appname=${AppName,,}
         local LABELS_FILE
-        LABELS_FILE="$(run_script 'app_instance_file' "${APPNAME}" ".labels.yml")"
+        LABELS_FILE="$(run_script 'app_instance_file' "${appname}" ".labels.yml")"
         if [[ -f ${LABELS_FILE} ]]; then
             grep --color=never -Po "\scom\.dockstarter\.appinfo\.nicename: \K.*" "${LABELS_FILE}" | sed -E 's/^([^"].*[^"])$/"\1"/' | xargs || echo "${appname}"
         else
