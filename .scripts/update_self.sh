d21c576b9fabba586459b340afd5f158161c554c
CLHatch <CLHatch@gmail.com>
2025-06-03 15:52:47 -0500

    Update update_self.sh

--- .scripts/update_self.sh
+++ .scripts/update_self.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 update_self() {
     local BRANCH
-    BRANCH=${1-$(git branch --show)}
+    BRANCH=${1-}
 
     local Title="Update DockSTARTer"
     if ! run_script 'question_prompt' Y "Would you like to update DockSTARTer to ${BRANCH} now?" "${DC["TitleWarning"]}${Title}" "${FORCE:+Y}"; then
@@ -22,6 +22,8 @@ update_self() {
 
 commands_update_self() {
     local BRANCH=${1-}
+    local CurrentBranch
+    CurrentBranch="$(git branch --show)"
     notice "Clearing instances folder"
     rm -R "${INSTANCES_FOLDER:?}/"* &> /dev/null || true
     notice "Updating DockSTARTer to ${BRANCH}."
@@ -35,6 +37,11 @@ commands_update_self() {
     git fetch --all --prune || fatal "Failed to fetch recent changes from git.\nFailing command: ${F[C]}git fetch --all --prune"
     if [[ ${CI-} != true ]]; then
         if [[ -n ${BRANCH-} ]]; then
+            if [[ -n ${CurrentBranch-} ]]; then
+                info "Switching DockSTARTer from github branch ${CurrentBranch} to ${BRANCH}"
+            else
+                info "Switching DockSTARTer to github branch ${BRANCH}"
+            fi
             git switch --force "${BRANCH}" || fatal "Failed to switch to github branch ${BRANCH}.\nFailing command: ${F[C]}git switch --force \"${BRANCH}\""
         fi
         git reset --hard HEAD || fatal "Failed to reset to current branch.\nFailing command: ${F[C]}git reset --hard HEAD"
@@ -42,9 +49,9 @@ commands_update_self() {
         git pull || fatal "Failed to pull recent changes from git.\nFailing command: ${F[C]}git pull"
     fi
     info "Cleaning up unnecessary files and optimizing the local repository."
-    git gc > /dev/null 2>&1 || true
+    git gc || true
     info "Setting file ownership on new repository files"
-    git ls-tree -rt --name-only "${BRANCH}" | xargs sudo chown "${DETECTED_PUID}":"${DETECTED_PGID}" > /dev/null 2>&1 || true
+    git ls-tree -rt --name-only HEAD | xargs sudo chown "${DETECTED_PUID}":"${DETECTED_PGID}" > /dev/null 2>&1 || true
     sudo chown -R "${DETECTED_PUID}":"${DETECTED_PGID}" "${SCRIPTPATH}/.git" > /dev/null 2>&1 || true
     sudo chown "${DETECTED_PUID}":"${DETECTED_PGID}" "${SCRIPTPATH}" > /dev/null 2>&1 || true
 }
