cf7bc5b0f1035f0f1b7499c69be1181a18ea0928
CLHatch <CLHatch@gmail.com>
2025-06-05 04:11:33 -0500

    Update menu_add_app.sh

--- .scripts/menu_add_app.sh
+++ .scripts/menu_add_app.sh
@@ -3,7 +3,8 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 menu_add_app() {
-    return
+    local Title="Add Application"
+    dialog --title "${Title}" --msgbox "Add Variable is not implemented yet." 0 0 || true
 }
 test_menu_add_app() {
     # run_script 'menu_add_var'
