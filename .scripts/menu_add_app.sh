ac1221099d3b8c605a4eec12d6b87a307afe4e9c
CLHatch <CLHatch@gmail.com>
2025-06-05 03:42:59 -0500

    Create menu_add_app.sh

---
+++ .scripts/menu_add_app.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+menu_add_app() {
+    return
+}
+test_menu_add_app() {
+    # run_script 'menu_add_var'
+    warn "CI does not test menu_add_var."
+}
