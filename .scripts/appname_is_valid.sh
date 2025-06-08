21b9d720d8655a8393981c005363c197257ec717
CLHatch <CLHatch@gmail.com>
2025-06-07 19:06:36 -0500

    Update appname_is_valid.sh

--- .scripts/appname_is_valid.sh
+++ .scripts/appname_is_valid.sh
@@ -4,6 +4,11 @@ IFS=$'\n\t'
 
 appname_is_valid() {
     local AppName=${1-}
+    if [[ ${AppName-} == *":" ]]; then
+        AppName="${AppName%:*}"
+    elif [[ ${AppName-} == ":"* ]]; then
+        AppName="${AppName#:*}"
+    fi
     if [[ ${AppName} =~ ^[a-zA-Z][a-zA-Z0-9]*(__[a-zA-Z0-9]+)?$ ]]; then
         local InvalidInstanceNames="CONTAINER|ENABLED|ENVIRONMENT|HOSTNAME|PORT|NETWORK|RESTART|TAG"
         local InstanceName
