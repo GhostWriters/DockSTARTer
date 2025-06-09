dc38142b5ada981ed81fc8bd9bb2e0bc061d9c84
CLHatch <CLHatch@gmail.com>
2025-06-09 01:03:42 -0500

    Update env_merge_newonly.sh

--- .scripts/env_merge_newonly.sh
+++ .scripts/env_merge_newonly.sh
@@ -3,40 +3,42 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 env_merge_newonly() {
-    # env_merge_newonly "MERGE_TO_FILE" "MERGE_FROM_FILE"
+    # env_merge_newonly "MergeToFile" "MergeFromFile"
     #
-    # Merges all new variables from "MERGE_FROM_FILE" to "MERGE_TO_FILE".
-    # Skips all variables that already exist in "MERGE_TO_FILE"
-    local MERGE_TO_FILE=${1-}
-    local MERGE_FROM_FILE=${2-}
+    # Merges all new variables from "MergeFromFile" to "MergeToFile".
+    # Skips all variables that already exist in "MergeToFile"
+    local MergeToFile=${1-}
+    local MergeFromFile=${2-}
 
-    # If "MERGE_TO_FILE" doesn't exists, create it
-    if [[ ! -f ${MERGE_TO_FILE} ]]; then
-        touch "${MERGE_TO_FILE}"
+    # If "MergeToFile" doesn't exists, create it
+    if [[ ! -f ${MergeToFile} ]]; then
+        touch "${MergeToFile}"
     fi
 
-    # If "MERGE_FROM_FILE" doesn't exists, give a warning
-    if [[ ! -f ${MERGE_FROM_FILE} ]]; then
-        warn "File ${MERGE_FROM_FILE} does not exist."
+    # If "MergeFromFile" doesn't exists, give a warning
+    if [[ ! -f ${MergeFromFile} ]]; then
+        warn "File ${MergeFromFile} does not exist."
     else
-        local MERGE_FROM_LINES=()
+        local MergeFromLines=()
         # Read all variable lines into an array, stripping whitespace before and after the variable name
-        readarray -t MERGE_FROM_LINES < <(sed -n -E "s/^\s*(\w+)\s*=/\1=/p" "${MERGE_FROM_FILE}" || true)
-        for index in "${!MERGE_FROM_LINES[@]}"; do
-            local line=${MERGE_FROM_LINES[$index]}
-            local VARNAME="${line%%=*}"
-            if grep -q -P "^\s*${VARNAME}\s*=\K.*" "${MERGE_TO_FILE}"; then
-                # Variable is already in file, skip it
-                unset 'MERGE_FROM_LINES[index]'
-            fi
-        done
-        if [[ -n ${MERGE_FROM_LINES[*]-} ]]; then
-            notice "Adding variables to ${MERGE_TO_FILE}:"
-            for line in "${MERGE_FROM_LINES[@]}"; do
+        readarray -t MergeFromLines < <(sed -n -E "s/^\s*(\w+)\s*=/\1=/p" "${MergeFromFile}" || true)
+        if [[ -n ${MergeFromLines[*]-} ]]; then
+            for index in "${!MergeFromLines[@]}"; do
+                local line=${MergeFromLines[$index]}
+                local VarName="${line%%=*}"
+                if grep -q -P "^\s*${VarName}\s*=\K.*" "${MergeToFile}"; then
+                    # Variable is already in file, skip it
+                    unset 'MergeFromLines[index]'
+                fi
+            done
+        fi
+        if [[ -n ${MergeFromLines[*]-} ]]; then
+            notice "Adding variables to ${MergeToFile}:"
+            for line in "${MergeFromLines[@]}"; do
                 notice "   $line"
             done
-            echo >> "${MERGE_TO_FILE}" || fatal "Failed to write to ${MERGE_TO_FILE}.\nFailing command: echo >> \"${MERGE_TO_FILE}\""
-            printf '%s\n' "${MERGE_FROM_LINES[@]}" >> "${MERGE_TO_FILE}" || fatal "Failed to add variables to ${MERGE_TO_FILE}"
+            echo >> "${MergeToFile}" || fatal "Failed to write to ${MergeToFile}.\nFailing command: echo >> \"${MergeToFile}\""
+            printf '%s\n' "${MergeFromLines[@]}" >> "${MergeToFile}" || fatal "Failed to add variables to ${MergeToFile}"
         fi
     fi
 
