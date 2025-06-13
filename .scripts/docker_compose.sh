d0a8f96b3775ef31e2dd4da6319c2696d7caf494
CLHatch <CLHatch@gmail.com>
2025-06-13 11:42:45 -0500

    Squashed commit of the following:
    commit 866c566b7cb1deae2f5f0e58a3c7765099815977
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 11:41:31 2025 -0500
    
        Add `ds -c stop`
    
    commit 76b114ede8b4bbc8abb2571f65d5d8fb0adf66bb
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 11:28:33 2025 -0500
    
        Update docker_compose.sh
    
    commit a2891a9cf0380e0db5bac5a4089cccceac9ab397
    Merge: 478f0eb4e af87dc139
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 11:25:01 2025 -0500
    
        Merge branch 'gui-work' of https://github.com/GhostWriters/DockSTARTer into gui-work
    
    commit 478f0eb4ec76699038d51aa11734bd0bf7c37800
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 11:03:17 2025 -0500
    
        Update docker_compose.sh
    
    commit af87dc139a1480aeb218146ef4d3172cf26b8869
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 11:03:17 2025 -0500
    
        Update docker_compose.sh
    
    commit 869b165dfa924567502e9eab81f914f3b8a9448e
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 10:40:46 2025 -0500
    
        Update main.sh
    
    commit 672929bd4f8e8a35d0a4ffe8c11deb2851f09891
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 10:33:53 2025 -0500
    
        Update main.sh
    
    commit ac8d59931bab54f692e4246319cd406821b5fbae
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 10:27:59 2025 -0500
    
        Update docker_compose.sh
    
    commit 75dcaf0d216868a3c5712576fa42f51a0bde676e
    Merge: d6d9b8b78 811d9bbbb
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 10:08:21 2025 -0500
    
        Merge branch 'app-env-files' into gui-work
    
    commit d6d9b8b789277d27dbdbe0a55f398d9640058cf2
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 09:50:28 2025 -0500
    
        Update docker_compose.sh
    
    commit 424f73590ff1c5eca784fd57579b78ce18ad1422
    Author: CLHatch <CLHatch@gmail.com>
    Date:   Fri Jun 13 08:58:35 2025 -0500
    
        Update docker_compose.sh
    
        Change case of variables

--- .scripts/docker_compose.sh
+++ .scripts/docker_compose.sh
@@ -3,52 +3,79 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 docker_compose() {
-    local COMPOSEINPUT=${1-}
-    local COMMAND=${COMPOSEINPUT%% *}
-    local APPNAME
-    local AppName
-    if [[ ${COMPOSEINPUT} == *" "* ]]; then
-        APPNAME=${COMPOSEINPUT#* }
-        AppName="$(run_script 'app_nicename' "${APPNAME}")"
+    local ComposeInput=${1-}
+    local Command=${ComposeInput%% *}
+    local APPNAME AppName
+    if [[ ${ComposeInput} == *" "* ]]; then
+        APPNAME=${ComposeInput#* }
+        AppName="$(run_script 'app_nicename' "${APPNAME}" | xargs)"
     fi
-    local COMPOSECOMMAND COMPOSECOMMAND2
-    local COMMANDINFO
-    case ${COMMAND} in
+
+    local Title="Docker Compose"
+
+    local Question YesNotice NoNotice
+    local -a CommandNotice
+    local -a ComposeCommand
+    case ${Command} in
         down)
-            COMPOSECOMMAND="down --remove-orphans ${APPNAME-}"
-            COMMANDINFO="Stopping and removing ${AppName:-containers, networks, volumes, and images created by DockSTARTer}."
+            Question="Stop and remove ${AppName:-containers, networks, volumes, and images created by DockSTARTer}?"
+            NoNotice="Not stopping and removing ${AppName:-containers, networks, volumes, and images created by DockSTARTer}."
+            YesNotice="Stopping and removing ${AppName:-containers, networks, volumes, and images created by DockSTARTer}."
+            ComposeCommand[0]="down --remove-orphans ${APPNAME-}"
             ;;
         pull)
-            COMPOSECOMMAND="pull --include-deps ${APPNAME-}"
-            COMMANDINFO="Pulling the latest images for ${AppName:-all enabled services}."
+            Question="Pull the latest images for ${AppName:-all enabled services}?"
+            NoNotice="Not pulling the latest images for ${AppName:-all enabled services}."
+            YesNotice="Pulling the latest images for ${AppName:-all enabled services}."
+            ComposeCommand[0]="pull --include-deps ${APPNAME-}"
             ;;
         restart)
-            COMPOSECOMMAND="restart ${APPNAME-}"
-            COMMANDINFO="Restarting ${AppName:-all stopped and running services}."
+            Question="Restart ${AppName:-all stopped and running services}?"
+            NoNotice="Not restarting ${AppName:-all stopped and running services}."
+            YesNotice="Restarting ${AppName:-all stopped and running services}."
+            ComposeCommand[0]="restart ${APPNAME-}"
+            ;;
+        stop)
+            Question="Stop ${AppName:-containers, networks, volumes, and images created by DockSTARTer}?"
+            NoNotice="Not stopping ${AppName:-containers, networks, volumes, and images created by DockSTARTer}."
+            YesNotice="Stopping  ${AppName:-containers, networks, volumes, and images created by DockSTARTer}."
+            ComposeCommand[0]="stop ${APPNAME-}"
             ;;
         update)
-            COMPOSECOMMAND="pull --include-deps ${APPNAME-}"
-            COMMANDINFO="Pulling the latest images for ${AppName:-all enabled services}."
-            COMPOSECOMMAND2="up -d --remove-orphans ${APPNAME-}"
-            COMMANDINFO2="Creating ${AppName:-containers for all enabled services}."
+            Question="Update ${AppName:-containers for all enabled services}?"
+            NoNotice="Not updating ${AppName:-containers for all enabled services}."
+            YesNotice="Updating ${AppName:-containers for all enabled services}."
+            CommandNotice[0]="Pulling the latest images for ${AppName:-all enabled services}."
+            ComposeCommand[0]="pull --include-deps ${APPNAME-}"
+            CommandNotice[1]="Starting ${AppName:-containers for all enabled services}."
+            ComposeCommand[1]="up -d --remove-orphans ${APPNAME-}"
             ;;
         up)
-            COMPOSECOMMAND="up -d --remove-orphans ${APPNAME-}"
-            COMMANDINFO="Creating ${AppName:-containers for all enabled services}."
+            Question="Start ${AppName:-containers for all enabled services}?"
+            NoNotice="Not starting ${AppName:-containers for all enabled services}."
+            YesNotice="Starting ${AppName:-containers for all enabled services}."
+            ComposeCommand[0]="up -d --remove-orphans ${APPNAME-}"
             ;;
         *)
-            COMPOSECOMMAND="up -d --remove-orphans"
-            COMMANDINFO="Creating containers for all enabled services."
+            Question="Update containers for all enabled services?"
+            NoNotice="Not updating containers for all enabled services."
+            YesNotice="Updating containers for all enabled services."
+            CommandNotice[0]="Pulling the latest images for all enabled services."
+            ComposeCommand[0]="pull --include-deps"
+            CommandNotice[1]="Starting containers for all enabled services."
+            ComposeCommand[1]="up -d --remove-orphans"
             ;;
     esac
-    run_script 'require_docker'
-    notice "${COMMANDINFO}"
-    eval "docker compose --project-directory ${COMPOSE_FOLDER}/ ${COMPOSECOMMAND}" ||
-        fatal "Failed to run compose.\nFailing command: ${F[C]}docker compose --project-directory ${COMPOSE_FOLDER}/ ${COMPOSECOMMAND}"
-    if [[ -n ${COMPOSECOMMAND2-} ]]; then
-        notice "${COMMANDINFO2}"
-        eval "docker compose --project-directory ${COMPOSE_FOLDER}/ ${COMPOSECOMMAND2}" ||
-            fatal "Failed to run compose.\nFailing command: ${F[C]}docker compose --project-directory ${COMPOSE_FOLDER}/ ${COMPOSECOMMAND2}"
+    if run_script 'question_prompt' Y "${Question}" "${DC["TitleWarning"]}${Title}" "${FORCE:+Y}"; then
+        [[ -n ${YesNotice-} ]] && notice "${YesNotice}"
+        run_script 'require_docker'
+        for index in "${!ComposeCommand[@]}"; do
+            [[ -n ${CommandNotice[index]-} ]] && notice "${CommandNotice[index]}"
+            eval "docker compose --project-directory ${COMPOSE_FOLDER}/ ${ComposeCommand[index]}" ||
+                fatal "Failed to run compose.\nFailing command: ${F[C]}docker compose --project-directory ${COMPOSE_FOLDER}/ ${ComposeCommand[index]}"
+        done
+    else
+        [[ -n ${NoNotice-} ]] && notice "${NoNotice}"
     fi
 }
 
