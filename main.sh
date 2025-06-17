e1ce7f07805b3eea4ad4f683e8aa564f9c4ba2f1
CLHatch <CLHatch@gmail.com>
2025-06-17 10:32:17 -0500

    Update main.sh
    Fix order of parameters for `-u` ds -fuv` tries to update to branch `v`

--- main.sh
+++ main.sh
@@ -769,8 +769,8 @@ main() {
                 fi
             fi
             warn "Attempting to run DockSTARTer from ${DS_SYMLINK} location."
-            bash "${DS_SYMLINK}" -fuv
-            bash "${DS_SYMLINK}" -fiv
+            bash "${DS_SYMLINK}" -fvu
+            bash "${DS_SYMLINK}" -fvi
             exec bash "${DS_SYMLINK}" "${ARGS[@]-}"
         fi
     else
@@ -778,7 +778,7 @@ main() {
             warn "Attempting to clone DockSTARTer repo to ${DETECTED_HOMEDIR}/.docker location."
             git clone https://github.com/GhostWriters/DockSTARTer "${DETECTED_HOMEDIR}/.docker" || fatal "Failed to clone DockSTARTer repo.\nFailing command: ${F[C]}git clone https://github.com/GhostWriters/DockSTARTer \"${DETECTED_HOMEDIR}/.docker\""
             notice "Performing first run install."
-            exec bash "${DETECTED_HOMEDIR}/.docker/main.sh" "-fiv"
+            exec bash "${DETECTED_HOMEDIR}/.docker/main.sh" "-fvi"
         fi
     fi
     # Create Symlink
