e16ca3e84b2092846efbb65dd1256d11674eb1aa
CLHatch <CLHatch@gmail.com>
2025-06-20 05:44:29 -0500

    Update menu_display_options_theme.sh

--- .scripts/menu_display_options_theme.sh
+++ .scripts/menu_display_options_theme.sh
@@ -27,7 +27,7 @@ menu_display_options_theme() {
         ThemeDescription["${ThemeName}"]="$(run_script 'env_get' ThemeDescription "${ThemeFile}")"
     done
 
-    local LastChoice="${ThemeName}"
+    local LastChoice="${CurrentTheme}"
     while true; do
         local -a Opts=()
         for ThemeName in "${ThemeList[@]-}"; do
