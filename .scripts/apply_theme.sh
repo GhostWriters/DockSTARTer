882ff37788cb56e522fc2f18f7892b748da3ee3d
CLHatch <CLHatch@gmail.com>
2025-06-19 23:33:11 -0500

    Update apply_theme.sh

--- .scripts/apply_theme.sh
+++ .scripts/apply_theme.sh
@@ -67,9 +67,27 @@ apply_theme() {
     local -a VarList
     readarray -t VarList < <(run_script 'env_var_list' "${ColorFile}")
     for VarName in "${VarList[@]-}"; do
-        local Value="$(run_script 'env_get' "${VarName}" "${ColorFile}")"
+        local Value
+        Value=="$(run_script 'env_get' "${VarName}" "${ColorFile}")"
         notice "${VarName}=${Value}"
-        Value="$(envsubst <<< "${Value}")"
+        Value="$(
+            _B_="${_B_}" \
+            _C_="${_C_}" \
+            _G_="${_G_}" \
+            _K_="${_K_}" \
+            _M_="${_M_}" \
+            _R_="${_R_}" \
+            _W_="${_W_}" \
+            _Y_="${_Y_}" \
+            _RV_="${_RV_}" \
+            _NRV_="${_NRV_}" \
+            _BD_="${_BD_}" \
+            _NBD_="${_NBD_}" \
+            _U_="${_U_}" \
+            _NU_="${_NU_}" \
+            _NC_="${_NC_}" \
+            envsubst <<< "${Value}"
+        )"
         notice "${VarName}=${Value}"
         # shellcheck disable=SC2016 # Expressions don't expand in single quotes, use double quotes for that.
         DC["${VarName}"]="${Value}"
