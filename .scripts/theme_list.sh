f1bb3aeb90e9d59fa27fccac729877217ff02c15
CLHatch <CLHatch@gmail.com>
2025-06-21 13:31:31 -0500

    Update theme_list.sh

--- .scripts/theme_list.sh
+++ .scripts/theme_list.sh
@@ -6,9 +6,7 @@ theme_list() {
     local -a ThemeList
     readarray -t ThemeList < <(find "${THEME_FOLDER}" -maxdepth 1 -type d ! -path "${THEME_FOLDER}" -printf "%f\n" | sort)
     for ThemeName in "${ThemeList[@]-}"; do
-        local ThemeFile="${THEME_FOLDER}/${ThemeName}/${THEME_FILE_NAME}"
-        local DialogFile="${THEME_FOLDER}/${ThemeName}/${DIALOGRC_NAME}"
-        if [[ -f ${ThemeFile} && -f ${DialogFile} ]]; then
+        if run_script 'theme_exists' "${ThemeName}"; then
             echo "${ThemeName}"
         fi
     done
