82dc5e60f68202a22c38ae1043c9868f86b66a9a
CLHatch <CLHatch@gmail.com>
2025-06-22 02:33:09 -0500

    Update menu_dialog_example.sh
    Show examples of color elements

--- .scripts/menu_dialog_example.sh
+++ .scripts/menu_dialog_example.sh
@@ -3,8 +3,31 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 menu_dialog_example() {
-    local Title=${1-}
-    dialog_success "${Title}"
+    local Message=${1-}
+
+    local Title='' 
+    for TitleStyle in Title TitleSuccess TitleWarning TitleError TitleQuestion; do
+        if [[ -n ${Title-} ]]; then
+            Title+=' '
+        fi
+        Title+="${DC["${TitleStyle}"]}${TitleStyle}${DC[NC]}"
+    done
+    DialogText=''
+    DialogText+="${DC["Subtitle"]}${Message}${DC[NC]}\n"
+    DialogText+="  DC["CommandLine"]}Command Line Text${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="DC["KeyCap"]}[up]${DC[NC]} [down]${DC[NC]} [left]${DC[NC]} [right]${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="Application: ${DC[Heading]}AppName${DC[NC]} ${DC[HeadingTag]}(User Defined)${DC[NC]}\n"
+    DialogText+="             ${DC["HeadingAppDescription"]}Application Description${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="   Variable: ${DC[HeadingValue]}VarName${DC[NC]} ${DC[HeadingTag]}(User Defined)${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="Normal text\n"
+    DialogText+="${DC["HeadingAppDescription"]}Hightlighted text${DC[NC]}\n"
+
+
+    dialog_messages "${Title}" "${DialogText}"
 }
 
 test_menu_dialog_example() {
