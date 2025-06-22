6e7f494ac42acf110f8b7f0382734d1ec14e6a83
CLHatch <CLHatch@gmail.com>
2025-06-22 02:38:36 -0500

    Update menu_dialog_example.sh

--- .scripts/menu_dialog_example.sh
+++ .scripts/menu_dialog_example.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 menu_dialog_example() {
     local Message=${1-}
 
-    local Title='' 
+    local Title=''
     for TitleStyle in Title TitleSuccess TitleWarning TitleError TitleQuestion; do
         if [[ -n ${Title-} ]]; then
             Title+=' '
@@ -14,18 +14,17 @@ menu_dialog_example() {
     done
     DialogText=''
     DialogText+="${DC["Subtitle"]}${Message}${DC[NC]}\n"
-    DialogText+="  DC["CommandLine"]}Command Line Text${DC[NC]}\n"
+    DialogText+="  ${DC["CommandLine"]}Command Line Text${DC[NC]}\n"
     DialogText+="\n"
-    DialogText+="DC["KeyCap"]}[up]${DC[NC]} [down]${DC[NC]} [left]${DC[NC]} [right]${DC[NC]}\n"
+    DialogText+="${DC["KeyCap"]}[up]${DC[NC]} [down]${DC[NC]} [left]${DC[NC]} [right]${DC[NC]}\n"
     DialogText+="\n"
     DialogText+="Application: ${DC[Heading]}AppName${DC[NC]} ${DC[HeadingTag]}(User Defined)${DC[NC]}\n"
     DialogText+="             ${DC["HeadingAppDescription"]}Application Description${DC[NC]}\n"
     DialogText+="\n"
-    DialogText+="   Variable: ${DC[HeadingValue]}VarName${DC[NC]} ${DC[HeadingTag]}(User Defined)${DC[NC]}\n"
+    DialogText+="   Variable: ${DC["HeadingValue"]}VarName${DC[NC]} ${DC["HeadingTag"]}(User Defined)${DC[NC]}\n"
     DialogText+="\n"
     DialogText+="Normal text\n"
     DialogText+="${DC["HeadingAppDescription"]}Hightlighted text${DC[NC]}\n"
-
 
     dialog_messages "${Title}" "${DialogText}"
 }
