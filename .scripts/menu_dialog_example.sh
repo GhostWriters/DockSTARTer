c09f050111bbe51ea7bdc542d48da3ce0410d73f
CLHatch <CLHatch@gmail.com>
2025-06-22 02:51:49 -0500

    Themes (#1912)
    * Set up the `.themes` folder
    
    * Implement applying themes, and apply the current theme on GUI startup
    
    * Change color variable names to avoid conflict with global B array.
    
    * Update apply_theme.sh
    
    * Update main.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Add the display option menu entries.
    
    * Update menu_display_options_theme.sh
    
    * Update menu_display_options.sh
    
    * Update colors.ini
    
    * Update menu_display_options_theme.sh
    
    * Update menu_display_options_theme.sh
    
    * Test adding a second theme
    
    * Update apply_theme.sh
    
    * Update menu_display_options_general.sh
    
    * Update main.sh
    
    * Update apply_theme.sh
    
    * Update main.sh
    
    * Move DC and DIALOG declarations to function
    
    * Update main.sh
    
    * Update main.sh
    
    * Update apply_theme.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update apply_theme.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_theme.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update apply_theme.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_general.sh
    
    * Update menu_display_options_theme.sh
    
    * Update menu_display_options_general.sh
    
    * Update apply_theme.sh
    
    * Update menu_display_options_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update apply_theme.sh
    
    * Update colors.ini
    
    * Change `colors.ini` to `theme.ini`
    
    * Update menu_display_options_theme.sh
    
    * Fix theme file comments.
    
    * Added themes made by Westlin
    
    * Update theme.ini
    
    Black Underlined Title.
    White Bold Underlined for LineHeading
    
    * Update .dialogrc
    
    Reverse instead of Bold for selected items.
    
    * Update theme.ini
    
    Add highlighting
    
    * Update highlighting in themes to match Green Screen
    
    * Update theme.ini
    
    Red/Green/Yellow for title.
    
    * Update theme.ini
    
    * Update apply_theme.sh
    
    Adjust full-screen window size to be a bit larger.
    
    * Create theme_list.sh
    
    * Update menu_display_options_theme.sh
    
    * Update apply_theme.sh
    
    * Add color variable reference in comments
    
    * Update theme.ini
    
    Add highlighting
    
    * Update theme.ini
    
    Add highlighting
    
    * Update theme.ini
    
    * Update theme.ini
    
    * Update theme_list.sh
    
    * Create theme_exists.sh
    
    * Create theme_description.sh
    
    * Update menu_display_options_theme.sh
    
    * Update apply_theme.sh
    
    * Update theme_description.sh
    
    * Update apply_theme.sh
    
    * Add `AmberScreen` theme
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Create menu_dialog_example.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update menu_dialog_example.sh
    
    * Revert "Update menu_dialog_example.sh"
    
    This reverts commit c365a74c0fe310134ab851b27f3b5f650cc24f82.
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update menu_display_options_theme.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Update main.sh
    
    * Remove spaces in theme names
    
    * Rename `AmberScreen` to `Marigold`
    
    * Add a color variable for the "User Defined App" in the app list.
    
    * Add a color color variable for "KeyCaps"
    
    * Update menu_dialog_example.sh
    
    * Update menu_dialog_example.sh
    
    Show examples of color elements
    
    * Update menu_dialog_example.sh
    
    * Update menu_dialog_example.sh
    
    * Update menu_dialog_example.sh
    
    * Fix TitleWarning vars
    
    * Update menu_dialog_example.sh

--- .scripts/menu_dialog_example.sh
+++ .scripts/menu_dialog_example.sh
@@ -3,8 +3,30 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 menu_dialog_example() {
-    local Title=${1-}
-    dialog_success "${Title}"
+    local Message=${1-}
+
+    local Title=''
+    for TitleStyle in Title TitleSuccess TitleWarning TitleError TitleQuestion; do
+        if [[ -n ${Title-} ]]; then
+            Title+=' '
+        fi
+        Title+="${DC["${TitleStyle}"]}${TitleStyle}${DC[NC]}"
+    done
+    DialogText=''
+    DialogText+="${DC["Subtitle"]}${Message}${DC[NC]}\n"
+    DialogText+="  ${DC["CommandLine"]}Command Line Text${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="${DC["KeyCap"]}[up]${DC[NC]} ${DC["KeyCap"]}[down]${DC[NC]} ${DC["KeyCap"]}[left]${DC[NC]} ${DC["KeyCap"]}[right]${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="Application: ${DC[Heading]}AppName${DC[NC]} ${DC[HeadingTag]}(User Defined)${DC[NC]}\n"
+    DialogText+="             ${DC["HeadingAppDescription"]}Application Description${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="   Variable: ${DC["HeadingValue"]}VarName${DC[NC]} ${DC["HeadingTag"]}(User Defined)${DC[NC]}\n"
+    DialogText+="\n"
+    DialogText+="Normal text\n"
+    DialogText+="${DC["Highlight"]}Highlighted text${DC[NC]}\n"
+
+    dialog_message "${Title}" "${DialogText}"
 }
 
 test_menu_dialog_example() {
