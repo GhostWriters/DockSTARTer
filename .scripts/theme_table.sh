c069f6781cfc02d51e5b7745b9e7d66d0e935470
CLHatch <CLHatch@gmail.com>
2025-06-22 22:18:51 -0500

    Update theme_table.sh

--- .scripts/theme_table.sh
+++ .scripts/theme_table.sh
@@ -3,6 +3,16 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 theme_table() {
+
+    local ThemeHeading="Theme"
+    local DescriptionHeading="Description"
+    local AuthorHeading="Author"
+
+    local -i ThemeLength=${#ThemeHeading}
+    local -i DescriptionLength=${#DescriptionHeading}
+    local -i AuthorLength=${#AuthorHeading}
+
+    local -a TableArray=()
     local -a ThemeList
     readarray -t ThemeList < <(find "${THEME_FOLDER}" -maxdepth 1 -type d ! -path "${THEME_FOLDER}" -printf "%f\n" | sort)
     for ThemeName in "${ThemeList[@]-}"; do
@@ -10,9 +20,19 @@ theme_table() {
             local ThemeDescription ThemeAuthor
             ThemeDescription="$(run_script 'theme_description' "${ThemeName}")"
             ThemeAuthor="$(run_script 'theme_author' "${ThemeName}")"
-            printf '%s\t%s\t%s\n' "${ThemeName}" "${ThemeDescription}" "${ThemeAuthor}"
+            ThemeLength=$((${#ThemeName} > ThemeLength ? ${#ThemeName} : ThemeLength))
+            DescriptionLength=$((${#ThemeDescription} > DescriptionLength ? ${#ThemeDescription} : DescriptionLength))
+            AuthorLength=$((${#ThemeAuthor} > AuthorLength ? ${#ThemeAuthor} : AuthorLength))
+            TableArray+=("${ThemeName}" "${ThemeDescription}" "${ThemeAuthor}")
         fi
-    done | column --table --separator $'\t' --output-separator '    ' --table-columns=Theme,Description,Author
+    done
+    local ThemeLine DescriptionLine AuthorLine
+    ThemeLine="$(printf %$((ThemeLength + 1))s '' | tr ' ' '-')"
+    DescriptionLine="$(printf %$((DescriptionLength + 1))s '' | tr ' ' '-')"
+    AuthorLine="$(printf %$((AuthorLength + 1))s '' | tr ' ' '-')"
+    TableArray=("${ThemeLine}" "${DescriptionLine}" "${AuthorLine}" "${TableArray[@]}")
+    printf '%s\t%s\t%s\n' "${TableArray[@]}" |
+        column --table --separator $'\t' --output-separator '   ' --table-columns=Theme,Description,Author
 }
 
 test_theme_table() {
