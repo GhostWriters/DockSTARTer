c1a3d4ade6aa66983eb3ee29bd6eeb449e700dd9
CLHatch <CLHatch@gmail.com>
2025-07-08 17:06:06 -0500

    Allow `on/true/yes` for `APPNAME__ENABLED` vars, ignoring case (#1951)

--- .scripts/app_list_disabled.sh
+++ .scripts/app_list_disabled.sh
@@ -4,14 +4,13 @@ IFS=$'\n\t'
 
 app_list_disabled() {
     local APPNAME_REGEX='^[A-Z][A-Z0-9]*(__[A-Z0-9]+)?'
-    local FALSE_REGEX="('?false'?)"
-    local DISABLED_REGEX="__ENABLED\s*=${FALSE_REGEX}"
-    local DISABLED_APPS_REGEX="${APPNAME_REGEX}(?=${DISABLED_REGEX})"
     local -a DISABLED_APPS
     local -a BUILTIN_APPS
 
     #notice "DISABLED_APPS_REGEX [ ${DISABLED_APPS_REGEX} ]"
-    readarray -t DISABLED_APPS < <(grep --color=never -o -P "${DISABLED_APPS_REGEX}" "${COMPOSE_ENV}" | sort || true)
+    readarray -t DISABLED_APPS < <(
+        grep --color=never -o -P "${APPNAME_REGEX}(?=__ENABLED\s*=(?!(?<quote>['|\"]?)(?i:on|true|yes)\k<quote>))" "${COMPOSE_ENV}" | sort || true
+    )
     readarray -t BUILTIN_APPS < <(run_script 'app_list_builtin')
     local -a COMBINED=("${DISABLED_APPS[@]}" "${BUILTIN_APPS[@]}")
     printf "%s\n" "${COMBINED[@]}" | sort | uniq -d
--- .scripts/app_list_enabled.sh
+++ .scripts/app_list_enabled.sh
@@ -4,13 +4,12 @@ IFS=$'\n\t'
 
 app_list_enabled() {
     local APPNAME_REGEX='^[A-Z][A-Z0-9]*(__[A-Z0-9]+)?'
-    local TRUE_REGEX="('?true'?)"
-    local ENABLED_REGEX="__ENABLED\s*=${TRUE_REGEX}"
-    local ENABLED_APPS_REGEX="${APPNAME_REGEX}(?=${ENABLED_REGEX})"
     local -a ENABLED_APPS
 
     #notice "ENABLED_APPS_REGEX [ ${ENABLED_APPS_REGEX} ]"
-    readarray -t ENABLED_APPS < <(grep --color=never -o -P "${ENABLED_APPS_REGEX}" "${COMPOSE_ENV}" | sort || true)
+    readarray -t ENABLED_APPS < <(
+        grep --color=never -o -P "${APPNAME_REGEX}(?=__ENABLED\s*=(?<quote>['|\"]?)(?i:on|true|yes)\k<quote>)" "${COMPOSE_ENV}" | sort || true
+    )
     for AppName in "${ENABLED_APPS[@]}"; do
         if [[ -d "$(run_script 'app_instance_folder' "${AppName}")" ]]; then
             echo "${AppName}"
