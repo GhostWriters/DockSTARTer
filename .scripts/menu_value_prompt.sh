d37e3445174eb8c9474795afe4dc384efc430235
Eric Nemchik <eric@nemchik.com>
2025-07-08 22:18:13 -0500

    fix: :green_heart: fix indent
    Signed-off-by: Eric Nemchik <eric@nemchik.com>

--- .scripts/menu_value_prompt.sh
+++ .scripts/menu_value_prompt.sh
@@ -353,7 +353,7 @@ menu_value_prompt() {
                                 *)
                                     ValueValid="false"
                                     dialog_error "${Title}" "${DialogHeading}\n${DC[Highlight]}${OptionValue["${CurrentValueOption}"]}${DC[NC]} is not ${DC[Highlight]}true${DC[NC]}/${DC[Highlight]}on${DC[NC]}/${DC[Highlight]}yes${DC[NC]} or ${DC[Highlight]}false${DC[NC]}/${DC[Highlight]}off${DC[NC]}/${DC[Highlight]}no${DC[NC]}. Please try setting ${DC[Highlight]}${CleanVarName}${DC[NC]} again."
-                                ;;
+                                    ;;
                             esac
                             ;;
                         "${APPNAME}__NETWORK_MODE")
