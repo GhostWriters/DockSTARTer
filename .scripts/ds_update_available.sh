ba7c11b0a45a2048fd4889c81533b11fbdd627fe
CLHatch <CLHatch@gmail.com>
2025-07-09 14:00:25 -0500

    Update ds_update_available.sh

--- .scripts/ds_update_available.sh
+++ .scripts/ds_update_available.sh
@@ -6,7 +6,8 @@ ds_update_available() {
     pushd "${SCRIPTPATH}" &> /dev/null || fatal "Failed to change directory.\nFailing command: ${F[C]}pushd \"${SCRIPTPATH}\""
     git fetch --quiet &> /dev/null
     local -i result=0
-    [[ $(git rev-parse HEAD 2> /dev/null) != $(git rev-parse @{u} 2> /dev/null) ]] || result=$?
+    # shellcheck disable=SC2319 # This $? refers to a condition, not a command. Assign to a variable to avoid it being overwritten.
+    [[ $(git rev-parse HEAD 2> /dev/null) != $(git rev-parse '@{u}' 2> /dev/null) ]] || result=$?
     popd &> /dev/null
     return ${result}
 }
