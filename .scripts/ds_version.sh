f08d336c0077175a18bb7fb74f71ae7e6559017f
CLHatch <CLHatch@gmail.com>
2025-07-09 15:51:55 -0500

    Use `<branch> <tag>`  or `<branch> commit <commit>` as the DS version

--- .scripts/ds_version.sh
+++ .scripts/ds_version.sh
@@ -3,7 +3,17 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 ds_version() {
-    echo ''
+    local Branch Version
+    pushd "${SCRIPTPATH}" &> /dev/null || fatal "Failed to change directory.\nFailing command: ${F[C]}pushd \"${SCRIPTPATH}\""
+    git fetch --quiet &> /dev/null
+
+    # Get the branch
+    Branch="$(git symbolic-ref --short -q HEAD)"
+    # Get the current tag. If no tag, use the commit instead.
+    Version="$(git describe --tags --exact-match 2> /dev/null)" || Version="commit $(git rev-parse --short HEAD)"
+
+    echo "${Branch} ${Version}"
+    popd &> /dev/null
 }
 
 test_ds_version() {
