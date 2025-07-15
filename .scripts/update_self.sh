9a796610d5204c2231bcafb9ab2b9e041a44b54e
CLHatch <CLHatch@gmail.com>
2025-07-15 17:23:13 -0500

    Return an error notice for `ds --u` if the branch does not exist

--- .scripts/update_self.sh
+++ .scripts/update_self.sh
@@ -50,16 +50,28 @@ update_self() {
         fi
         return
     fi
+
+    BRANCH="${BRANCH:-"${CurrentBranch}"}"
+    if ! ds_branch_exists "${BRANCH}"; then
+        local ErrorMessage="${APPLICATION_NAME} branch ${BRANCH} does not exists."
+        if use_dialog_box; then
+            error "${ErrorMessage}" |&
+                dialog_pipe "${DC[TitleError]}${Title}" "${ErrorMessage}\n${DC[CommandLine]} ds --update $*"
+        else
+            error "${ErrorMessage}"
+        fi
+        return 1
+    fi
+
     if ! run_script 'question_prompt' Y "${Question}" "${Title}" "${FORCE:+Y}"; then
         if use_dialog_box; then
             notice "${NoNotice}" |& dialog_pipe "${DC[TitleError]}${Title}" "${NoNotice}"
         else
             notice "${NoNotice}"
         fi
-        return
+        return 1
     fi
 
-    BRANCH="${BRANCH:-"${CurrentBranch}"}"
     if use_dialog_box; then
         commands_update_self "${BRANCH}" "${YesNotice}" |&
             dialog_pipe "${DC[TitleSuccess]}${Title}" "${YesNotice}\n${DC[CommandLine]} ds --update $*"
