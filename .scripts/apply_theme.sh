546ec1012e0b06c50fcf8c4931c716e7de5e3f26
CLHatch <CLHatch@gmail.com>
2025-07-22 17:02:12 -0500

    Adjust notices when applying themes
    Add the application name to notices when applying themes.  Add CYAN color to the theme names in notices, and add single quotes around theme names in error messages, to match notices and errors in branch names.

--- .scripts/apply_theme.sh
+++ .scripts/apply_theme.sh
@@ -28,7 +28,7 @@ apply_theme() {
     fi
 
     if ! run_script 'theme_exists' "${ThemeName}"; then
-        error "Theme ${ThemeName} does not exist."
+        error "${APPLICATION_NAME} theme '${F[C]}${ThemeName}${NC}' does not exist."
         return
     fi
 
