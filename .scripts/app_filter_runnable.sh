e3b1913943fd300cd5647a1ebcadd7c3bfe42e33
CLHatch <CLHatch@gmail.com>
2025-07-28 04:21:48 -0500

    Optimize `app_filter_runnable` for speed by bringing the `app_is_runnable` code inline to it

--- .scripts/app_filter_runnable.sh
+++ .scripts/app_filter_runnable.sh
@@ -6,7 +6,13 @@ app_filter_runnable() {
     local AppList
     AppList="$(xargs -n 1 <<< "$*")"
     for AppName in ${AppList}; do
-        if run_script 'app_is_runnable' "${AppName}"; then
+        #if run_script 'app_is_runnable' "${AppName}"; then
+        local basename
+        basename=$(run_script 'appname_to_baseappname' "${AppName}")
+        basename=${basename,,}
+        local main_yml="${TEMPLATES_FOLDER}/${basename}/${basename}.yml"
+        local arch_yml="${TEMPLATES_FOLDER}/${basename}/${basename}.${ARCH}.yml"
+        if [[ -f ${main_yml} && -f ${arch_yml} ]]; then
             echo "${AppName}"
         fi
     done
