e7921a85bee04308849a1986ff77e6442d254837
CLHatch <CLHatch@gmail.com>
2025-08-10 16:23:55 -0500

    Migrate the user's `env_files/<appname>.env` files to `.env.app.<appname>` files (#2133)

--- .scripts/app_env_file.sh
+++ .scripts/app_env_file.sh
@@ -3,14 +3,32 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 app_env_file() {
-    local AppName=${1:-}
+    local appname=${1:-}
+    appname="${appname,,}"
 
-    if [[ ! -d ${APP_ENV_FOLDER} ]]; then
-        warn "Folder ${C["Folder"]}${APP_ENV_FOLDER}${NC} not found. Creating it."
-        mkdir -p "${APP_ENV_FOLDER}" ||
-            fatal "Failed to create folder.\nFailing command: ${C["FailingCommand"]}mkdir -p \"${APP_ENV_FOLDER}\""
+    local AppEnvFilename=".env.app.${appname}"
+    local OldAppEnvFilename="${appname}.env"
+    local AppEnvFile="${COMPOSE_FOLDER}/${AppEnvFilename}"
+    local OldAppEnvFile="${APP_ENV_FOLDER}/${OldAppEnvFilename}"
+
+    if [[ ! -f ${AppEnvFile} && -f ${OldAppEnvFile} ]]; then
+        # Migrate from the old env_files/appname.env files to .env.app.appname
+        notice "Renaming '${C["File"]}${OldAppEnvFile}${NC}' to '${C["File"]}${AppEnvFile}${NC}'"
+        mv "${OldAppEnvFile}" "${AppEnvFile}" ||
+            fatal "Failed to rename file.\nFailing command: ${C["FailingCommand"]}mv \"${OldAppEnvFile}\" \"${AppEnvFile}\""
+        local SearchString="${APP_ENV_FOLDER_NAME}/${OldAppEnvFilename}"
+        if [[ -f ${COMPOSE_OVERRIDE} ]] && grep -q -F "${SearchString}" "${COMPOSE_OVERRIDE}"; then
+            local ReplaceString="${AppEnvFilename}"
+            # Replace all references to 'env_files/appname.env' with '.env.app.appname' in the override file
+            notice "Replacing in '${C["File"]}${COMPOSE_OVERRIDE}${NC}':"
+            notice "   '${C["Var"]}${SearchString}${NC}' with '${C["Var"]}${ReplaceString}${NC}'"
+            # Escape . to [.] to use in sed
+            SearchString="${SearchString//./[.]}"
+            sed -i "s|${SearchString}|${ReplaceString}|g" "${COMPOSE_OVERRIDE}" ||
+                fatal "Failed to edit override file.\nFailing command: ${C["FailingCommand"]}sed -i \"s|${SearchString}|${ReplaceString}|g\" \"${COMPOSE_OVERRIDE}\""
+        fi
     fi
-    echo "${APP_ENV_FOLDER}/${AppName,,}.env"
+    echo "${AppEnvFile}"
 }
 
 test_app_env_file() {
