2d89b34494ae0f97b4ae0edfa1309549ca1e3337
CLHatch <CLHatch@gmail.com>
2025-09-07 05:43:23 -0500

    Slight fix to regex searching for `.env.app.appname` lines in the override file (#2185)
    The handling of checking for a space or a newline at the end of the string was incorrect. It was checking for a space AND a new line

--- .scripts/app_is_referenced.sh
+++ .scripts/app_is_referenced.sh
@@ -20,7 +20,7 @@ app_is_referenced() {
         local AppEnvFile
         AppEnvFile="$(basename "$(run_script 'app_env_file' "${APPNAME}")")"
         local SearchString="${AppEnvFile//./[.]}"
-        if grep -q -P "^(?:[^#]*)(?:^|\s)(?<Q>['\"]?)${SearchString}(?=\k<Q>\s|$)" "${COMPOSE_OVERRIDE}" &> /dev/null; then
+        if grep -q -P "^(?:[^#]*)(?:\s|^)(?<Q>['\"]?)${SearchString}(?=\k<Q>(?:\s|$))" "${COMPOSE_OVERRIDE}" &> /dev/null; then
 
             return 0
         fi
