328b9bc3d129e26d20152dd289afc0b6ba1503b7
CLHatch <CLHatch@gmail.com>
2025-09-13 23:22:28 -0500

    Check for existing dependencies before trying to install them in apk

--- .scripts/pm_apk_install.sh
+++ .scripts/pm_apk_install.sh
@@ -2,17 +2,70 @@
 set -Eeuo pipefail
 IFS=$'\n\t'
 
+declare Title="Install Dependencies"
+
 pm_apk_install() {
-    local Title="Install Dependencies"
-    notice "Installing dependencies. Please be patient, this can take a while."
-    local COMMAND=""
-    local REDIRECT="> /dev/null 2>&1"
-    if run_script 'question_prompt' Y "Would you like to display the command output?" "${Title}" "${VERBOSE:+Y}"; then
-        #shellcheck disable=SC2016 # (info): Expressions don't expand in single quotes, use double quotes for that.
-        REDIRECT='run_command_dialog "${Title}" "${COMMAND}" "" '
+    if use_dialog_box; then
+        coproc {
+            dialog_pipe "${DC["TitleSuccess"]-}Install Dependencies" "Please be patient, this can take a while.\n${DC["CommandLine"]-} ${APPLICATION_COMMAND} --install" ""
+        }
+        local -i DialogBox_PID=${COPROC_PID}
+        local -i DialogBox_FD="${COPROC[1]}"
+        pm_apk_install_commands >&${DialogBox_FD} 2>&1
+        exec {DialogBox_FD}<&-
+        wait ${DialogBox_PID}
+    else
+        pm_apk_install_commands
     fi
-    COMMAND='sudo apk add coreutils bsdmainutils curl dialog gettext git grep openrc sed util-linux'
-    eval "${REDIRECT}${COMMAND}" || fatal "Failed to install dependencies from apk.\nFailing command: ${C["FailingCommand"]}${COMMAND}"
+}
+
+pm_apk_install_commands() {
+    local IgnorePackages=''
+    local Command=""
+
+    local REDIRECT='> /dev/null 2>&1 '
+    if run_script 'question_prompt' Y "Would you like to display the command output?" "${Title}" "${VERBOSE:+Y}"; then
+        REDIRECT='2>&1 '
+    fi
+
+    local -a Dependencies=("${COMMAND_DEPS[@]}")
+    if [[ ${FORCE-} != true ]]; then
+        for index in "${!Dependencies[@]}"; do
+            if [[ -n $(command -v "${Dependencies[index]}") ]]; then
+                unset 'Dependencies[index]'
+            fi
+        done
+        Dependencies=("${Dependencies[@]}")
+    fi
+    if [[ ${#Dependencies[@]} -eq 0 ]]; then
+        notice "All dependencies have already been installed."
+    else
+        notice "Installing dependencies. Please be patient, this can take a while."
+
+        notice "Determining packages to install."
+        local -a Packages
+        local DepsSearch
+        DepsSearch="$(printf 'cmd:%s ' "${Dependencies[@]}" | xargs)"
+        Command="apk search -xqa ${DepsSearch}"
+        info "Running: ${C["RunningCommand"]}${Command}${NC}"
+        local Packages
+        Packages="$(eval "${Command}" 2> /dev/null)" ||
+            fatal "Failed to find packages to install.\nFailing command: ${C["FailingCommand"]}${Command}"
+        if [[ -n ${IgnorePackages} ]]; then
+            Packages="$(grep -E -v "\b(${IgnorePackages})\b" <<< "${Packages}")"
+        fi
+        Packages="$(sort -u <<< "${Packages}" | xargs)"
+
+        if [[ -z ${Packages} ]]; then
+            notice "No packages found to install."
+        else
+            notice "Installing packages."
+            Command="sudo apk add ${Packages}"
+            info "Running: ${C["RunningCommand"]}${Command}${NC}"
+            eval "${REDIRECT}${Command}" ||
+                fatal "Failed to install dependencies from apk.\nFailing command: ${C["FailingCommand"]}${Command}"
+        fi
+    fi
 }
 
 test_pm_apk_install() {
