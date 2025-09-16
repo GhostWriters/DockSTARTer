058356b97b56a5621290489bc1cc1c7ac5a693b0
CLHatch <CLHatch@gmail.com>
2025-09-16 08:54:36 -0500

    Update ds_functions.sh

--- .includes/ds_functions.sh
+++ .includes/ds_functions.sh
@@ -66,7 +66,7 @@ ds_switch_branch() {
         export FORCE=true
         export PROMPT="CLI"
         notice "Automatically switching from ${APPLICATION_NAME} branch '${C["Branch"]}${SOURCE_BRANCH}${NC}' to '${C["Branch"]}${TARGET_BRANCH}${NC}'."
-        run_script 'update_self' "${TARGET_BRANCH}" bash "${SCRIPTNAME}" "$@"
+        run_script 'update_self' "${TARGET_BRANCH}" "${ARGS[@]}"
         exit
     fi
 }
