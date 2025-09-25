f3e04ed01b2343293ffc653ab9f069b0f546c4ce
CLHatch <CLHatch@gmail.com>
2025-09-25 01:09:15 -0500

    Update usage.sh

--- .includes/usage.sh
+++ .includes/usage.sh
@@ -18,6 +18,7 @@ usage() {
                     APPLICATION_HEADING+=" (${C["Update"]-}Update Available${NC-})"
                 fi
                 cat << EOF
+Usage: ${C["UserCommand"]-}${APPLICATION_COMMAND}${NC-} [${C["UserCommand"]-}<Flags>${NC-}] [${C["UserCommand"]-}<Command>${NC-}] ...${NC-}
 NOTE: The '${C["UserCommand"]-}${APPLICATION_COMMAND}${NC-}' shortcut is only available after the first run of
     bash main.sh
 
