f5189fd0301a27ddc9d88cba8affbde8c8e26e98
CLHatch <CLHatch@gmail.com>
2025-09-25 09:41:05 -0500

    Migrate the `DOCKER_STORAGE` vars to `DOCKER_STORAGE_ON`

--- .scripts/env_migrate_global.sh
+++ .scripts/env_migrate_global.sh
@@ -8,6 +8,10 @@ env_migrate_global() {
     run_script 'env_migrate' DOCKERGID DOCKER_GID
     run_script 'env_migrate' DOCKERHOSTNAME DOCKER_HOSTNAME
     run_script 'env_migrate' DOCKERSTORAGEDIR DOCKER_VOLUME_STORAGE
+    run_script 'env_migrate' DOCKER_STORAGE DOCKER_STORAGE_ON
+    run_script 'env_migrate' DOCKER_STORAGE2 DOCKER_STORAGE2_ON
+    run_script 'env_migrate' DOCKER_STORAGE3 DOCKER_STORAGE3_ON
+    run_script 'env_migrate' DOCKER_STORAGE4 DOCKER_STORAGE4_ON
     run_script 'env_migrate' LAN_NETWORK GLOBAL_LAN_NETWORK
     run_script 'env_migrate' NS1 GLOBAL_NS1
     run_script 'env_migrate' NS2 GLOBAL_NS2
