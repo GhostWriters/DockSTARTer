4f02b6597dee6c296b4371c243df802331ce0583
CLHatch <CLHatch@gmail.com>
2025-10-01 04:18:11 -0500

    Add unit tests to `app_description` and `app_description_from_template` (#2255)

--- .scripts/app_description_from_template.sh
+++ .scripts/app_description_from_template.sh
@@ -22,9 +22,32 @@ app_description_from_template() {
 }
 
 test_app_description_from_template() {
-    for AppName in WATCHTOWER SAMBA RADARR NZBGET NONEXISTENTAPP; do
-        local Result="no"
-        Result="$(run_script 'app_description_from_template' "${AppName}")"
-        notice "[${AppName}] [${Result}]"
-    done
+    local ForcePass='' # Force the tests to pass even on failure if set to a non-empty value
+    local -i result=0
+    run_script 'appvars_create' WATCHTOWER NZBGET
+    local -a Test=(
+        WATCHTOWER "Automatically update running Docker containers"
+        SAMBA "(DEPRECATED) No replacement"
+        RADARR "Automatically download movies via Usenet and BitTorrent"
+        nzbget "Efficient usenet downloader"
+        NZBGet "Efficient usenet downloader"
+        NZBGET "Efficient usenet downloader"
+        NONEXISTENTAPP "Nonexistentapp is a user defined application"
+        WATCHTOWER__INSTANCE "Automatically update running Docker containers"
+        SAMBA__INSTANCE "(DEPRECATED) No replacement"
+        RADARR__INSTANCE "Automatically download movies via Usenet and BitTorrent"
+        NZBGET__INSTANCE "Efficient usenet downloader"
+        NONEXISTENTAPP__INSTANCE "Nonexistentapp__Instance is a user defined application"
+    )
+    run_unit_tests_pipe "App" "App" "${ForcePass}" < <(
+        for ((i = 0; i < ${#Test[@]}; i += 2)); do
+            printf '%s\n' \
+                "${Test[i]}" \
+                "${Test[i + 1]}" \
+                "$(run_script 'app_description_from_template' "${Test[i]}")"
+        done
+    )
+    result=$?
+    run_script 'appvars_purge' WATCHTOWER NZBGET
+    return ${result}
 }
