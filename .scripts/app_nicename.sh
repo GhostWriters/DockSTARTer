0f770e81808408ffa66f814db35008405261146d
CLHatch <CLHatch@gmail.com>
2025-09-30 23:19:48 -0500

    Add unit tests for `app_nicename` and `app_nicename_from_template` (#2253)

--- .scripts/app_nicename.sh
+++ .scripts/app_nicename.sh
@@ -28,9 +28,32 @@ app_nicename() {
 }
 
 test_app_nicename() {
-    for AppName in WATCHTOWER SAMBA RADARR NZBGET NONEXISTENTAPP; do
-        local Result="no"
-        Result="$(run_script 'app_nicename' "${AppName}")"
-        notice "[${AppName}] [${Result}]"
-    done
+    local ForcePass=''
+    local -i result=0
+    run_script 'appvars_create' WATCHTOWER NZBGET
+    local -a Test=(
+        WATCHTOWER Watchtower
+        SAMBA Samba
+        RADARR Radarr
+        nzbget NZBGet
+        NZBGet NZBGet
+        NZBGET NZBGet
+        NONEXISTENTAPP Nonexistentapp
+        WATCHTOWER__INSTANCE Watchtower__Instance
+        SAMBA__INSTANCE Samba__Instance
+        RADARR__INSTANCE Radarr__Instance
+        NZBGET__INSTANCE Nzbget__Instance
+        NONEXISTENTAPP__INSTANCE Nonexistentapp__Instance
+    )
+    run_unit_tests_pipe "App" "App" "${ForcePass}" < <(
+        for ((i = 0; i < ${#Test[@]}; i += 2)); do
+            printf '%s\n' \
+                "${Test[i]}" \
+                "${Test[i + 1]}" \
+                "$(run_script 'app_nicename' "${Test[i]}")"
+        done
+    )
+    result=$?
+    run_script 'appvars_purge' WATCHTOWER NZBGET
+    return ${result}
 }
