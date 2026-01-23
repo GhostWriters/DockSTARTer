f17926c269661d21a58d7cefc786e02106dc4783
CLHatch <CLHatch@gmail.com>
2026-01-22 22:34:36 -0600

    Use RunAndLog in `get_docker`

--- .scripts/get_docker.sh
+++ .scripts/get_docker.sh
@@ -23,43 +23,33 @@ get_docker() {
 command_get_docker() {
 	# https://github.com/docker/docker-install
 	local MKTEMP_GET_DOCKER
-	local COMMAND
-	#shellcheck disable=SC2034 # (warning): MKTEMP_GET_DOCKER appears unused. Verify use (or export if used externally).
 	MKTEMP_GET_DOCKER=$(mktemp -t "${APPLICATION_NAME}.${FUNCNAME[0]}.MKTEMP_GET_DOCKER.XXXXXXXXXX") ||
 		fatal \
 			"Failed to create temporary docker install script." \
 			"Failing command: ${C["FailingCommand"]}mktemp -t \"${APPLICATION_NAME}.${FUNCNAME[0]}.MKTEMP_GET_DOCKER.XXXXXXXXXX\""
 	info "Downloading docker install script."
-	#shellcheck disable=SC2016 # (info): Expressions don't expand in single quotes, use double quotes for that.
-	COMMAND="curl -fsSL https://get.docker.com -o '${MKTEMP_GET_DOCKER}'"
-	notice "Running: ${C["RunningCommand"]}${COMMAND}${NC}"
-	eval "${COMMAND}" ||
-		fatal \
-			"Failed to get docker install script." \
-			"Failing command: ${C["FailingCommand"]}${COMMAND}"
+	RunAndLog notice notice \
+		fatal "Failed to get docker install script." \
+		curl -fsSL https://get.docker.com -o "${MKTEMP_GET_DOCKER}"
+
 	info "Running docker install script."
-	#shellcheck disable=SC2016 # (info): Expressions don't expand in single quotes, use double quotes for that.
-	COMMAND="sh '${MKTEMP_GET_DOCKER}'"
-	notice "Running: ${C["RunningCommand"]}${COMMAND}${NC}"
-	eval "${COMMAND}" ||
-		fatal \
-			"Failed to install docker." \
-			"Failing command: ${C["FailingCommand"]}${COMMAND}"
-	#shellcheck disable=SC2016 # (info): Expressions don't expand in single quotes, use double quotes for that.
-	COMMAND='rm -f "${MKTEMP_GET_DOCKER}"'
-	eval "${COMMAND}" ||
-		warn "Failed to remove temporary docker install script.\nFailing command: ${C["FailingCommand"]}${COMMAND}"
+	RunAndLog notice notice \
+		warn "Failed to install docker." \
+		sh "${MKTEMP_GET_DOCKER}"
+
+	info "Removing temporary docker install script."
+	RunAndLog notice notice \
+		warn "Failed to remove temporary docker install script." \
+		rm -f "${MKTEMP_GET_DOCKER}"
 }
 
 test_get_docker() {
 	run_script 'remove_snap_docker'
 	run_script 'get_docker'
-	docker --version ||
-		fatal \
-			"Failed to determine docker version." \
-			"Failing command: ${C["FailingCommand"]}docker --version"
-	docker compose version ||
-		fatal \
-			"Failed to determine docker compose version." \
-			"Failing command: ${C["FailingCommand"]}docker compose version"
+	RunAndLog notice notice \
+		fatal "Failed to determine docker version." \
+		docker --version
+	RunAndLog notice notice \
+		fatal "Failed to determine docker compose version." \
+		docker compose version
 }
