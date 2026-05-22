f16dfac80e686942ba9068a30f59ffdd4e5d9b36
CLHatch <CLHatch@gmail.com>
2026-05-22 12:01:35 -0500

    Introduce helper functions for arrays in `env_format`

--- scripts/env_lines.sh
+++ scripts/env_lines.sh
@@ -2,14 +2,17 @@
 set -Eeuo pipefail
 IFS=$'\n\t'
 
-declare -a _dependencies_list=(
-	sed
-)
+declare -a _dependencies_list=()
 
 env_lines() {
 	local VAR_FILE=${1:-$COMPOSE_ENV}
 	if [[ -f ${VAR_FILE} ]]; then
-		${SED} -n "s/^\s*\([A-Za-z0-9_]*\)\s*=/\1=/p" "${VAR_FILE}"
+		local line
+		while IFS= read -r line; do
+			if [[ ${line} =~ ^[[:space:]]*([A-Za-z0-9_]+)[[:space:]]*=(.*) ]]; then
+				printf '%s=%s\n' "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}"
+			fi
+		done < "${VAR_FILE}"
 	fi
 }
 
